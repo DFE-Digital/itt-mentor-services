0c044cd6bf62268ae745fbb3a37876ea0011d6f5
Jamie <jamie.cleare@education.gov.uk>
2023-12-12 11:02:08 +0000

    Linting since better_html added

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 RSpec.describe "Sessions", type: :request do
   describe "POST /auth/developer/callback" do
     it "returns http success" do
-      post auth_developer_callback_path, params: {
-        first_name: "Anne", 
-        last_name: "Wilson",
-        email: "anne_wilson@example.com"
-      }
+      post auth_developer_callback_path,
+           params: {
+             first_name: "Anne",
+             last_name: "Wilson",
+             email: "anne_wilson@example.com"
+           }
       follow_redirect!
 
       expect(response).to have_http_status(:success)
