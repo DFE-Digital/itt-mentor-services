45171096efd5fd320efe13361e98c0c8ccc4b470
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2023-12-19 15:16:06 +0000

    Add phase banner to the app
    We want to add a phase banner that will indicate to our users that this
    app is in Beta. The banner title is dynamic based on the environment we
    are in and both claims and placements can control the content of it.

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module IttMentorServices
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_lib(ignore: %w[])
 
     config.assets.paths << Rails.root.join(
       "node_modules/govuk-frontend/dist/govuk/assets"
---
+++ spec/lib/hosting_environment_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe HostingEnvironment do
+  describe ".name" do
+    it "returns the name of the hosting environment" do
+      current_service = "claims"
+      expect(described_class.name(current_service)).to eq("test")
+    end
+
+    context "when environment is production" do
+      it "returns the name of the hosting environment for claims" do
+        allow(Rails.env).to receive(:production?).and_return(true)
+        current_service = "claims"
+        expect(described_class.name(current_service)).to eq("beta")
+      end
+
+      it "returns the name of the hosting environment for placements" do
+        allow(Rails.env).to receive(:production?).and_return(true)
+        current_service = "placements"
+        expect(described_class.name(current_service)).to eq("beta")
+      end
+    end
+  end
+
+  describe ".banner_description" do
+    it "returns the banner description of the hosting environment for claims" do
+      current_service = "claims"
+      expect(described_class.banner_description(current_service)).to eq(
+        "Make a complaint or give feedback"
+      )
+    end
+
+    it "returns the banner description of the hosting environment for placements" do
+      current_service = "placements"
+      expect(described_class.banner_description(current_service)).to eq(
+        "Make a complaint or give feedback"
+      )
+    end
+  end
+end
