90044d5a9665ca1668250119b314582ff5fc0736
Daniel Dye <daniel.dye@education.gov.uk>
2023-12-19 14:09:38 +0000

    Add foundations for claims support routing

--- config/routes/claims.rb
+++ config/routes/claims.rb
@@ -2,4 +2,13 @@ scope module: :claims, as: :claims, constraints: { host: ENV["CLAIMS_HOST"] } do
   root to: "pages#index"
 
   resources :schools, only: [:index]
+
+  namespace :support do
+    root to: redirect("/support/schools")
+
+    resources :schools do
+      resources :claims
+      resources :users
+    end
+  end
 end
