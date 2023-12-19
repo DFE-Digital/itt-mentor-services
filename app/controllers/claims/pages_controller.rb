1509910151111d8ac5d62a36b29e89ad81daa25d
Daniel Dye <daniel.dye@education.gov.uk>
2023-12-19 14:09:16 +0000

    Redirect claims users to sign-in page when unauthenticated
    - Also redirect users from the sign-in page to the personas page when `SIGN_IN_METHOD` is "persona".
    
    - Add `after_sign_in_path` and `after_sign_out_path` methods to customise redirects based on criteria.
    
    - Add "account" and "sign out" navigation items to application layout.

--- app/controllers/claims/pages_controller.rb
+++ app/controllers/claims/pages_controller.rb
@@ -1,2 +1,3 @@
 class Claims::PagesController < ApplicationController
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,15 @@ Rails.application.routes.draw do
   end
 
   # User Account Details
-  get("/account", to: "account#index")
+  get "/account", to: "account#index"
+  get "/sign-in", to: "sessions#new", as: :sign_in
 
   # Persona Sign In
   case ENV.fetch("SIGN_IN_METHOD")
   when "persona"
-    get("/personas", to: "personas#index")
-    get("/auth/developer/sign-out", to: "sessions#signout")
-    post("/auth/developer/callback", to: "sessions#callback")
+    get "/personas", to: "personas#index"
+    get "/auth/developer/sign-out", to: "sessions#destroy", as: :sign_out
+    post "/auth/developer/callback", to: "sessions#callback", as: :auth_callback
   end
 
   resources :service_updates, only: %i[index]
