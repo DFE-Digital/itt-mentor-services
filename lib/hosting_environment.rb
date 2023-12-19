39779608ab91fdc313fbfaa380b282516b7c9294
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2023-12-19 15:16:06 +0000

    Add phase banner to the app
    We want to add a phase banner that will indicate to our users that this
    app is in Beta. The banner title is dynamic based on the environment we
    are in and both claims and placements can control the content of it.

---
+++ lib/hosting_environment.rb
@@ -0,0 +1,18 @@
+module HostingEnvironment
+  PRODUCTION_BANNER_NAME = {
+    claims: "beta",
+    placements: "beta"
+  }.with_indifferent_access.freeze
+
+  def self.name(current_service)
+    if Rails.env.production?
+      PRODUCTION_BANNER_NAME.fetch(current_service)
+    else
+      Rails.env
+    end
+  end
+
+  def self.banner_description(current_service)
+    I18n.t(".models.hosting_environment.#{current_service}.banner.description")
+  end
+end
