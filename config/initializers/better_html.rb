a050644b85cc48491f0682d719fdde18381cb5d1
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2023-12-20 15:54:57 +0000

    Skip BetterHtml config in production
    Co-authored-by: Ollie Treend <ollie@treend.uk>

--- config/initializers/better_html.rb
+++ config/initializers/better_html.rb
@@ -1,3 +1,6 @@
+# better_html gem is only present in development and test
+return if Rails.env.production?
+
 BetterHtml.config =
   BetterHtml::Config.new(
     YAML.safe_load(File.read(Rails.root.join(".better-html.yml")))
