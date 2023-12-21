ae658ae8f93c6fe1ff877188f637e6939f93094c
Daniel Dye <daniel.dye@education.gov.uk>
2023-12-21 17:35:56 +0000

    Configure Capybara to use rack_test driver by default in system specs

---
+++ spec/support/capybara.rb
@@ -0,0 +1,30 @@
+require 'capybara/rspec'
+
+# Use different Capybara ports when running tests in parallel
+if ENV['TEST_ENV_NUMBER']
+  Capybara.server_port = 9887 + ENV['TEST_ENV_NUMBER'].to_i
+end
+
+Capybara.register_driver :chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless') unless ENV['HEADLESS'] == 'false'
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+end
+
+Capybara.javascript_driver = :chrome_headless
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    if self.class.metadata[:js] == true
+      driven_by(:chrome_headless)
+    else
+      driven_by(:rack_test)
+    end
+  end
+end
