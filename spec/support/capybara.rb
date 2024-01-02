968a6fda8adcdc305cd586c265f7eacb3cd863b7
Daniel Dye <daniel.dye@education.gov.uk>
2024-01-02 17:03:43 +0000

    Update Capybara configuration
    Capybara's default chrome headless driver is :selenium_chrome_headless. Override that one.
    
    Use --headless=new. See more: https://developer.chrome.com/docs/chromium/new-headless
    
    `js: true` is a default supported option for Capybara. We only need to set the system tests to be driven by Capybara's current driver.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,26 +7,22 @@ end
 
 Capybara.always_include_port = true
 
-Capybara.register_driver :chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-
-  options.add_argument("--headless") unless ENV["HEADLESS"] == "false"
-  options.add_argument("--no-sandbox")
-  options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--disable-gpu")
-  options.add_argument("--window-size=1400,1400")
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.add_argument("--headless=new")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--window-size=1400,1400")
+  end
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
-Capybara.javascript_driver = :chrome_headless
+Capybara.javascript_driver = :selenium_chrome_headless
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    if self.class.metadata[:js] == true
-      driven_by(:chrome_headless)
-    else
-      driven_by(:rack_test)
-    end
+    driven_by Capybara.current_driver
   end
 end
