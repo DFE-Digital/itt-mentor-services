da68e2630d8dd9a553805f2b8141ddad2b7ab951
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-01-02 14:23:44 +0000

    Change `/organisations` to `/schools` in claims
    In the claims domain, we don’t have the concept of a provider
    so we should just use the /schools for route/controller

--- config/routes/claims.rb
+++ config/routes/claims.rb
@@ -1,5 +1,5 @@
 scope module: :claims, as: :claims, constraints: { host: ENV["CLAIMS_HOST"] } do
   root to: "pages#index"
 
-  resources :organisations, only: [:index]
+  resources :schools, only: [:index]
 end
--- spec/system/claims/view_organisations_spec.rb
+++ spec/system/claims/view_organisations_spec.rb
@@ -1,23 +1,23 @@
 require "rails_helper"
 
-RSpec.describe "View organisations", type: :system do
+RSpec.describe "View schools", type: :system do
   around do |example|
     Capybara.app_host = "https://#{ENV["CLAIMS_HOST"]}"
     example.run
     Capybara.app_host = nil
   end
 
-  scenario "I sign in as persona Mary with multiple organistions" do
+  scenario "I sign in as persona Mary with multiple schools" do
     persona = given_the_claims_persona("Mary")
-    and_persona_has_multiple_organisations(persona)
+    and_persona_has_multiple_schools(persona)
     when_i_visit_claims_personas
     when_i_click_sign_in(persona)
-    i_can_see_a_list_marys_claims_organisations(persona)
+    i_can_see_a_list_marys_claims_schools(persona)
   end
 
-  scenario "I sign in as persona Anne with one organisation" do
+  scenario "I sign in as persona Anne with one school" do
     persona = given_the_claims_persona("Anne")
-    and_persona_has_one_organisation(persona)
+    and_persona_has_one_school(persona)
     when_i_visit_claims_personas
     when_i_click_sign_in(persona)
     i_am_redirected_to_the_root_path
@@ -30,14 +30,14 @@ def given_the_claims_persona(persona_name)
   create(:persona, persona_name.downcase.to_sym, service: "claims")
 end
 
-def and_persona_has_multiple_organisations(persona)
+def and_persona_has_multiple_schools(persona)
   school1 = create(:school)
   school2 = create(:school)
   create(:membership, user: persona, organisation: school1)
   create(:membership, user: persona, organisation: school2)
 end
 
-def and_persona_has_one_organisation(persona)
+def and_persona_has_one_school(persona)
   create(:membership, user: persona, organisation: create(:school))
 end
 
@@ -49,7 +49,7 @@ def when_i_click_sign_in(persona)
   click_on "Sign In as #{persona.first_name}"
 end
 
-def i_can_see_a_list_marys_claims_organisations(persona)
+def i_can_see_a_list_marys_claims_schools(persona)
   expect(page).to have_content("Organisations")
   expect(page).to have_content(persona.schools.first.name)
   expect(page).to have_content(persona.schools.last.name)
