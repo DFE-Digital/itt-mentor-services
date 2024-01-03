430493d7348598131b398594c9593be7cdea194e
Ollie Treend <ollie@treend.uk>
2024-01-03 16:33:12 +0000

    Add FactoryBot sequences for DfE identifiers
    DfE uses various different identifiers:
    
    - UKPRN (UK Provider Reference Number)
    - URN (Unique Reference Number): used to identify schools in GIAS
    - Provider codes: used to identify ITT Providers in the 'Find teacher
      training courses' and 'Publish teacher training courses' services, and
      the associated Teacher Training Courses API.
    
    Each of these identifiers should be unique. Factories can use these
    global sequences to avoid duplicate identifiers being created during
    test runs.

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,1 +1,14 @@
 RSpec.configure { |config| config.include FactoryBot::Syntax::Methods }
+
+# Define global sequences for generating various types of identifiers that we need to use
+FactoryBot.define do
+  # UKPRNs are 8 character numeric strings beginning at 10000001
+  # We treat them as strings because we never perform maths on them
+  sequence(:ukprn) { |n| (10_000_000 + n).to_s }
+
+  # URNs are 6 character numeric strings beginning at 100000
+  sequence(:urn) { |n| (100_000 + n - 1).to_s }
+
+  # Provider codes are 3 character alphanumeric strings – e.g. "T92"
+  sequence(:provider_code) { |n| n.to_s(36).rjust(3, "0").upcase }
+end
