8b643d98d55d21aa648873d3206be8d60c334302
Ollie Treend <ollie@treend.uk>
2024-01-04 17:01:44 +0000

    Add new attributes to the Provider model
    Add columns to hold the Provider attributes that we receive from the
    Teacher Training Courses API.
    
    I did consider storing the address in one single 'text' field, rather
    than the individual address lines received from the API. However we may
    still need query providers by their postcode, so it still makes sense to
    keep these fields separate.

---
+++ db/migrate/20240104161831_add_provider_attributes.rb
@@ -0,0 +1,24 @@
+class AddProviderAttributes < ActiveRecord::Migration[7.1]
+  def change
+    create_enum :provider_type, %w[scitt lead_school university]
+
+    change_table :providers, bulk: true do |t|
+      t.enum :provider_type, enum_type: "provider_type", null: false
+      t.string :name, null: false
+      t.string :ukprn
+      t.string :urn
+      t.string :email
+      t.string :telephone
+      t.string :website
+
+      # Postal address fields
+      t.string :street_address_1
+      t.string :street_address_2
+      t.string :street_address_3
+      t.string :town
+      t.string :city
+      t.string :county
+      t.string :postcode
+    end
+  end
+end
