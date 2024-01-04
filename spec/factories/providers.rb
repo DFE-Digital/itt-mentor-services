145e6b2b0a88aa9d8e243cc5d4ac16d393cf2cc8
Ollie Treend <ollie@treend.uk>
2024-01-04 15:05:09 +0000

    Add a 'placements' flag to Providers
    This commit adds a 'placements' boolean (true|false) attribute to the
    Provider model. This follows the existing pattern established by the
    School model which uses the boolean values to indicate whether that
    organisation has been 'onboarded' into the service.
    
    We will use this to differentiate between 'known' providers – i.e.
    providers whose name and details we know – and those which have been
    onboarded into the School Placements service.
    
    The 'known' providers will be populated using data sourced from the
    Teacher Training Courses API, meaning we'll be able to use it as a
    lookup table whenever we need to list *all* providers, regardless of
    their onboarding state.

--- spec/factories/providers.rb
+++ spec/factories/providers.rb
@@ -3,12 +3,14 @@
 # Table name: providers
 #
 #  id            :uuid             not null, primary key
+#  placements    :boolean          default(FALSE)
 #  provider_code :string           not null
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #
 # Indexes
 #
+#  index_providers_on_placements     (placements)
 #  index_providers_on_provider_code  (provider_code) UNIQUE
 #
 FactoryBot.define do
