ff9100e8377c0f88f3d5bd37ec1b4c81884dfbd4
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-01-04 10:51:43 +0000

    Capture additional fields for gias school

---
+++ db/migrate/20240104104439_add_additional_details_to_gias_schools.rb
@@ -0,0 +1,26 @@
+class AddAdditionalDetailsToGiasSchools < ActiveRecord::Migration[7.1]
+  def change
+    change_table :gias_schools, bulk: true do |t|
+      t.string :group
+      t.string :type_of_establishment
+      t.string :phase
+      t.string :gender
+      t.integer :minimum_age
+      t.integer :maximum_age
+      t.string :religious_character
+      t.string :admissions_policy
+      t.string :urban_or_rural
+      t.integer :school_capacity
+      t.integer :total_pupils
+      t.integer :total_girls
+      t.integer :total_boys
+      t.integer :percentage_free_school_meals
+      t.string :special_classes
+      t.string :send_provision
+      t.string :training_with_disabilities
+      t.string :rating
+      t.date :last_inspection_date
+      t.string :email_address
+    end
+  end
+end
