008c398ed05e3ae11f2e39edce24dc9ada36e53e
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-01-04 15:58:34 +0000

    Allow support user to add a school in claims
    This uses the school.js autocomplete and pg search to search all
    GiasSchools and create a link between a School record and a GiasSchool
    in the claims domain.

--- config/routes/claims.rb
+++ config/routes/claims.rb
@@ -6,9 +6,12 @@ scope module: :claims, as: :claims, constraints: { host: ENV["CLAIMS_HOST"] } do
   namespace :support do
     root to: redirect("/support/schools")
 
-    resources :schools, only: %i[index show] do
+    resources :schools, except: %i[destroy update] do
+      collection { get :check }
+
       resources :claims
       resources :users, only: [:index]
     end
+    resources :school_suggestions, only: [:index]
   end
 end
