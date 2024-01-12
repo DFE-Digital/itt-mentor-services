619f55b3dfe7e35b9e0e92dac688b26d1ffef2be
Daniel Dye <daniel.dye@education.gov.uk>
2024-01-12 14:50:21 +0000

    Reorganise Claims controllers, views, and locales
    - Update locale files to 1 file per controller and use same nesting structure as controllers and views.

---
+++ app/controllers/claims/support/schools/claims_controller.rb
@@ -0,0 +1,11 @@
+class Claims::Support::Schools::ClaimsController < Claims::Support::ApplicationController
+  before_action :set_school, only: [:index]
+
+  def index; end
+
+  private
+
+  def set_school
+    @school = School.find(params.require(:school_id))
+  end
+end
--- config/routes/claims.rb
+++ config/routes/claims.rb
@@ -1,17 +1,29 @@
 scope module: :claims, as: :claims, constraints: { host: ENV["CLAIMS_HOST"] } do
-  root to: "pages#index"
+  root to: redirect("/schools")
 
-  resources :schools, only: %i[index show]
+  resources :schools, only: %i[index show] do
+    scope module: :schools do
+      resources :claims, only: %i[index show]
+      resources :users, only: %i[index show]
+    end
+  end
 
   namespace :support do
     root to: redirect("/support/schools")
 
-    resources :schools, except: %i[destroy update] do
-      collection { get :check }
+    resources :claims, only: %i[index show]
+    resources :users, only: %i[index show]
 
-      resources :claims
-      resources :users, only: %i[index new create show] do
-        collection { get :check }
+    resources :schools, except: %i[destroy update] do
+      get :check, on: :collection
+
+      scope module: :schools do
+        resources :claims
+        resources :mentors, only: %i[index]
+        resources :providers, only: %i[index]
+        resources :users, only: %i[index new create show] do
+          get :check, on: :collection
+        end
       end
     end
   end
