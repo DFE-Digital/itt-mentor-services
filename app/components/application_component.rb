69f72c7c10e8db43bc1f0dc96f9f87165cb06f46
Daniel Dye <daniel.dye@education.gov.uk>
2024-01-22 12:46:08 +0000

    Scope view component translations to "components/#{component_name}"
    This doubles down on the "components." prefix convention.

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,5 +1,7 @@
 class ApplicationComponent < GovukComponent::Base
   def initialize(classes: [], html_attributes: {})
+    @virtual_path = "components/#{self.class.name.underscore}"
+
     super(classes:, html_attributes:)
   end
 
