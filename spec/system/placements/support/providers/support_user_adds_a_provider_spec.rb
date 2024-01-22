885abcc2c5f08bfff676abccb94d410084973d63
Jamie <jamie.cleare@education.gov.uk>
2024-01-22 15:14:16 +0000

    Fix rspec since error handling change

--- spec/system/placements/support/providers/support_user_adds_a_provider_spec.rb
+++ spec/system/placements/support/providers/support_user_adds_a_provider_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe "Placements / Support / Providers / Support User adds a Provider"
   end
 
   def and_i_enter_a_provider_named(provider_name)
-    fill_in "provider-onboarding-form-code-field-error", with: provider_name
+    fill_in "provider-code-field-error", with: provider_name
   end
 
   def then_i_see_a_dropdown_item_for(provider_name)
