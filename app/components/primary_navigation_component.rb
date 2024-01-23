622062c7b6291ac9764c472cb47e8a82c5f9c591
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-01-23 13:57:13 +0000

    Add optional current attribute to primary navigation

--- app/components/primary_navigation_component.rb
+++ app/components/primary_navigation_component.rb
@@ -2,19 +2,26 @@ class PrimaryNavigationComponent < ApplicationComponent
   renders_many :navigation_items, "NavigationItemComponent"
 
   class NavigationItemComponent < ApplicationComponent
-    attr_reader :name, :url
+    attr_reader :name, :url, :current
 
-    def initialize(name, url, classes: [], html_attributes: {})
+    def initialize(name, url, current: false, classes: [], html_attributes: {})
       @name = name
       @url = url
+      @current = current
 
       super(classes:, html_attributes:)
     end
 
     def call
       content_tag(:li, class: "app-primary-navigation__item") do
-        link_to name, url, class: "app-primary-navigation__link", aria: { current: current_page?(url) && "page" }
+        link_to name, url, class: "app-primary-navigation__link", aria: { current: show_as_current?(url) && "page" }
       end
+    end
+
+    private
+
+    def show_as_current?(url)
+      current || current_page?(url)
     end
   end
 end
