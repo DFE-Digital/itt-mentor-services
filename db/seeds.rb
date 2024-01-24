5f3ea464e0097a80d7d6942a6c2fbb28bfaa27ce
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-01-24 16:26:19 +0000

    Fix seeds after renaming claims and placements on school

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ Rails.logger.debug "Personas successfully created!"
 Rake::Task["gias_update"].invoke unless School.any?
 
 School.last(2).each do |school|
-  school.update!(claims: true, placements: true)
+  school.update!(claims_service: true, placements_service: true)
 end
 
-School.first.update!(placements: true)
-School.second.update!(claims: true)
+School.first.update!(placements_service: true)
+School.second.update!(claims_service: true)
 
 Rails.logger.debug "Services added to schools"
 # Create Providers Imported from Publfish
@@ -28,14 +28,14 @@ Rake::Task["provider_data:import"].invoke unless Provider.any?
 User
   .where(first_name: %w[Anne Patricia])
   .find_each do |user|
-    school = School.public_send(user.service).first
+    school = School.where("#{user.service}_service": true).first
     user.memberships.find_or_create_by!(organisation: school)
   end
 
 User
   .where(first_name: %w[Mary Colin])
   .find_each do |user|
-    schools = School.where("#{user.service}": true)
+    schools = School.where("#{user.service}_service": true)
 
     schools.each do |school|
       user.memberships.find_or_create_by!(organisation: school)
