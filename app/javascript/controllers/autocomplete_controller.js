2de08df0e64cd1026febdfc160723a025e6d46ac
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-01-26 10:56:38 +0000

    Stimulus autocomplete
    Previously we had two autocompletes, one for schools and one for
    providers. These were also not using Stimulus.
    
    This commit ports these two into one autocomplete stimulus controller
    which should work with any model.

---
+++ app/javascript/controllers/autocomplete_controller.js
@@ -0,0 +1,49 @@
+import { Controller } from "@hotwired/stimulus"
+import accessibleAutocomplete from 'accessible-autocomplete'
+import debounce from "lodash.debounce";
+import { request } from "../utils/request_helper";
+
+// Connects to data-controller="autocomplete"
+export default class extends Controller {
+  static targets = [ "serverInput", "input" ]
+  static values = {
+    path: String,
+  }
+
+  connect() {
+    const minLength = 2;
+
+   accessibleAutocomplete({
+     element: this.inputTarget,
+     id: this.serverInputTarget.id,
+     showNoOptionsFound: true,
+     name: this.inputTarget.dataset.inputName,
+     minLength,
+     source: debounce(request(this.pathValue), 900),
+     templates: {
+       inputValue: this.inputValueTemplate.bind(this),
+       suggestion: this.resultTemplate.bind(this),
+     },
+     onConfirm: this.onConfirm.bind(this),
+     confirmOnBlur: false,
+     autoselect: true,
+   });
+
+    // Hijack the original input to submit the selected value.
+    this.serverInputTarget.type = "hidden";
+  }
+
+  private
+
+  inputValueTemplate(result) {
+    if (result) return result.name;
+  }
+
+  resultTemplate(result) {
+    if (result) return `${result.name} (${result.town} ${result.postcode})`;
+  }
+
+  onConfirm(option) {
+    this.serverInputTarget.value = option ? option.id : "";
+  }
+}
