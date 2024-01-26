9d6d88eae9ba2ef4d15c135274853494fa0761d0
Daniel Dye <daniel.dye@education.gov.uk>
2024-01-26 13:36:07 +0000

    Skip authenticate user for error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, :authenticate_user!
 
   def not_found
     render "not_found", status: :not_found
