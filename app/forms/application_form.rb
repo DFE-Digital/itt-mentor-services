9ff58ed304636e661f32a516c4f0c3fc337d5067
Daniel Dye <daniel.dye@education.gov.uk>
2024-01-31 17:26:42 +0000

    Update ApplicationForm to match documentation

--- app/forms/application_form.rb
+++ app/forms/application_form.rb
@@ -7,4 +7,25 @@ class ApplicationForm
 
     super
   end
+
+  # Defaults to doing nothing and signaling a successful save.
+  # Override this method in your subclass to implement your own persistence logic.
+  # Side effects like sending notifications should not be triggered from this method.
+  def persist
+    true
+  end
+
+  def save
+    validate ? persist : false
+  end
+
+  def save!
+    validate ? persist : raise_validation_error
+  end
+
+  private
+
+  def raise_validation_error
+    raise ActiveRecord::RecordInvalid, self
+  end
 end
