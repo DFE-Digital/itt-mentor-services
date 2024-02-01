c62cc980b9ca8e34e89b0035187b31b5b1550259
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-02-01 12:05:57 +0000

    Use the main gov uk notify account

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < Mail::Notify::Mailer
-  GENERIC_NOTIFY_TEMPLATE = "4181b9bb-b326-488d-9ff6-b7ea15158880".freeze
+  GENERIC_NOTIFY_TEMPLATE = "a2c0c8f0-6d15-47db-b334-5fe198da1740".freeze
 
   def notify_email(headers)
     headers.merge!(rails_mailer: mailer_name, rails_mail_template: action_name)
