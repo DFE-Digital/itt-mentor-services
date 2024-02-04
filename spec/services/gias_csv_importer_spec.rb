55f6682ec7127f8d447eff6c95ba2b677c61780a
George Schena <george.schena@education.gov.uk>
2024-02-04 18:54:07 +0000

    Update importer to associate_schools_to_regions

--- spec/services/gias_csv_importer_spec.rb
+++ spec/services/gias_csv_importer_spec.rb
@@ -4,18 +4,32 @@ RSpec.describe GiasCsvImporter do
   subject { described_class.call("spec/fixtures/test_gias_import.csv") }
 
   it "inserts the correct schools" do
-    expect { subject }.to change(School, :count).from(0).to(1)
+    expect { subject }.to change(School, :count).from(0).to(4)
   end
 
   it "updates the correct schools" do
     school = create(:school, urn: "123")
-    expect { subject }.to change { school.reload.name }.to "School"
+    expect { subject }.to change { school.reload.name }.to "FringeSchool"
   end
 
   it "logs messages to STDOUT" do
-    expect(Rails.logger).to receive(:info).with("Invalid rows - [\"Row 5 is invalid\"]")
+    expect(Rails.logger).to receive(:info).with("Invalid rows - [\"Row 8 is invalid\"]")
     expect(Rails.logger).to receive(:info).with("Done!")
 
     subject
+  end
+
+  it "associates schools to regions" do
+    subject
+
+    inner_london_school = School.find_by(urn: "130")
+    outer_london_school = School.find_by(urn: "131")
+    fringe_school = School.find_by(urn: "123")
+    rest_of_england_school = School.find_by(urn: "132")
+
+    expect(inner_london_school.region.name).to eq("Inner London")
+    expect(outer_london_school.region.name).to eq("Outer London")
+    expect(fringe_school.region.name).to eq("Fringe")
+    expect(rest_of_england_school.region.name).to eq("Rest of England")
   end
 end
