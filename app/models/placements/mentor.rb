b2e83ea9371b73c9c28e8d291a5753aede67d4d6
Daniel Dye <daniel.dye@education.gov.uk>
2024-02-12 17:00:17 +0000

    fixup! Add namespaced Mentor and MentorMembership models

--- app/models/placements/mentor.rb
+++ app/models/placements/mentor.rb
@@ -1,6 +1,26 @@
+# == Schema Information
+#
+# Table name: mentors
+#
+#  id         :uuid             not null, primary key
+#  first_name :string           not null
+#  last_name  :string           not null
+#  trn        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  school_id  :uuid             not null
+#
+# Indexes
+#
+#  index_mentors_on_school_id  (school_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (school_id => schools.id)
+#
 class Placements::Mentor < Mentor
   has_many :mentor_memberships
   has_many :schools, through: :mentor_memberships
 
-  default_scope { joins(:mentor_memberships).merge(Placements::MentorMembership.all) }
+  default_scope { joins(:mentor_memberships) }
 end
--- app/models/placements/mentor_membership.rb
+++ app/models/placements/mentor_membership.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: mentor_memberships
+#
+#  id         :uuid             not null, primary key
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  mentor_id  :uuid             not null
+#  school_id  :uuid             not null
+#
+# Indexes
+#
+#  index_mentor_memberships_on_mentor_id  (mentor_id)
+#  index_mentor_memberships_on_school_id  (school_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (mentor_id => mentors.id)
+#  fk_rails_...  (school_id => schools.id)
+#
 class Placements::MentorMembership < MentorMembership
   belongs_to :mentor
   belongs_to :school
