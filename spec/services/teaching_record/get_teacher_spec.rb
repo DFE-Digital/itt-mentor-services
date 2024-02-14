7330320168d39b142cbc561dcad3859df8649ba4
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-02-14 13:53:09 +0000

    improve readability of stub requests

--- spec/services/teaching_record/get_teacher_spec.rb
+++ spec/services/teaching_record/get_teacher_spec.rb
@@ -4,24 +4,7 @@ RSpec.describe TeachingRecord::GetTeacher do
   context "with invalid trn" do
     subject(:get_teacher) { described_class.call(trn: "2222222") }
 
-    before do
-      stub_request(:get, "https://preprod.teacher-qualifications-api.education.gov.uk/v3/teachers/2222222")
-        .with(
-          headers: {
-            "Accept" => "application/json",
-            "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-            "Authorization" => "Bearer secret",
-            "Content-Type" => "application/json;odata.metadata=minimal",
-            "User-Agent" => "Ruby",
-            "X-Api-Version" => "20240101",
-          },
-        )
-        .to_return(
-          status: 404,
-          body: "{\"type\":\"https://tools.ietf.org/html/rfc9110#section-15.5.5\",\"title\":\"Not Found\",\"status\":404,\"traceId\":\"00-dff9d2243466591e882b480c8bdbfc27-f60a1ced105d1602-00\"}",
-          headers: {},
-        )
-    end
+    before { failure_stub_request }
 
     it "raises error" do
       expect { get_teacher }.to raise_error(TeachingRecord::RestClient::HttpError)
@@ -31,24 +14,7 @@ RSpec.describe TeachingRecord::GetTeacher do
   context "with valid trn" do
     subject(:get_teacher) { described_class.call(trn: "1234567") }
 
-    before do
-      stub_request(:get, "https://preprod.teacher-qualifications-api.education.gov.uk/v3/teachers/1234567")
-        .with(
-          headers: {
-            "Accept" => "application/json",
-            "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-            "Authorization" => "Bearer secret",
-            "Content-Type" => "application/json;odata.metadata=minimal",
-            "User-Agent" => "Ruby",
-            "X-Api-Version" => "20240101",
-          },
-        )
-        .to_return(
-          status: 200,
-          body: "{\"trn\":\"1234567\",\"firstName\":\"Judith\",\"middleName\":\"\",\"lastName\":\"Chicken\",\"dateOfBirth\":\"1991-01-22\",\"nationalInsuranceNumber\":\"B15J60R13\",\"email\":\"anonymous@anonymousdomain.org.net.co.uk\",\"qts\":null,\"eyts\":null}",
-          headers: {},
-        )
-    end
+    before { success_stub_request }
 
     it "returns teacher details" do
       teacher = get_teacher
@@ -64,5 +30,43 @@ RSpec.describe TeachingRecord::GetTeacher do
         "eyts" => nil,
       })
     end
+  end
+
+  def success_stub_request
+    stub_request(:get, "https://preprod.teacher-qualifications-api.education.gov.uk/v3/teachers/1234567")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+          "Authorization" => "Bearer secret",
+          "Content-Type" => "application/json;odata.metadata=minimal",
+          "User-Agent" => "Ruby",
+          "X-Api-Version" => "20240101",
+        },
+      )
+      .to_return(
+        status: 200,
+        body: "{\"trn\":\"1234567\",\"firstName\":\"Judith\",\"middleName\":\"\",\"lastName\":\"Chicken\",\"dateOfBirth\":\"1991-01-22\",\"nationalInsuranceNumber\":\"B15J60R13\",\"email\":\"anonymous@anonymousdomain.org.net.co.uk\",\"qts\":null,\"eyts\":null}",
+        headers: {},
+      )
+  end
+
+  def failure_stub_request
+    stub_request(:get, "https://preprod.teacher-qualifications-api.education.gov.uk/v3/teachers/2222222")
+      .with(
+        headers: {
+          "Accept" => "application/json",
+          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+          "Authorization" => "Bearer secret",
+          "Content-Type" => "application/json;odata.metadata=minimal",
+          "User-Agent" => "Ruby",
+          "X-Api-Version" => "20240101",
+        },
+      )
+      .to_return(
+        status: 404,
+        body: "{\"type\":\"https://tools.ietf.org/html/rfc9110#section-15.5.5\",\"title\":\"Not Found\",\"status\":404,\"traceId\":\"00-dff9d2243466591e882b480c8bdbfc27-f60a1ced105d1602-00\"}",
+        headers: {},
+      )
   end
 end
