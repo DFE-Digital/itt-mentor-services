621e3b1ee58d150a1bb0f95a74e73b2b7c3afe1c
George Schena <george.schena@education.gov.uk>
2024-02-14 08:49:44 +0000

    Increase code coverage for application helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe ApplicationHelper do
+  describe "#external_link" do
+    context "when value is blank" do
+      it "returns nil" do
+        value = ""
+        expect(external_link(value)).to eq(nil)
+      end
+    end
+
+    context "when value does not have a protocol" do
+      it "returns just the correct url" do
+        value = "www.google.com"
+        expect(external_link(value)).to eq("http://www.google.com")
+      end
+    end
+
+    context "when value already has http" do
+      it "returns just the correct url" do
+        value = "http://www.google.com"
+        expect(external_link(value)).to eq("http://www.google.com")
+      end
+    end
+
+    context "when value already has https" do
+      it "returns just the correct url" do
+        value = "https://www.google.com"
+        expect(external_link(value)).to eq("https://www.google.com")
+      end
+    end
+  end
+end
