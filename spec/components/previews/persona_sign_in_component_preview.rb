1ee80e29b2f95b7f86d908e1e653fd285d6f3bda
Jamie <jamie.cleare@education.gov.uk>
2024-02-15 09:41:10 +0000

    Move structure of preview templates

--- spec/components/previews/persona_sign_in_component_preview.rb
+++ spec/components/previews/persona_sign_in_component_preview.rb
@@ -1,35 +1,27 @@
-require "factory_bot"
-
-class PersonaSignInComponentPreview < ViewComponent::Preview
+class PersonaSignInComponentPreview < ApplicationComponentPreview
   def user_anne
     persona = FactoryBot.build(:placements_support_user, :anne)
-    render_with_template(
-      template: "persona_sign_in_component_preview/default",
-      locals: { persona: },
-    )
+    render_with_template(template:, locals: { persona: })
   end
 
   def user_mary
     persona = FactoryBot.build(:placements_user, :mary)
-    render_with_template(
-      template: "persona_sign_in_component_preview/default",
-      locals: { persona: },
-    )
+    render_with_template(template:, locals: { persona: })
   end
 
   def user_patricia
     persona = FactoryBot.build(:placements_user, :patricia)
-    render_with_template(
-      template: "persona_sign_in_component_preview/default",
-      locals: { persona: },
-    )
+    render_with_template(template:, locals: { persona: })
   end
 
   def support_user_colin
     persona = FactoryBot.build(:placements_support_user, :colin)
-    render_with_template(
-      template: "persona_sign_in_component_preview/default",
-      locals: { persona: },
-    )
+    render_with_template(template:, locals: { persona: })
+  end
+
+  private
+
+  def template
+    "templates/persona_sign_in_component_preview"
   end
 end
