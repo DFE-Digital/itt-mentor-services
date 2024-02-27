714a013c89accbd6d837087acc493a3b61ac93f3
Jamie <jamie.cleare@education.gov.uk>
2024-02-27 17:08:05 +0000

    Same changes from prototype update

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -89,4 +89,26 @@ RSpec.describe School, type: :model do
     it { is_expected.to validate_presence_of(:urn) }
     it { is_expected.to validate_uniqueness_of(:urn).case_insensitive }
   end
+
+  describe "#primary_or_secondary_only?" do
+    subject { school.primary_or_secondary_only? }
+
+    context "when given a school with phase Primary" do
+      let(:school) { create(:school, phase: "Primary") }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context "when given a school with phase Secondary" do
+      let(:school) { create(:school, phase: "Secondary") }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context "when given a school with phase not Primary or Secondary" do
+      let(:school) { create(:school, phase: "All-through") }
+
+      it { is_expected.to eq(false) }
+    end
+  end
 end
