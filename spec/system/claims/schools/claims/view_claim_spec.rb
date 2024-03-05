4e0edcd3ed1c1fa4acba47719275de5f7a4c5bef
George Schena <george.schena@education.gov.uk>
2024-03-05 14:24:19 +0000

    As a User, I should be able to view the details of a Claim

---
+++ spec/system/claims/schools/claims/view_claim_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+RSpec.describe "View a claim", type: :system, service: :claims do
+  let(:school) { create(:claims_school, name: "A School") }
+
+  let(:anne) do
+    create(
+      :claims_user,
+      :anne,
+      user_memberships: [create(:user_membership, organisation: school)],
+    )
+  end
+
+  let!(:provider) { create(:provider, :best_practice_network) }
+  let!(:claims_mentor) { create(:claims_mentor, first_name: "Barry", last_name: "Garlow") }
+
+  let!(:claim) { create(:claim, school:, reference: "12345678", submitted_at: Time.new(2024, 3, 5, 12, 31, 52, "+00:00"), provider:) }
+  let!(:mentor_training) { create(:mentor_training, claim:, mentor: claims_mentor, hours_completed: 6) }
+
+  scenario "Anne visits the claims index page with a submited" do
+    user_exists_in_dfe_sign_in(user: anne)
+    given_i_sign_in
+    when_i_visit_the_claim_show_page
+    then_i_can_then_see_the_claim_details
+  end
+
+  private
+
+  def when_i_visit_the_claim_show_page
+    click_on claim.reference
+  end
+
+  def then_i_can_then_see_the_claim_details
+    expect(page).to have_content("Claim - 12345678")
+    expect(page).to have_content("SchoolA School")
+    expect(page).to have_content("ProviderBest Practice Network")
+    expect(page).to have_content("Mentors\nBarry Garlow")
+    expect(page).to have_content("Date submitted 5 March 2024")
+    expect(page).to have_content("Hours of training")
+    expect(page).to have_content("Status\nSubmitted")
+    expect(page).to have_content("Barry Garlow#{mentor_training.hours_completed} hours")
+  end
+
+  def given_i_sign_in
+    visit sign_in_path
+    click_on "Sign in using DfE Sign In"
+  end
+end
