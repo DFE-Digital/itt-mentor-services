dd2460f59c7755bd50f0bc32f5cf6148f1acde3d
George Schena <george.schena@education.gov.uk>
2024-03-06 16:24:35 +0000

    Fix failing test due to region changes

--- spec/system/claims/schools/claims/view_claim_spec.rb
+++ spec/system/claims/schools/claims/view_claim_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe "View a claim", type: :system, service: :claims do
     expect(page).to have_content("Hours of training")
     expect(page).to have_content("Status\nSubmitted")
     expect(page).to have_content("Barry Garlow#{mentor_training.hours_completed} hours")
-    expect(page).to have_content("Claim amount£321.60")
+    expect(page).to have_content("Claim amount£289.50")
   end
 
   def given_i_sign_in
