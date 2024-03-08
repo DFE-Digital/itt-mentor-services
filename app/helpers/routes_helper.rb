99960ee37a2bafe4be75cc9e1784580dc04d571b
Ben <16797406+Kizr@users.noreply.github.com>
2024-03-08 20:45:52 +0000

    Update the routes_helper.rb and routes_helper_spec.rb
    https://trello.com/c/psXhmCl9
    
    Removes the loopy logic where we were accessing a `current_service` due the inclusion of `ApplicationHelper` via `ApplicationController`. This made the code hard to test and generally smelled a little. We now call `HostingService` directly removing this dependency.
    
    This change facilitated the need to rewrite the spec file, which is included in this commit.

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -5,42 +5,42 @@ module RoutesHelper
     {
       claims: sign_in_path,
       placements: placements_root_path,
-    }.fetch current_service
+    }.fetch HostingEnvironment.current_service(request)
   end
 
   def support_root_path
     {
       claims: claims_support_root_path,
       placements: placements_support_root_path,
-    }.fetch current_service
+    }.fetch HostingEnvironment.current_service(request)
   end
 
   def support_organisations_path
     {
       claims: claims_support_schools_path,
       placements: placements_support_organisations_path,
-    }.fetch current_service
+    }.fetch HostingEnvironment.current_service(request)
   end
 
   def support_support_users_path
     {
       claims: claims_support_support_users_path,
       placements: placements_support_support_users_path,
-    }.fetch current_service
+    }.fetch HostingEnvironment.current_service(request)
   end
 
   def organisations_path
     {
       claims: claims_schools_path,
       placements: placements_organisations_path,
-    }.fetch current_service
+    }.fetch HostingEnvironment.current_service(request)
   end
 
   def feedback_path
     {
       claims: claims_feedback_path,
       placements: placements_feedback_path,
-    }.fetch current_service
+    }.fetch HostingEnvironment.current_service(request)
   end
 
   def omniauth_sign_in_path(provider)
