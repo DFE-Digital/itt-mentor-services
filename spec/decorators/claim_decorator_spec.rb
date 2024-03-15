36b18632437af59d6e5673de06444ae8cbe06809
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-03-15 18:01:44 +0000

    Extend the `ClaimDecorator` spec
    https://trello.com/c/00ckRT1T
    
    Adds more tests to complete code coverage for this class.

--- spec/decorators/claim_decorator_spec.rb
+++ spec/decorators/claim_decorator_spec.rb
@@ -2,13 +2,26 @@ require "rails_helper"
 
 RSpec.describe ClaimDecorator do
   describe "item_status_tag" do
-    it "returns the completed status tag for a claim item" do
-      school = create(:claims_school)
-      claim = create(:claim, school:, provider: create(:provider))
+    context "when the claim association is present" do
+      it "returns the completed status tag for a claim item" do
+        school = build(:claims_school)
+        claim = build(:claim, school:, provider: create(:provider))
 
-      expect(claim.decorate.item_status_tag("provider")).to eq(
-        { text: "Completed", colour: "green" },
-      )
+        expect(claim.decorate.item_status_tag("provider")).to eq(
+          { text: "Completed", colour: "green" },
+        )
+      end
+    end
+
+    context "when the claim association is not present" do
+      it "returns the not started status tag for a claim item" do
+        school = build(:claims_school)
+        claim = build(:claim, school:, provider: nil)
+
+        expect(claim.decorate.item_status_tag("provider")).to eq(
+          { text: "Not started", colour: "grey" },
+        )
+      end
     end
   end
 end
