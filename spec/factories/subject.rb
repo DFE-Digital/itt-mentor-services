45cd6e887d1dc8d941f8c50f6b76cc506dc06a55
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-03-18 13:02:22 +0000

    Update subjects to use symbols instead of strings
    https://trello.com/c/k7B8irib
    
    Finds all instances where strings were used for specifying enumerable values and converts them to symbols.

--- spec/factories/subject.rb
+++ spec/factories/subject.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :subject do
-    subject_area { %w[primary secondary].sample }
+    subject_area { %i[primary secondary].sample }
     name { Faker::Educator.subject }
     code { Faker::Alphanumeric.alpha(number: 2) }
   end
 
   trait :primary do
-    subject_area { "primary" }
+    subject_area { :primary }
   end
 
   trait :secondary do
-    subject_area { "secondary" }
+    subject_area { :secondary }
   end
 end
