3805d1ed6a3ce3e36b019325104a0a128d90b458
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-03-18 17:16:32 +0000

    Match the decorator subject text with mentor text
    https://trello.com/c/00ckRT1T
    
    Adds "Not known yet" when there are no subjects found to bring the logic in line with the mentor names logic and adds the missing specs to fully test these methods.

--- spec/decorators/placement_decorator_spec.rb
+++ spec/decorators/placement_decorator_spec.rb
@@ -10,22 +10,58 @@ RSpec.describe PlacementDecorator do
       end
     end
 
-    it "returns a list of mentor names" do
-      placement = build(:placement, mentors: [
-        build(:placements_mentor, first_name: "John", last_name: "Doe")
-      ])
+    context "when the placement has one mentor" do
+      it "returns a list of mentor names" do
+        placement = build(:placement, mentors: [
+          build(:placements_mentor, first_name: "John", last_name: "Doe"),
+        ])
 
-      expect(placement.decorate.mentor_names).to eq(
-        "John Doe",
-      )
+        expect(placement.decorate.mentor_names).to eq(
+          "John Doe",
+        )
+      end
+    end
+
+    context "when the placement has multiple mentors" do
+      it "returns a list of mentor names in alphabetical order" do
+        placement = build(:placement, mentors: [
+          build(:placements_mentor, first_name: "John", last_name: "Doe"),
+          build(:placements_mentor, first_name: "Jane", last_name: "Doe"),
+        ])
+
+        expect(placement.decorate.mentor_names).to eq(
+          "Jane Doe and John Doe",
+        )
+      end
     end
   end
 
   describe "#subject_names" do
-    it "returns a list of subject names" do
-      placement = create(:placement, subjects: [build(:subject, name: "Maths")])
+    context "when the placement has no subjects" do
+      it "returns Not known yet" do
+        placement = build(:placement)
 
-      expect(placement.decorate.subject_names).to eq("Maths")
+        expect(placement.decorate.subject_names).to eq("Not known yet")
+      end
+    end
+
+    context "when the placement has one subject" do
+      it "returns a list of subject names" do
+        placement = create(:placement, subjects: [build(:subject, name: "Maths")])
+
+        expect(placement.decorate.subject_names).to eq("Maths")
+      end
+    end
+
+    context "when the placement has multiple subjects" do
+      it "returns a list of subject names in alphabetical order" do
+        placement = create(:placement, subjects: [
+          build(:subject, name: "Maths"),
+          build(:subject, name: "English"),
+        ])
+
+        expect(placement.decorate.subject_names).to eq("English and Maths")
+      end
     end
   end
 
