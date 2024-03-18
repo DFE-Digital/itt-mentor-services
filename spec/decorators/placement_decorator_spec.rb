af5b0629f3791550d761f050bd75ea3e54a8ec0a
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-03-18 17:07:57 +0000

    Improve factory creation
    https://trello.com/c/00ckRT1T
    
    Refactored the factory creation to be more efficient and replaced `create` with `build` where possible.

--- spec/decorators/placement_decorator_spec.rb
+++ spec/decorators/placement_decorator_spec.rb
@@ -4,21 +4,16 @@ RSpec.describe PlacementDecorator do
   describe "#mentor_names" do
     context "when the placement has no mentors" do
       it "returns Not known yet" do
-        placement = create(:placement)
+        placement = build(:placement)
 
         expect(placement.decorate.mentor_names).to eq("Not known yet")
       end
     end
 
     it "returns a list of mentor names" do
-      placement = create(:placement)
-      mentor = create(:placements_mentor, first_name: "John", last_name: "Doe")
-      create(
-        :mentor_membership,
-        :placements,
-        mentor:,
-      )
-      PlacementMentorJoin.create!(placement:, mentor:)
+      placement = build(:placement, mentors: [
+        build(:placements_mentor, first_name: "John", last_name: "Doe")
+      ])
 
       expect(placement.decorate.mentor_names).to eq(
         "John Doe",
@@ -28,9 +23,7 @@ RSpec.describe PlacementDecorator do
 
   describe "#subject_names" do
     it "returns a list of subject names" do
-      placement = create(:placement)
-      subject = create(:subject, name: "Maths")
-      PlacementSubjectJoin.create!(placement:, subject:)
+      placement = create(:placement, subjects: [build(:subject, name: "Maths")])
 
       expect(placement.decorate.subject_names).to eq("Maths")
     end
@@ -38,9 +31,7 @@ RSpec.describe PlacementDecorator do
 
   describe "#school_level" do
     it "returns the school level" do
-      placement = create(:placement)
-      subject = create(:subject, subject_area: "primary")
-      PlacementSubjectJoin.create!(placement:, subject:)
+      placement = build(:placement, subjects: [build(:subject, :primary)])
 
       expect(placement.decorate.school_level).to eq("Primary")
     end
@@ -49,7 +40,7 @@ RSpec.describe PlacementDecorator do
   describe "#window" do
     context "when the window is autumn" do
       it "returns autumn" do
-        placement = create(:placement, start_date: "2020-09-01", end_date: "2020-12-31")
+        placement = build(:placement, start_date: "2020-09-01", end_date: "2020-12-31")
 
         expect(placement.decorate.window).to eq("Autumn")
       end
@@ -57,7 +48,7 @@ RSpec.describe PlacementDecorator do
 
     context "when the window is spring" do
       it "returns spring" do
-        placement = create(:placement, start_date: "2020-01-01", end_date: "2020-03-31")
+        placement = build(:placement, start_date: "2020-01-01", end_date: "2020-03-31")
 
         expect(placement.decorate.window).to eq("Spring")
       end
@@ -65,7 +56,7 @@ RSpec.describe PlacementDecorator do
 
     context "when the window is summer" do
       it "returns summer" do
-        placement = create(:placement, start_date: "2020-04-01", end_date: "2020-07-31")
+        placement = build(:placement, start_date: "2020-04-01", end_date: "2020-07-31")
 
         expect(placement.decorate.window).to eq("Summer")
       end
@@ -73,7 +64,7 @@ RSpec.describe PlacementDecorator do
 
     context "when the window is not autumn, spring or summer" do
       it "returns the window date" do
-        placement = create(:placement, start_date: "2020-02-01", end_date: "2020-03-31")
+        placement = build(:placement, start_date: "2020-02-01", end_date: "2020-03-31")
 
         expect(placement.decorate.window).to eq("February to March")
       end
@@ -82,7 +73,7 @@ RSpec.describe PlacementDecorator do
 
   describe "#formatted_start_date" do
     it "returns the formatted start date" do
-      placement = create(:placement, start_date: "2020-09-01")
+      placement = build(:placement, start_date: "2020-09-01")
 
       expect(placement.decorate.formatted_start_date).to eq(" 1 September 2020")
     end
