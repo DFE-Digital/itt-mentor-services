4357ad1f530feed606b74ecdfa58e86bec26286d
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-03-18 16:01:07 +0000

    Claim should belong to a Claims::School

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -26,7 +26,7 @@
 #  fk_rails_...  (school_id => schools.id)
 #
 class Claim < ApplicationRecord
-  belongs_to :school
+  belongs_to :school, class_name: "Claims::School"
   belongs_to :provider
   belongs_to :created_by, polymorphic: true
 
