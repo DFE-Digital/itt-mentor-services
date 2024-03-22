1a3e9675fe42b6e78099dd74b67dea03d31f8f71
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-03-22 14:18:58 +0000

    Refactor footer helper to accommodate both services

---
+++ spec/helpers/footer_helper_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe FooterHelper do
+  before { helper.instance_variable_set(:@virtual_path, "layouts/footer") }
+
+  describe "#footer_meta_items" do
+    context "when in claims" do
+      it "returns the correct footer meta items" do
+        allow(HostingEnvironment).to receive(:current_service).and_return(:claims)
+        expect(helper.footer_meta_items).to eq(
+          [
+            { href: "/grant_conditions", text: "Grant conditions" },
+            { href: "/accessibility", text: "Accessibility" },
+            { href: "/cookies", text: "Cookies" },
+            { href: "/privacy", text: "Privacy policy" },
+            { href: "/terms", text: "Terms and conditions" },
+          ],
+        )
+      end
+    end
+
+    context "when in placements" do
+      it "returns the correct footer meta items" do
+        allow(HostingEnvironment).to receive(:current_service).and_return(:placements)
+        expect(helper.footer_meta_items).to eq(
+          [
+            { href: "#", text: "Guidance" },
+            { href: "#", text: "Accessibility" },
+            { href: "#", text: "Cookies" },
+            { href: "#", text: "Privacy policy" },
+            { href: "#", text: "Terms and conditions" },
+          ],
+        )
+      end
+    end
+  end
+end
