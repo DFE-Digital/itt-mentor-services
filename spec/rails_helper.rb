eca63dcae3de119b4c12dba105e5a3437bae8fb2
Daniel Dye <daniel.dye@education.gov.uk>
2024-03-26 14:17:33 +0000

    Don't setup fixtures when running smoke tests
    The smoke tests should not need access to a database. Fixtures tap into pre-filling data in a database. Without a database, this fails, even with the nulldb adapter.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,7 +47,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [Rails.root.join("spec/fixtures")]
 
-  config.global_fixtures = :all
+  config.global_fixtures = :all unless ENV["SMOKE_TEST"] == "true"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
