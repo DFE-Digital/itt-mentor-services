26516756c1425eb0ea590213a29fe6700e5b10e9
Ollie Treend <ollie@treend.uk>
2024-04-09 10:43:22 +0100

    Add a high-level test for Gias::SyncAllSchoolsJob
    I've added a high-level integration test for this job because I intend
    to split & refactor the underlying service objects, and I want to make
    sure I don't break the overall GIAS import feature.
    
    This tests the end to end happy path, starting with the GIAS CSV file
    being downloaded, and ending with schools existing in the database.

--- spec/jobs/gias/sync_all_schools_job_spec.rb
+++ spec/jobs/gias/sync_all_schools_job_spec.rb
@@ -17,4 +17,28 @@ RSpec.describe Gias::SyncAllSchoolsJob, type: :job do
       described_class.perform_now
     end
   end
+
+  describe "integration test" do
+    let(:gias_csv_url) do
+      today = Time.zone.today.strftime("%Y%m%d")
+      base_url = ENV.fetch("GIAS_CSV_BASE_URL")
+      "#{base_url}/edubasealldata#{today}.csv"
+    end
+
+    let(:gias_csv_file) { File.open("spec/fixtures/gias/gias_subset.csv") }
+
+    before do
+      stub_request(:get, gias_csv_url).to_return(status: 200, body: gias_csv_file)
+    end
+
+    it "downloads and imports school data from GIAS" do
+      expect { described_class.perform_now }.to change(School, :count).from(0).to(3)
+
+      expect(School.pluck(:urn, :name)).to eq [
+        ["100000", "The Aldgate School"],
+        ["137666", "Chudleigh Knighton Church of England Primary School"],
+        ["124087", "Thomas Barnes Primary School"],
+      ]
+    end
+  end
 end
