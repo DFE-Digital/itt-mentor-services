fb7f3f963f944b8b0e57ef45e8b05196940860f3
Jamie <jamie.cleare@education.gov.uk>
2024-04-09 17:20:29 +0100

    Add autocomplete select component
    Locales for autocomplete select form component
    
    more locales

---
+++ spec/components/autocomplete_select_form_component_spec.rb
@@ -0,0 +1,139 @@
+require "rails_helper"
+
+RSpec.describe AutocompleteSelectFormComponent, type: :component do
+  subject(:component) do
+    described_class.new(
+      model:,
+      scope:,
+      url:,
+      back_link:,
+      data:,
+      input:,
+    )
+  end
+
+  context "with default attributes" do
+    let(:model) { User.new }
+    let(:scope) { :form }
+    let(:url) { "" }
+    let(:back_link) { "" }
+    let(:input) { {} }
+
+    describe "data attribute validation" do
+      context "when not given a autocomplete_path_value attribute" do
+        let(:data) { {} }
+
+        it "raises an error" do
+          expect { component }.to raise_error(
+            InvalidComponentError,
+            "data[:autocomplete_path_value] can't be blank.",
+          )
+        end
+      end
+
+      context "when not given a autocomplete_return_attributes_value attribute" do
+        let(:data) { { autocomplete_path_value: "#" } }
+
+        it "raises an error" do
+          expect { component }.to raise_error(
+            InvalidComponentError,
+            "data[:autocomplete_return_attributes_value] can't be blank.",
+          )
+        end
+      end
+    end
+
+    context "when given sufficent data attributes" do
+      let(:data) do
+        { autocomplete_path_value: "#", autocomplete_return_attributes_value: [:value] }
+      end
+
+      it "returns an autocomplete select form" do
+        render_inline(component)
+
+        expect(page.find(".govuk-label")).to have_content("Label")
+        expect(page.find(".govuk-caption-l")).to have_content("Caption")
+        expect(page).to have_field("form-id-field")
+        page.find("div[data-input-name='form[name]']")
+      end
+    end
+  end
+
+  context "with school onboarding attributes" do
+    let(:model) { SchoolOnboardingForm.new }
+    let(:scope) { :school }
+    let(:url) { "" }
+    let(:back_link) { "" }
+    let(:data) do
+      {
+        turbo: false,
+        controller: "autocomplete",
+        autocomplete_path_value: "/api/school_suggestions",
+        autocomplete_return_attributes_value: %w[town postcode],
+        input_name: "school[name]",
+      }
+    end
+    let(:input) do
+      {
+        field_name: :id,
+        value: nil,
+        label: "Enter a school name, URN or postcode",
+        caption: "Add organisation",
+        previous_search: nil,
+      }
+    end
+
+    it "returns an autocomplete select form for onboarding schools" do
+      render_inline(component)
+
+      page.find(
+        "form[data-controller='autocomplete']" \
+        "[data-autocomplete-path-value='/api/school_suggestions']" \
+        "[data-autocomplete-return-attributes-value='[\"town\",\"postcode\"]']",
+      )
+      expect(page.find(".govuk-label")).to have_content("Enter a school name, URN or postcode")
+      expect(page.find(".govuk-caption-l")).to have_content("Add organisation")
+      expect(page).to have_field("school-id-field")
+      page.find("div[data-input-name='school[name]']")
+    end
+  end
+
+  context "with provider onboarding attributes" do
+    let(:model) { ProviderOnboardingForm.new }
+    let(:scope) { :provider }
+    let(:url) { "" }
+    let(:back_link) { "" }
+    let(:data) do
+      {
+        turbo: false,
+        controller: "autocomplete",
+        autocomplete_path_value: "/api/provider_suggestions",
+        autocomplete_return_attributes_value: %w[code],
+        input_name: "provider[name]",
+      }
+    end
+    let(:input) do
+      {
+        field_name: :id,
+        value: nil,
+        label: "Enter a provider name, UKPRN, URN or postcode",
+        caption: "Add organisation",
+        previous_search: nil,
+      }
+    end
+
+    it "returns an autocomplete select form for onboarding schools" do
+      render_inline(component)
+
+      page.find(
+        "form[data-controller='autocomplete']" \
+        "[data-autocomplete-path-value='/api/provider_suggestions']" \
+        "[data-autocomplete-return-attributes-value='[\"code\"]']",
+      )
+      expect(page.find(".govuk-label")).to have_content("Enter a provider name, UKPRN, URN or postcode")
+      expect(page.find(".govuk-caption-l")).to have_content("Add organisation")
+      expect(page).to have_field("provider-id-field")
+      page.find("div[data-input-name='provider[name]']")
+    end
+  end
+end
---
+++ spec/components/previews/autocomplete_select_form_component_preview.rb
@@ -0,0 +1,47 @@
+class AutocompleteSelectFormComponentPreview < ApplicationComponentPreview
+  def school_onboarding_form
+    render AutocompleteSelectFormComponent.new(
+      model: SchoolOnboardingForm.new,
+      scope: :school,
+      url: "#",
+      back_link: "#",
+      data: {
+        turbo: false,
+        controller: "autocomplete",
+        autocomplete_path_value: "/api/school_suggestions",
+        autocomplete_return_attributes_value: %w[town postcode],
+        input_name: "school[name]",
+      },
+      input: {
+        field_name: :id,
+        value: nil,
+        label: "Enter a school name, URN or postcode",
+        caption: "Add organisation",
+        previous_search: nil,
+      },
+    )
+  end
+
+  def provider_onboarding_form
+    render AutocompleteSelectFormComponent.new(
+      model: ProviderOnboardingForm.new,
+      scope: :provider,
+      url: "#",
+      back_link: "#",
+      data: {
+        turbo: false,
+        controller: "autocomplete",
+        autocomplete_path_value: "/api/provider_suggestions",
+        autocomplete_return_attributes_value: %w[code],
+        input_name: "provider[name]",
+      },
+      input: {
+        field_name: :id,
+        value: nil,
+        label: "Enter a provider name, UKPRN, URN or postcode",
+        caption: "Add organisation",
+        previous_search: nil,
+      },
+    )
+  end
+end
