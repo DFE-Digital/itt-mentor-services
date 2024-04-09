c4fc0e953826910bbf61c96e5dc4abc332ef63e7
Ollie Treend <ollie@treend.uk>
2024-04-09 16:34:11 +0100

    Transform Easting/Northing coordinates to Lat/Long
    I've updated the `Gias::CsvTransformer` to convert the Easting/Northing
    values provided by GIAS into Latitude/Longitude coordinates that can be
    used for location-based search.
    
    Previously we were using a geocoding service to get the
    Latitude/Longitude coordinates for schools based on their address.
    However this requires N+1 external API calls (one for each school) which
    is both slow and costly.
    
    Turns out it's much more efficient to just make use of the geolocation
    data GIAS already provides. It's a bit fiddly to perform the translation
    because it requires an external library called [PROJ][1].  However once
    that's installed it's pretty straightforward.
    
    I would have liked to have used the gem [proj4rb][2] which is a Ruby
    binding for PROJ. However, for [unexplained reasons][3], I couldn't
    get that gem to work correctly. So instead I'm calling out to a command
    line app that PROJ provides called [cs2cs][4], which converts coordinates
    from one 'coordinate reference system' to another.
    
    In my testing, this performs surprisingly well. Even though we need to
    call out to an external process, it will easily convert the coordinates
    of 20,000 schools in ~200ms. Not too shabby!
    
    [1]: https://proj.org
    [2]: https://github.com/cfis/proj4rb
    [3]: https://github.com/cfis/proj4rb/issues/23
    [4]: https://proj.org/en/9.4/apps/cs2cs.html

--- app/services/gias/csv_transformer.rb
+++ app/services/gias/csv_transformer.rb
@@ -11,10 +11,15 @@ module Gias
       output_csv = CSV.new(output_file)
 
       CSV.new(input_file, headers: true, return_headers: true).each do |row|
-        if row.header_row? || school_in_scope?(row)
-          output_csv << row
+        if row.header_row?
+          output_csv << header_row(row)
+        elsif school_in_scope?(row)
+          output_csv << school_row(row)
         end
       end
+
+      # Close the IO handle to the cs2cs process
+      coordinate_transformer.close
 
       # Rewind the file so it's ready for reading
       output_file.rewind
@@ -24,6 +29,23 @@ module Gias
     private
 
     attr_reader :input_file, :output_file
+
+    def header_row(row)
+      row.headers + %w[Latitude Longitude]
+    end
+
+    def school_row(row)
+      coordinates = coordinate_transformer.transform(
+        easting: row.fetch("Easting"),
+        northing: row.fetch("Northing"),
+      )
+
+      row.fields + [coordinates[:latitude], coordinates[:longitude]]
+    end
+
+    def coordinate_transformer
+      @coordinate_transformer ||= CoordinateTransformer.new
+    end
 
     # The 'School Placements' and 'Funding Mentors' services only need to know
     # about schools in England. Closed schools and those outside of England can
@@ -49,6 +71,35 @@ module Gias
 
       def in_england?
         !NON_ENGLISH_ESTABLISHMENTS.include? row.fetch("TypeOfEstablishment (code)")
+      end
+    end
+
+    class CoordinateTransformer
+      # GIAS provides coordinates in British National Grid Easting/Northing format.
+      # https://epsg.io/27700
+      SOURCE_CRS = "EPSG:27700".freeze
+
+      # We need coordinates in WGS84 format, the standard Latitude/Longitude
+      # coordinate system used in GPS and online mapping tools.
+      # https://epsg.io/4326
+      TARGET_CRS = "EPSG:4326".freeze
+
+      attr_reader :cs2cs
+
+      delegate :close, to: :cs2cs
+
+      def initialize
+        # cs2cs is provided as part of the PROJ library
+        # Mac: brew install proj
+        # Linux (Debian): apt-get install proj-bin
+        # Usage: https://proj.org/en/9.4/apps/cs2cs.html
+        @cs2cs = IO.popen(["cs2cs", "-d", "10", SOURCE_CRS, TARGET_CRS], "r+")
+      end
+
+      def transform(easting:, northing:)
+        cs2cs.write "#{easting} #{northing}\n"
+        output = cs2cs.gets.split(" ")
+        { latitude: output[0], longitude: output[1] }
       end
     end
   end
