a0ab40e18c5dbcfc9dc255f5b5c70c515d2a1020
George Schena <george.schena@education.gov.uk>
2024-04-10 13:04:25 +0100

    Download claims from filter params

--- app/controllers/claims/support/claims_controller.rb
+++ app/controllers/claims/support/claims_controller.rb
@@ -12,7 +12,7 @@ class Claims::Support::ClaimsController < Claims::Support::ApplicationController
   def show; end
 
   def download_csv
-    csv_data = Claims::GenerateClaimsCsv.call
+    csv_data = Claims::GenerateClaimsCsv.call(claims: Claims::ClaimsQuery.call(params:))
 
     send_data csv_data, filename: "claims-#{Date.current}.csv", type: "text/csv", disposition: "attachment"
   end
