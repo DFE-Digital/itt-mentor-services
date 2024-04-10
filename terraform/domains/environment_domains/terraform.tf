e9e0b83b04150651111d99f97e243e528eb45fb9
John Ake <johnake@hotmail.com>
2024-04-10 10:03:55 +0100

    Create official domains

---
+++ terraform/domains/environment_domains/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+
+  required_version = "= 1.6.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.82.0"
+    }
+  }
+  backend "azurerm" {
+    container_name       = "ittmsdomains-tf"
+    resource_group_name  = "s189p01-ittmsdomains-rg"
+    storage_account_name = "s189p01ittmsdomainstf"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
+}
