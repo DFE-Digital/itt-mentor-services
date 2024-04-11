40c88e157573aeba772637b1493a6349c4c52f67
Ollie Treend <ollie@treend.uk>
2024-04-11 12:16:21 +0100

    Revert "Silence deprecation warning by monkey-patching Rack for Capybara"
    This reverts commit 71fa2ff07ed15a460bf599ef0d8f06bfdeaa411d.

--- config/initializers/capybara.rb
+++
@@ -1,9 +0,0 @@
-# https://github.com/teamcapybara/capybara/issues/2705#issuecomment-1752093026
-
-if Rails.env.test?
-  require "rackup"
-
-  module Rack
-    Handler = ::Rackup::Handler
-  end
-end
