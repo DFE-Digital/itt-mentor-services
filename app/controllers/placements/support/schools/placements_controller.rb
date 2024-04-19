ba460513747e7278785a391d81fd7c0f4041d883
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-04-19 17:41:58 +0100

    Add show action to support placements controller
    https://trello.com/c/jlfU86Ym

--- app/controllers/placements/support/schools/placements_controller.rb
+++ app/controllers/placements/support/schools/placements_controller.rb
@@ -1,14 +1,23 @@
 class Placements::Support::Schools::PlacementsController < Placements::Support::ApplicationController
   before_action :set_school
+  before_action :set_placement, only: %i[show]
 
   def index
     @pagy, placements = pagy(@school.placements.includes(:subjects, :mentors).order("subjects.name"))
     @placements = placements.decorate
   end
 
+  def show
+    @placement = @school.placements.find(params.fetch(:id)).decorate
+  end
+
   private
 
   def set_school
     @school = Placements::School.find(params.fetch(:school_id))
+  end
+
+  def set_placement
+    @placement = @school.placements.find(params.fetch(:id))
   end
 end
