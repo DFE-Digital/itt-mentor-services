a4b7e8edbbc6f8850e2079b196e4ad6ae6a6a18b
Daniel Dye <daniel.dye@education.gov.uk>
2024-04-26 11:48:33 +0100

    When a claim's provider is updated, propagate that change to all mentor trainings

--- spec/forms/claims/claim/provider_form_spec.rb
+++ spec/forms/claims/claim/provider_form_spec.rb
@@ -1,10 +1,8 @@
 require "rails_helper"
 
 describe Claims::Claim::ProviderForm, type: :model do
-  let!(:school) { create(:claims_school) }
-  let!(:provider) { create(:claims_provider, :best_practice_network) }
-  let!(:claim) { create(:claim, school:) }
-  let!(:current_user) { create(:claims_user) }
+  let(:school) { create(:claims_school) }
+  let(:existing_claim) { create(:claim, school:) }
 
   describe "validations" do
     context "when provider is not set" do
@@ -24,34 +22,49 @@ describe Claims::Claim::ProviderForm, type: :model do
 
     context "when claim already exists" do
       it "returns the existing claim" do
-        form = described_class.new(id: claim.id, school:)
-        expect(form.claim).to eq(claim)
+        form = described_class.new(id: existing_claim.id, school:)
+        expect(form.claim).to eq(existing_claim)
       end
     end
   end
 
-  describe "save" do
+  describe "#save" do
+    let(:provider) { create(:claims_provider, :best_practice_network) }
+    let(:current_user) { create(:claims_user) }
+
+    before do
+      existing_claim.mentor_trainings << create(:mentor_training, provider:)
+    end
+
     context "when claim doesn't exist" do
       it "creates an internal draft claim with a provider" do
         form = described_class.new(provider_id: provider.id, school:, current_user:)
 
-        expect {
-          form.save!
-        }.to change { form.claim.provider }.to provider
+        expect { form.save! }.to change { form.claim.provider }.to provider
+
         expect(form.claim.internal_draft?).to be(true)
         expect(form.claim.created_by).to eq(current_user)
       end
     end
 
     context "when claim does exist" do
-      it "creates an internal draft claim with a provider" do
-        form = described_class.new(id: claim.id, provider_id: provider.id, school:, current_user:)
+      it "updates the claim with the new provider" do
+        form = described_class.new(id: existing_claim.id, provider_id: provider.id, school:, current_user:)
 
-        expect {
-          form.save!
-        }.to change { claim.reload.provider }.to provider
-        expect(claim.internal_draft?).to be(true)
-        expect(form.claim.created_by).to eq(current_user)
+        expect { form.save! }.to change { existing_claim.reload.provider }.to provider
+
+        expect(existing_claim.internal_draft?).to be(true)
+        expect(existing_claim.created_by).not_to eq(current_user)
+        expect(existing_claim.mentor_trainings.pluck(:provider_id)).to all(eq(provider.id))
+      end
+
+      context "when selecting the same provider" do
+        it "doesn't update anything" do
+          form = described_class.new(id: existing_claim.id, provider_id: existing_claim.provider.id, school:, current_user:)
+
+          expect { form.save! }.to not_change { existing_claim.reload.provider }
+            .and(not_change { existing_claim.reload.mentor_trainings.pluck(:provider_id) })
+        end
       end
     end
   end
