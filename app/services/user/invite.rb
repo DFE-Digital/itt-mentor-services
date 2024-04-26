4f5428c594aa19cc695919b466aede369c65f0a7
Jamie <jamie.cleare@education.gov.uk>
2024-04-26 15:59:56 +0100

    Linting error

--- app/services/user/invite.rb
+++ app/services/user/invite.rb
@@ -9,7 +9,7 @@ class User::Invite
   attr_reader :user, :organisation, :service
 
   def call
-    return if user.service == :placements && 
+    return if user.service == :placements &&
       !Flipflop.enabled?(:placements_user_onboarding_emails)
 
     UserMailer.with(service: user.service).user_membership_created_notification(user, organisation).deliver_later
