ba71ad7ec905af28668e012883c7ae42cb3345b7
Tope Emmanuel <tope.fakokunde@gmail.com>
2024-04-29 09:36:02 +0100

    Added a toggle for statuscake WHY: Status cake is not needed in all environments HOW: By adding enable_monitoring toggle to statuscake resource

--- terraform/application/statuscake.tf
+++ terraform/application/statuscake.tf
@@ -1,6 +1,8 @@
 module "statuscake" {
+    
+  count = var.enable_monitoring? 1 : 0
   source = "./vendor/modules/aks//monitoring/statuscake"
-  
+
   uptime_urls    = concat([module.web_application.probe_url], var.statuscake_alerts.website_url)
   ssl_urls       = var.statuscake_alerts.ssl_url
   contact_groups = var.statuscake_alerts.contact_groups
