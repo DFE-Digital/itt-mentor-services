a7de404d60065220cdc2e6c6fc6b7ab306862897
George Schena <george.schena@education.gov.uk>
2024-04-30 10:00:41 +0100

    Cant remove mentor who is part of active claim

--- app/policies/claims/mentor_policy.rb
+++ app/policies/claims/mentor_policy.rb
@@ -1,5 +1,9 @@
 class Claims::MentorPolicy < Claims::ApplicationPolicy
   def destroy?
+    !Claims::Claim.active.joins(:mentor_trainings).where(mentor_trainings: { mentor_id: record }).exists?
+  end
+
+  def remove?
     true
   end
 end
