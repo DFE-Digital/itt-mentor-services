c4e62404da65a7018c5e7e252282b2a90e77c2c2
George Schena <george.schena@education.gov.uk>
2024-05-01 09:23:14 +0100

    Refactor mentors controller

--- app/controllers/claims/application_controller.rb
+++ app/controllers/claims/application_controller.rb
@@ -4,6 +4,8 @@ class Claims::ApplicationController < ApplicationController
   private
 
   def has_school_accepted_grant_conditions?
+    return true if current_user.support_user?
+
     redirect_to claims_school_grant_conditions_path(@school) unless @school.grant_conditions_accepted?
   end
 end
--- app/policies/claims/mentor_membership_policy.rb
+++ app/policies/claims/mentor_membership_policy.rb
@@ -1,6 +1,6 @@
 class Claims::MentorMembershipPolicy < Claims::ApplicationPolicy
   def destroy?
-    !Claims::Claim.active.joins(:mentor_trainings).where(mentor_trainings: { mentor_id: record.mentor_id }).where(claims: { school_id: record.school_id }).exists?
+    !Claims::Claim.active.joins(:mentor_trainings).where(school_id: record.school_id, mentor_trainings: { mentor_id: record.mentor_id }).exists?
   end
 
   def remove?
