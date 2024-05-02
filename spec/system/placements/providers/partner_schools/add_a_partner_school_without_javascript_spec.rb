9734708e244d3953246bb0b109b1405529a914d5
Jamie <jamie.cleare@education.gov.uk>
2024-05-02 11:56:17 +0100

    Fix tests since environment prepend

--- spec/system/placements/providers/partner_schools/add_a_partner_school_without_javascript_spec.rb
+++ spec/system/placements/providers/partner_schools/add_a_partner_school_without_javascript_spec.rb
@@ -175,7 +175,7 @@ RSpec.describe "Placements / Providers / Partner schools / Add a partner school 
   def partner_school_notification(user)
     ActionMailer::Base.deliveries.find do |delivery|
       delivery.to.include?(user.email) &&
-        delivery.subject == "An ITT provider has added your organisation to its list of partner schools"
+        delivery.subject == "[TEST] An ITT provider has added your organisation to its list of partner schools"
     end
   end
 
