2cd77b03fa7cfe8a9c6df963f63aa8ee21534164
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-05-03 14:49:57 +0100

    Update the mentor builder
    https://trello.com/c/MqTIGRYi

--- spec/services/mentor_builder_spec.rb
+++ spec/services/mentor_builder_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe MentorBuilder do
   it_behaves_like "a service object" do
-    let(:params) { { trn: "1234567" } }
+    let(:params) { { trn: "1234567", date_of_birth: "1991-01-22" } }
   end
 
   context "with invalid trn provided" do
@@ -10,7 +10,7 @@ RSpec.describe MentorBuilder do
 
     it "returns mentor object with error on trn" do
       mentor = described_class.call(trn:)
-      expect(mentor.class).to eq(Mentor)
+      expect(mentor.class).to eq(Placements::Mentor)
       expect(mentor.errors[:trn]).to include "Enter a valid teacher reference number (TRN)"
     end
   end
@@ -20,13 +20,13 @@ RSpec.describe MentorBuilder do
 
     it "returns mentor object with error on trn" do
       mentor = described_class.call(trn:)
-      expect(mentor.class).to eq(Mentor)
+      expect(mentor.class).to eq(Placements::Mentor)
       expect(mentor.errors[:trn]).to include "Enter a teacher reference number (TRN)"
     end
   end
 
   context "when mentor with trn already exists" do
-    let(:existing_mentor) { create(:mentor) }
+    let(:existing_mentor) { create(:placements_mentor) }
     let(:trn) { existing_mentor.trn }
 
     it "returns existing mentor" do
@@ -37,13 +37,14 @@ RSpec.describe MentorBuilder do
 
   context "when mentor does not exist and valid trn is provided" do
     let(:trn) { "1234567" }
+    let(:date_of_birth) { "1991-01-22" }
 
     before "fetches teacher record from teaching record service" do
-      allow(TeachingRecord::GetTeacher).to receive(:call).with(trn:).once.and_return(teacher_object)
+      allow(TeachingRecord::GetTeacher).to receive(:call).with(trn:, date_of_birth:).once.and_return(teacher_object)
     end
 
     it "returns initialized mentor record without errors" do
-      mentor = described_class.call(trn:)
+      mentor = described_class.call(trn:, date_of_birth:)
       expect(mentor.trn).to eq trn
       expect(mentor.first_name).to eq teacher_object["firstName"]
       expect(mentor.last_name).to eq teacher_object["lastName"]
@@ -54,11 +55,16 @@ RSpec.describe MentorBuilder do
 
   context "with valid trn and first_name or last_name args given" do
     let(:trn) { "0987654" }
-    let(:first_name) { "First Name" }
-    let(:last_name) { "Last Name" }
+    let(:first_name) { "Elizabeth" }
+    let(:last_name) { "Milner-Lee" }
+    let(:date_of_birth) { "1991-01-22" }
+
+    before "fetches teacher record from teaching record service" do
+      allow(TeachingRecord::GetTeacher).to receive(:call).with(trn:, date_of_birth:).once.and_return(teacher_object)
+    end
 
     it "returns initialized mentor record with first and last name" do
-      mentor = described_class.call(trn:, first_name:, last_name:)
+      mentor = described_class.call(trn:, first_name:, last_name:, date_of_birth:)
       expect(mentor.trn).to eq trn
       expect(mentor.first_name).to eq first_name
       expect(mentor.last_name).to eq last_name
@@ -71,6 +77,7 @@ RSpec.describe MentorBuilder do
     { "trn" => "1234554",
       "firstName" => "Elizabeth",
       "middleName" => "Elaine",
-      "lastName" => "Milner-Lee" }
+      "lastName" => "Milner-Lee",
+      "dateOfBirth" => "1991-01-22" }
   end
 end
