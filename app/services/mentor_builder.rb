1f663eadc0ba3b2e3e743eef3afef1aaae5de476
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-05-03 14:49:57 +0100

    Update the mentor builder
    https://trello.com/c/MqTIGRYi

--- app/services/mentor_builder.rb
+++ app/services/mentor_builder.rb
@@ -1,27 +1,30 @@
 class MentorBuilder
   include ServicePattern
 
-  def initialize(trn:, first_name: nil, last_name: nil)
+  def initialize(trn:, first_name: nil, last_name: nil, date_of_birth: nil)
     @trn = trn
     @first_name = first_name
     @last_name = last_name
+    @date_of_birth = date_of_birth
   end
 
   def call
-    mentor = Mentor.find_or_initialize_by(trn:)
-    return mentor if mentor.persisted? # We already have the data (first_name, last_name), so don't need to fetch or assign
-    return mentor if mentor.invalid_attributes?(:trn) # If the trn doesn't meet basic validation, we can't build any further
+    mentor = Mentor.find_or_initialize_by(trn:).becomes(Placements::Mentor)
+    return mentor if mentor.invalid_attributes?(:trn)
+    return mentor if date_of_birth.nil?
 
-    mentor.assign_attributes(first_name: first_name || teacher["firstName"],
-                             last_name: last_name || teacher["lastName"])
+    mentor.assign_attributes(
+      first_name: teacher["firstName"],
+      last_name: teacher["lastName"],
+    )
     mentor
   end
 
   private
 
-  attr_accessor :trn, :first_name, :last_name
+  attr_accessor :trn, :first_name, :last_name, :date_of_birth
 
   def teacher
-    @teacher ||= TeachingRecord::GetTeacher.call(trn:)
+    @teacher ||= TeachingRecord::GetTeacher.call(trn:, date_of_birth: date_of_birth.to_s)
   end
 end
