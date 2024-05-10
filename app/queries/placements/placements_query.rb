05cb2c543667f1e9c5175a42c64a6ce16992bf8c
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-05-10 13:28:00 +0100

    Update the placement query to base school selection on partner schools presence

--- app/queries/placements/placements_query.rb
+++ app/queries/placements/placements_query.rb
@@ -1,16 +1,31 @@
 class Placements::PlacementsQuery < ApplicationQuery
   def call
-    scope = Placement.includes(:school, :subjects)
-
+    scope = set_initial_scope
     scope = school_condition(scope)
     scope = subject_condition(scope)
     scope = school_type_condition(scope)
-    scope = partner_school_condition(scope)
     scope = only_available_placements_condition(scope)
     scope.order_by_subject_school_name
   end
 
   private
+
+  def set_initial_scope
+    if partner_schools_present?
+      scope_with_partner_schools
+    else
+      Placement.includes(:school, :subjects)
+    end
+  end
+
+  def scope_with_partner_schools
+    provider = Placements::Provider.find(params[:partner_schools].first)
+    Placement.where(school_id: provider.partner_schools.ids).includes(:school, :subjects)
+  end
+
+  def partner_schools_present?
+    params[:partner_schools]&.compact_blank!.present?
+  end
 
   def school_condition(scope)
     return scope if filter_params[:school_ids].blank?
