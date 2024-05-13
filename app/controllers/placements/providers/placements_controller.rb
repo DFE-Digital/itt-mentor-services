f36ae1085ef4c6576ae81d19cf9b5c3da0cf9688
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-05-13 14:19:41 +0100

    Remove compact blank

--- app/controllers/placements/providers/placements_controller.rb
+++ app/controllers/placements/providers/placements_controller.rb
@@ -22,7 +22,7 @@ class Placements::Providers::PlacementsController < ApplicationController
   private
 
   def schools_scope
-    if filter_params[:only_partner_schools]&.compact_blank&.empty?
+    if filter_params[:only_partner_schools] && filter_params[:only_partner_schools].empty?
       @provider.partner_schools
     else
       Placements::School.all
