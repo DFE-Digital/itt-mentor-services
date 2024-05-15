8d671d339a0c2870373b5cd9e4a082e20830be86
Jamie <jamie.cleare@education.gov.uk>
2024-05-15 09:50:58 +0100

    JS disabled support partnerships

--- app/controllers/placements/support/schools/partner_providers_controller.rb
+++ app/controllers/placements/support/schools/partner_providers_controller.rb
@@ -6,12 +6,18 @@ class Placements::Support::Schools::PartnerProvidersController < Placements::Sch
   end
 
   def redirect_to_check_path
-    redirect_to check_placements_support_school_partner_provider_path(
+    redirect_to check_placements_support_school_partner_providers_path(
       partnership: { provider_id: partner_provider_params.fetch(:provider_id), provider_name: search_param },
     )
   end
 
   def redirect_to_index_path
     redirect_to placements_support_school_partner_providers_path(@school)
+  end
+
+  def redirect_to_provider_options
+    redirect_to provider_options_placements_support_school_partner_providers_path(
+      partnership: { search_param: },
+    )
   end
 end
