4441bf0aa2bd50dbd0b042a67befc62ada0273f4
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-05-17 15:10:06 +0100

    Add provider importer and clean up specs

---
+++ app/services/placements/importers/provider_users_importer.rb
@@ -0,0 +1,82 @@
+require "csv"
+
+module Placements
+  module Importers
+    class ProviderUsersImporter
+      include ServicePattern
+
+      attr_reader :csv_path
+
+      def initialize(csv_path)
+        @csv_path = csv_path
+      end
+
+      def call
+        @user_records = Hash.new { |h, k| h[k] = [] }
+
+        CSV.foreach(csv_path, headers: true) do |user|
+          @user_records[user["UKPRN"]] << {
+            first_name: user["First name"],
+            last_name: user["Last name"],
+            email: user["Email address"],
+          }
+        end
+
+        process_user_records
+
+        Rails.logger.info("#{successful_count} of #{total_count} users have been imported successfully.")
+      end
+
+      private
+
+      attr_reader :user_records, :successful_count
+
+      def process_user_records
+        @successful_count = 0
+
+        user_records.each do |ukprn, users|
+          process_provider_users(ukprn, users)
+        end
+      end
+
+      def process_provider_users(ukprn, users)
+        provider = ::Provider.find_by(ukprn:)
+
+        if provider.present?
+          onboard_provider(provider)
+          process_users(users, provider)
+        else
+          Rails.logger.error("Failed to import users for provider with UKPRN: #{ukprn}. Provider not found.")
+        end
+      end
+
+      def onboard_provider(provider)
+        provider.update!(placements_service: true)
+      end
+
+      def process_users(users, provider)
+        users.each do |user|
+          process_user(user, provider)
+        end
+      end
+
+      def process_user(user, provider)
+        user_instance = User.find_or_initialize_by(email: user[:email]) do |new_user|
+          new_user.first_name = user[:first_name]
+          new_user.last_name = user[:last_name]
+          new_user.providers << provider unless new_user.providers.exists?(provider.id)
+        end
+
+        if user_instance.save
+          @successful_count += 1
+        else
+          Rails.logger.error("Failed to import user for #{provider.name}: #{user_instance.errors.full_messages.to_sentence}")
+        end
+      end
+
+      def total_count
+        @total_count ||= user_records.values.sum(&:length)
+      end
+    end
+  end
+end
---
+++ spec/services/placements/importers/provider_users_importer_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+
+RSpec.describe Placements::Importers::ProviderUsersImporter do
+  it_behaves_like "a service object" do
+    let(:params) { { path: "some_path_to_nowhere" } }
+  end
+
+  describe "#call" do
+    let(:csv_path) { "spec/fixtures/placements/importers/provider_users.csv" }
+
+    context "when the data is valid" do
+      let(:provider) { create(:provider, ukprn: "10052837", placements_service: false) }
+
+      it "onboards the provider" do
+        expect { described_class.call(csv_path) }.to change { provider.reload.placements_service }.from(false).to(true)
+      end
+
+      it "creates users for the provider" do
+        expect { described_class.call(csv_path) }.to change { provider.reload.users.count }.from(0).to(1)
+      end
+
+      it "creates users with the correct attributes", :aggregate_failures do
+        provider
+        described_class.call(csv_path)
+        user = provider.reload.users.first
+
+        expect(user.first_name).to eq("Jeff")
+        expect(user.last_name).to eq("Barnes")
+        expect(user.email).to eq("j.barnes@example.com")
+      end
+
+      it "logs the number of users imported" do
+        provider
+        expect(Rails.logger).to receive(:info).with("1 of 4 users have been imported successfully.")
+
+        described_class.call(csv_path)
+      end
+
+      it "does not create duplicate users" do
+        provider
+        described_class.call(csv_path)
+        expect { described_class.call(csv_path) }.not_to(change(Placements::User, :count))
+      end
+    end
+
+    context "when the user details are not valid" do
+      let!(:provider) { create(:provider, ukprn: "10064216", placements_service: false) }
+
+      before do
+        create(:provider, ukprn: "10052837", placements_service: false)
+        create(:provider, ukprn: "10032194", placements_service: false)
+      end
+
+      it "logs an error when a user fails to import" do
+        expect(Rails.logger).to receive(:error)
+          .with("Failed to import user for #{provider.name}: Email address Enter an email address and Email address Enter an email address in the correct format, like name@example.com")
+
+        described_class.call(csv_path)
+      end
+    end
+
+    context "when the provider is not found" do
+      before do
+        create(:provider, ukprn: "10052837", placements_service: false)
+        create(:provider, ukprn: "10032194", placements_service: false)
+      end
+
+      it "logs an error when a provider is not found" do
+        expect(Rails.logger).to receive(:error).with("Failed to import users for provider with UKPRN: 10064216. Provider not found.")
+
+        described_class.call(csv_path)
+      end
+    end
+  end
+end
