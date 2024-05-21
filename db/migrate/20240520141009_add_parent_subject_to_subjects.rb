86eb0e27d77b19476a8bca68524827ed0fc4cb20
Jamie <jamie.cleare@education.gov.uk>
2024-05-21 15:42:28 +0100

    Quick fix to get migrations to run

--- db/migrate/20240520141009_add_parent_subject_to_subjects.rb
+++ db/migrate/20240520141009_add_parent_subject_to_subjects.rb
@@ -1,5 +1,7 @@
 class AddParentSubjectToSubjects < ActiveRecord::Migration[7.1]
   def change
+    StrongMigrations.disable_check(:add_reference)
+
     add_reference :subjects, :parent_subject, type: :uuid, foreign_key: { to_table: :subjects }
   end
 end
--- db/migrate/20240520141421_add_subject_to_placements.rb
+++ db/migrate/20240520141421_add_subject_to_placements.rb
@@ -1,5 +1,7 @@
 class AddSubjectToPlacements < ActiveRecord::Migration[7.1]
   def change
+    StrongMigrations.disable_check(:add_reference)
+
     add_reference :placements, :subject, type: :uuid, foreign_key: true
   end
 end
