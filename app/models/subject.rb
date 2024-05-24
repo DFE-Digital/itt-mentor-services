82b3aead8b7dfcb61cb02181efee9fb7ddd3718a
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-05-24 17:15:38 +0100

    Add scope and child_subject check
    https://trello.com/c/nmNKh8kE/371-update-the-add-a-placement-and-change-subject-journeys

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -32,4 +32,9 @@ class Subject < ApplicationRecord
   validates :subject_area, :name, presence: true
 
   scope :order_by_name, -> { order(:name) }
+  scope :parent_subjects, -> { where(parent_subject_id: nil).order_by_name }
+
+  def has_child_subjects?
+    child_subjects.exists?
+  end
 end
