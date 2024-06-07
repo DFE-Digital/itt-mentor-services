66373d9c3ffa9619712d665551571b188f20875a
Ollie Treend <ollie@treend.uk>
2024-06-07 14:42:30 +0100

    Format JavaScript file for consistent style
    My editor (VS Code) has opinions about how JavaScript files should be
    formatted. It has auto-formatted this file, so I've pulled its changes
    out into a separate commit to try and remove noise from my actual
    intended commit which will change the debounce timeout.
    
    The original formatting in this file was inconsistent. Lines were
    indented incorrectly, there was a mix of single and double quotes, and
    some lines ended with semicolons where others didn't.
    
    I therefore think it's better for the file to be formatted consistently,
    rather than not at all.

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,61 +1,66 @@
-import { Controller } from "@hotwired/stimulus"
-import accessibleAutocomplete from 'accessible-autocomplete'
+import { Controller } from "@hotwired/stimulus";
+import accessibleAutocomplete from "accessible-autocomplete";
 import debounce from "lodash.debounce";
 import { request } from "../utils/request_helper";
 
 // Connects to data-controller="autocomplete"
 export default class extends Controller {
-  static targets = [ "serverInput", "input" ]
+  static targets = ["serverInput", "input"];
   static values = {
     path: String,
     returnAttributes: Array,
-  }
+  };
 
   connect() {
     const minLength = 2;
 
-   accessibleAutocomplete({
-     element: this.inputTarget,
-     id: this.serverInputTarget.id,
-     showNoOptionsFound: true,
-     name: this.inputTarget.dataset.inputName,
-     defaultValue: this.serverInputTarget.value,
-     minLength,
-     source: debounce(request(this.pathValue), 900),
-     templates: {
-       inputValue: this.inputValueTemplate.bind(this),
-       suggestion: this.resultTemplate.bind(this),
-     },
-     onConfirm: this.onConfirm.bind(this),
-     confirmOnBlur: false,
-     autoselect: true,
-   });
+    accessibleAutocomplete({
+      element: this.inputTarget,
+      id: this.serverInputTarget.id,
+      showNoOptionsFound: true,
+      name: this.inputTarget.dataset.inputName,
+      defaultValue: this.serverInputTarget.value,
+      minLength,
+      source: debounce(request(this.pathValue), 900),
+      templates: {
+        inputValue: this.inputValueTemplate.bind(this),
+        suggestion: this.resultTemplate.bind(this),
+      },
+      onConfirm: this.onConfirm.bind(this),
+      confirmOnBlur: false,
+      autoselect: true,
+    });
 
     // Hijack the original input to submit the selected value.
     this.serverInputTarget.id = `old-${this.serverInputTarget.id}`;
     this.serverInputTarget.type = "hidden";
-    this.serverInputTarget.value = this.serverInputTarget.dataset.previousSearch || '';
+    this.serverInputTarget.value =
+      this.serverInputTarget.dataset.previousSearch || "";
   }
 
   clearUndefinedSuggestions() {
-    const autocompleteElement = this.element.getElementsByClassName("autocomplete__wrapper")[0];
+    const autocompleteElement = this.element.getElementsByClassName(
+      "autocomplete__wrapper"
+    )[0];
     if (autocompleteElement) {
-      const autocompleteOptions = autocompleteElement.getElementsByClassName("autocomplete__option");
+      const autocompleteOptions = autocompleteElement.getElementsByClassName(
+        "autocomplete__option"
+      );
       for (const option of autocompleteOptions) {
-        if ( option.innerHTML === ""){
+        if (option.innerHTML === "") {
           option.remove();
         }
       }
     }
   }
 
-  private
+  private;
 
   inputValueTemplate(result) {
     if (result && typeof result === "object") {
       return result.name;
     } else {
-      return ""
+      return "";
     }
   }
 
@@ -68,7 +73,7 @@ export default class extends Controller {
 
       return `${result.name} (${attributes})`;
     } else {
-      return ""
+      return "";
     }
   }
 
