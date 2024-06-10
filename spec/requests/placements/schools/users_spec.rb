c6c4a6b8342217d7b7bc4a41c814d0cdd135b427
Ollie Treend <ollie@treend.uk>
2024-06-10 16:53:36 +0100

    Add test coverage for user deletion
    I've added request specs to prove that users must be authorized when
    deleting other users.
    
    It also proves that users cannot delete themselves.
    
    I've added this because I noticed we weren't covering the "support user
    cannot delete themselves" scenario, and so it was possible to remove the
    authorization code and the tests would still pass.

---
+++ spec/requests/placements/schools/users_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe "School users", type: :request, service: :placements do
+  let(:school) { build(:placements_school) }
+  let(:current_user) { create(:placements_user, schools: [school]) }
+
+  before do
+    user_exists_in_dfe_sign_in(user: current_user)
+    get "/auth/dfe/callback"
+    follow_redirect!
+  end
+
+  describe "DELETE" do
+    let(:school) { create(:placements_school) }
+
+    context "when deleting a user in the same school as the current user" do
+      let(:user) { create(:placements_user, schools: [school]) }
+
+      it "deletes the user" do
+        expect(school.users).to include(user)
+        delete placements_school_user_path(school, user)
+        expect(flash[:success]).to eq "User removed"
+        expect(school.users).not_to include(user)
+      end
+    end
+
+    context "when the current user tries to delete themselves" do
+      it "shows an error message" do
+        delete placements_school_user_path(school, current_user)
+        expect(flash[:alert]).to eq "You cannot perform this action"
+        expect(school.users).to include(current_user)
+      end
+    end
+
+    context "when deleting a user in a different school" do
+      let(:another_school) { build(:placements_school) }
+      let(:user) { create(:placements_user, schools: [another_school]) }
+
+      it "shows an error message" do
+        expect {
+          delete placements_school_user_path(another_school, user)
+        }.to raise_exception(ActiveRecord::RecordNotFound)
+        expect(another_school.users).to include(user)
+      end
+    end
+  end
+end
