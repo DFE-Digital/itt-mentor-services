43dc650eb4582649546c68af270d43f593468789
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-06-17 16:11:45 +0100

    Create new mentors step

---
+++ app/wizards/placements/add_placement/steps/mentors.rb
@@ -0,0 +1,36 @@
+class Placements::AddPlacement::Steps::Mentors
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :school
+  attribute :mentor_ids, default: []
+
+  validates :school, presence: true
+  validates :mentor_ids, presence: true
+
+  def mentors_for_selection
+    school.mentors
+  end
+
+  def mentor_ids=(value)
+    super normalised_mentor_ids(value)
+  end
+
+  def wizard_attributes
+    { mentor_ids: mentor_ids - NOT_KNOWN }
+  end
+
+  private
+
+  NOT_KNOWN = [:not_known].freeze
+
+  def normalised_mentor_ids(mentor_ids)
+    if mentor_ids.blank?
+      []
+    elsif mentor_ids.include?("not_known")
+      NOT_KNOWN
+    else
+      school.mentors.where(id: mentor_ids).ids
+    end
+  end
+end
