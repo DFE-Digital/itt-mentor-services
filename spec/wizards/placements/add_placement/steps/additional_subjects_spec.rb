2a07923cb63885f588d67349834e4589b8bf033d
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-06-21 14:08:42 +0100

    Rename spec and check presence

--- spec/wizards/placements/add_placement/steps/additional_subjects_spec.rb
+++ spec/wizards/placements/add_placement/steps/additional_subjects_spec.rb
@@ -9,9 +9,15 @@ RSpec.describe Placements::AddPlacement::Steps::AdditionalSubjects, type: :model
     it { is_expected.to validate_presence_of(:school) }
     it { is_expected.to validate_presence_of(:parent_subject_id) }
 
-    describe "#additional_subjects_valid" do
+    describe "additional subjects validation" do
       let(:school) { create(:placements_school) }
       let(:parent_subject) { create(:subject) }
+
+      it "is invalid if the parent subject is present and additional subjects are blank" do
+        step = described_class.new(school:, parent_subject_id: parent_subject.id)
+
+        expect(step).to be_invalid
+      end
 
       it "is invalid if the additional subject does not exist" do
         child_subject = create(:subject, parent_subject:)
