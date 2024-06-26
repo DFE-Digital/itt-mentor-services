7d618801d798b2149da60c6605593c6ecae037da
Jamie <jamie.cleare@education.gov.uk>
2024-06-26 13:59:58 +0100

    Remove service updates from the app

--- app/controllers/service_updates_controller.rb
+++
@@ -1,8 +0,0 @@
-class ServiceUpdatesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
-
-  def index
-    @service_name ||= current_service
-    @service_updates = ServiceUpdate.where(service: @service_name)
-  end
-end
