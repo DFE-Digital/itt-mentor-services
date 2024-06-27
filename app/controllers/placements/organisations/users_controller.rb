3d5b045571366bc415f6e464bef80e175a5ba9d2
Jamie <jamie.cleare@education.gov.uk>
2024-06-27 14:33:39 +0100

    Consolidate organisation user controllers

--- app/controllers/placements/organisations/users_controller.rb
+++ app/controllers/placements/organisations/users_controller.rb
@@ -1,7 +1,8 @@
 class Placements::Organisations::UsersController < Placements::ApplicationController
   before_action :set_organisation
   before_action :set_user, only: %i[show remove destroy]
-  before_action :redirect_if_not_allowed, only: %i[remove destroy]
+  before_action :set_user_membership, only: %i[show remove destroy]
+  before_action :authorize_user, only: %i[remove destroy]
 
   def index
     @users = users.order_by_full_name
@@ -42,13 +43,6 @@ class Placements::Organisations::UsersController < Placements::ApplicationContro
     @users = @organisation.users
   end
 
-  def redirect_if_not_allowed
-    unless policy(@user).destroy?
-      redirect_to_index
-      flash[:alert] = t(".you_cannot_perform_this_action")
-    end
-  end
-
   def user_params
     params.require(:user_invite_form)
           .permit(:first_name, :last_name, :email)
@@ -57,5 +51,13 @@ class Placements::Organisations::UsersController < Placements::ApplicationContro
 
   def user_form
     @user_form ||= UserInviteForm.new(user_params)
+  end
+
+  def set_user_membership
+    @user_membership = @user.user_memberships.find_by!(organisation: @organisation)
+  end
+
+  def authorize_user
+    authorize [:placements, @user_membership]
   end
 end
--- app/policies/placements/user_policy.rb
+++
@@ -1,9 +0,0 @@
-class Placements::UserPolicy < ApplicationPolicy
-  def destroy?
-    user != record
-  end
-
-  def remove?
-    destroy?
-  end
-end
