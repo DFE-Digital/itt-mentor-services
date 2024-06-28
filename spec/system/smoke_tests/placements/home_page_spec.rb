8fc9a0dcf9883eaafdd6e42a262ff8b3cb76951d
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-06-28 11:49:35 +0100

    Move smoke tests under the system folder for linting purposes

--- spec/system/smoke_tests/placements/home_page_spec.rb
+++ spec/system/smoke_tests/placements/home_page_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Home Page", type: :system, smoke_test: true, service: :placements do
+RSpec.describe "Home Page", :smoke_test, service: :placements, type: :system do
   scenario "User visits the placements homepage" do
     given_i_am_on_the_start_page
     i_can_see_the_placements_service_name_in_the_header
