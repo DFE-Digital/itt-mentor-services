531899a7506cd6aeb5e7eddff4cc6612e1cfc291
Jamie <jamie.cleare@education.gov.uk>
2024-06-28 11:00:08 +0100

    Improve policy code

--- app/policies/placements/user_membership_policy.rb
+++ app/policies/placements/user_membership_policy.rb
@@ -1,10 +1,13 @@
 class Placements::UserMembershipPolicy < ApplicationPolicy
   def destroy?
-    return false unless user != record.user
+    # Users cannot delete themselves
+    return false if user == record.user
 
     user.support_user? ||
-      user.schools.ids.include?(record.organisation.id) ||
-      user.providers.ids.include?(record.organisation.id)
+      user.user_memberships.exists?(
+        organisation_id: record.organisation_id,
+        organisation_type: record.organisation_type,
+      )
   end
 
   def remove?
