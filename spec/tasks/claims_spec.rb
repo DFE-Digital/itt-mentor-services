75d5d6ea68c8fa19519de4323e2e21b827861849
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-06-28 12:17:16 +0100

    Disable RSpec/VerifiedDoubleReference rule and revert auto-corrected changes

--- spec/tasks/claims_spec.rb
+++ spec/tasks/claims_spec.rb
@@ -8,7 +8,7 @@ describe "claims" do
     subject(:import_schools) { Rake::Task["claims:import_schools"].invoke }
 
     let(:csv_string) { "provider_name,placement_school_urn,placement_school_name,First name,Last name,Email\nBest Practice Network,40435,Yeo Moor Primary School,Bob,Yargen,bob1@gmail.com\nBest Practice Network,40431,Yargen Primary School,Bob,Yargen,bob2@gmail.com\nBest Practice Network,40432,Bob Primary School,Bob,Yargen,bob3@gmail.com\nBest Practice Network,40439,another Primary School,#N/A,#N/A,#N/A\nBest Practice Network,40440,Not matching School,#N/A,#N/A,#N/A" }
-    let(:encrypted_mock) { instance_double(encrypted, read: csv_string) }
+    let(:encrypted_mock) { instance_double("encrypted", read: csv_string) }
 
     it "calls Claims::ImportSchools service" do
       expect(Claims::ImportSchools).to receive(:call)
