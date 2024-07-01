d785d3b795ced7ae388104092dabfe44dd584dc6
Ollie Treend <ollie@treend.uk>
2024-07-01 12:37:37 +0100

    Tighten up policy rules for placements
    I've introduced a new permission called "add_placement_journey" to the
    Placement policy.
    
    This is the permission used by the 'Add placement' wizard controller,
    reflecting the fact that the wizard itself should be seen as a
    self-contained unit. Since it's not a traditional CRUD/resourceful
    controller, we don't need to authenticate each method (new, edit,
    update) of the wizard controller.
    
    Hopefully this provides a clearer mapping between the policy and where
    it's applied.
    
    I've also DRYed up a duplicate rule in the policy, as well as the
    associated spec file.

--- app/policies/placement_policy.rb
+++ app/policies/placement_policy.rb
@@ -3,9 +3,15 @@ class PlacementPolicy < ApplicationPolicy
     record.school.school_contact.present?
   end
 
-  def update?
-    record.school.school_contact.present?
-  end
-  alias_method :edit_provider?, :update?
-  alias_method :edit_mentors?, :update?
+  # Actions in Placements::Schools::PlacementsController
+  alias_method :edit_provider?, :new?
+  alias_method :edit_mentors?, :new?
+  alias_method :edit_year_group?, :new?
+  alias_method :update?, :new?
+
+  # This permission represents the entire "Add placement" journey.
+  # It's not a traditional CRUD/resourceful controller, but the end
+  # result is a new Placement being created.
+  # Placements::Schools::Placements::AddPlacementController
+  alias_method :add_placement_journey?, :new?
 end
