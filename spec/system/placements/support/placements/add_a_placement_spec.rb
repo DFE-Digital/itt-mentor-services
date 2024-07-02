dd21645fc840dcc2a2fcfacab624825afef455e7
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-07-02 12:26:06 +0100

    Refactor system spec for wizard to be re-usable to avoid needless duplication

--- spec/system/placements/support/placements/add_a_placement_spec.rb
+++ spec/system/placements/support/placements/add_a_placement_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Placements / Schools / Placements / Add a placement",
+RSpec.describe "Placements / Support / Schools / Placements / Add a placement",
                service: :placements, type: :system do
   let!(:subject_1) { create(:subject, name: "Primary subject", subject_area: :primary) }
   let!(:subject_2) { create(:subject, name: "Secondary subject", subject_area: :secondary) }
@@ -9,33 +9,21 @@ RSpec.describe "Placements / Schools / Placements / Add a placement",
   let!(:mentor_2) { create(:placements_mentor) }
 
   before do
-    given_i_sign_in_as_anne
-  end
-
-  context "when the school has no school contact assigned" do
-    let(:school) { build(:placements_school, with_school_contact: false) }
-
-    it "does not allow the user to add a placement" do
-      when_i_visit_the_placements_page
-      then_i_see_content(
-        "Before you add a placement, you must add a placement contact so that the teacher training providers can contact you.",
-      )
-      and_i_do_not_see_the_button_to("Add placement")
-    end
+    given_i_sign_in_as_colin
   end
 
   it_behaves_like "an add a placement wizard"
 
   private
 
-  def given_i_sign_in_as_anne
-    and_there_is_an_existing_user_for("Anne")
+  def given_i_sign_in_as_colin
+    and_there_is_an_existing_user_for("Colin")
     and_i_visit_the_sign_in_path
     and_i_click_sign_in
   end
 
   def and_there_is_an_existing_user_for(user_name)
-    user = create(:placements_user, user_name.downcase.to_sym)
+    user = create(:placements_support_user, user_name.downcase.to_sym)
     user_exists_in_dfe_sign_in(user:)
     create(:user_membership, user:, organisation: school)
   end
@@ -49,7 +37,7 @@ RSpec.describe "Placements / Schools / Placements / Add a placement",
   end
 
   def when_i_visit_the_placements_page
-    visit placements_school_placements_path(school)
+    visit placements_support_school_placements_path(school)
   end
 
   def then_i_see_content(text)
