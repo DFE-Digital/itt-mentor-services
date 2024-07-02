32660b37118f42f473d2a22e53caa4264b9dcf8d
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-07-02 17:03:54 +0100

    Use inheritance to remove duplication

--- app/controllers/placements/support/schools/placements/add_placement_controller.rb
+++ app/controllers/placements/support/schools/placements/add_placement_controller.rb
@@ -1,32 +1,4 @@
-class Placements::Support::Schools::Placements::AddPlacementController < Placements::ApplicationController
-  before_action :set_school
-  before_action :set_wizard
-  before_action :authorize_placement
-
-  helper_method :step_path, :current_step_path, :back_link_path
-
-  attr_reader :school
-
-  def new
-    @wizard.reset_state
-    redirect_to step_path(@wizard.first_step)
-  end
-
-  def edit; end
-
-  def update
-    if !@wizard.save_step
-      render "edit"
-    elsif @wizard.next_step.present?
-      redirect_to step_path(@wizard.next_step)
-    else
-      placement = @wizard.create_placement
-      Placements::PlacementSlackNotifier.placement_created_notification(@school, placement.decorate).deliver_later
-      @wizard.reset_state
-      redirect_to placements_support_school_placements_path(@school), flash: { success: t(".success") }
-    end
-  end
-
+class Placements::Support::Schools::Placements::AddPlacementController < Placements::Schools::Placements::AddPlacementController
   private
 
   def set_school
@@ -34,21 +6,12 @@ class Placements::Support::Schools::Placements::AddPlacementController < Placeme
     @school = Placements::School.find(school_id)
   end
 
-  def set_wizard
-    current_step = params[:step]&.to_sym
-    @wizard = Placements::AddPlacementWizard.new(school:, params:, session:, current_step:)
-  end
-
-  def authorize_placement
-    authorize school.placements.build, :add_placement_journey?
+  def update_path(school)
+    placements_support_school_placements_path(school)
   end
 
   def step_path(step)
     add_placement_placements_support_school_placements_path(step:)
-  end
-
-  def current_step_path
-    step_path(@wizard.current_step)
   end
 
   def back_link_path
