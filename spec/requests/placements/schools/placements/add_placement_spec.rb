e71f9e47df5c290ff848d2401ad75224ad45a244
Ollie Treend <ollie@treend.uk>
2024-07-05 14:18:01 +0100

    Add a request spec for the "Add placement" journey
    This covers the non-user facing outputs of the "Add placement" journey:
    
    - a Placement record gets created
    - a Slack message gets sent to the team
    - unauthorized users can't create a Placement
    
    I've also relocated the "placements" request spec because it maps to the
    placements controller which exists in the directory above.

---
+++ spec/requests/placements/schools/placements/add_placement_spec.rb
@@ -0,0 +1,97 @@
+require "rails_helper"
+
+RSpec.describe "'Add placement' journey", service: :placements, type: :request do
+  let(:current_user) { create(:placements_user, schools: [school]) }
+  let(:school) { create(:placements_school, :secondary, name: "Hogwarts") }
+  let(:school_id) { school.id }
+  let(:drama) { create(:subject, :secondary, name: "Drama") }
+  let(:start_path) { new_add_placement_placements_school_placements_path(school_id:) }
+
+  before { sign_in_as current_user }
+
+  context "when starting a new wizard journey" do
+    before do
+      # Populate the wizard so it has some existing state
+      put step_path(:subject), params: { "placements_add_placement_wizard_subject_step[subject_id]" => drama.id }
+    end
+
+    it "resets the wizard state" do
+      expect(request.session["Placements::AddPlacementWizard"]).to be_present
+      get start_path
+      expect(request.session["Placements::AddPlacementWizard"]).to be_empty
+    end
+
+    it "redirects to the first step of the wizard" do
+      get start_path
+      expect(response).to redirect_to step_path(:subject)
+    end
+  end
+
+  context "when the wizard is complete" do
+    before do
+      # Populate the wizard so it's ready to submit
+      get start_path
+      put step_path(:subject), params: { "placements_add_placement_wizard_subject_step[subject_id]" => drama.id }
+    end
+
+    it "creates a placement" do
+      expect { put step_path(:check_your_answers) }.to change(Placement, :count).by(1)
+      expect(Placement.last).to have_attributes({ school:, subject: drama })
+    end
+
+    it "sends a Slack message" do
+      expect { put step_path(:check_your_answers) }
+      .to have_enqueued_job(SlackNotifier::Message::DeliveryJob)
+      .with(anything, ":new: Placement added: Drama at Hogwarts", anything)
+    end
+
+    it "resets the wizard state" do
+      expect(request.session["Placements::AddPlacementWizard"]).to be_present
+      put step_path(:check_your_answers)
+      expect(request.session["Placements::AddPlacementWizard"]).to be_empty
+    end
+
+    it "redirects to the school's list of placements" do
+      put step_path(:check_your_answers)
+      expect(response).to redirect_to placements_school_placements_path(school_id:)
+    end
+  end
+
+  describe "unhappy paths" do
+    let(:test_paths) do
+      [
+        start_path,
+        step_path(:subject),
+        step_path(:check_your_answers),
+      ]
+    end
+
+    context "when the user does not belong to the school" do
+      let(:current_user) { create(:placements_user) }
+
+      it "raises an error" do
+        test_paths.each do |path|
+          expect { get path }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
+    end
+
+    context "when the school doesn't have a placement contact" do
+      let(:school) { create(:placements_school, with_school_contact: false) }
+
+      it "redirects to the homepage with an error message" do
+        test_paths.each do |path|
+          get path
+          expect(response).to redirect_to(placements_root_url)
+          expect(flash[:alert]).to eq("You cannot perform this action")
+        end
+      end
+    end
+  end
+
+  private
+
+  def step_path(step)
+    add_placement_placements_school_placements_path(school_id:, step:)
+  end
+end
