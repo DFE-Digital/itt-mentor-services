6a4ced68b293e097d665a250a0affc11885c707a
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-07-15 16:38:31 +0100

    Replace "on" with NOT_KNOWN constant

--- app/wizards/placements/edit_placement_wizard/provider_step.rb
+++ app/wizards/placements/edit_placement_wizard/provider_step.rb
@@ -1,15 +1,17 @@
 class Placements::EditPlacementWizard::ProviderStep < Placements::BaseStep
   attribute :provider_id
 
-  validates :provider_id, inclusion: { in: ->(step) { step.providers_for_selection.ids } }, if: -> { provider_id != "on" }
+  validates :provider_id, inclusion: { in: ->(step) { step.providers_for_selection.ids } }, if: -> { provider_id != NOT_KNOWN }
 
   delegate :school, to: :wizard
+
+  NOT_KNOWN = "not_known".freeze
 
   def providers_for_selection
     school.partner_providers
   end
 
   def provider
-    @provider ||= school.partner_providers.find(provider_id) if provider_id.present? && provider_id != "on"
+    @provider ||= school.partner_providers.find(provider_id) if provider_id.present? && provider_id != NOT_KNOWN
   end
 end
