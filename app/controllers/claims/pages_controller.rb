5a55487ee6954966ba76a6b093def9f6f28f65c8
Daniel Dye <daniel.dye@education.gov.uk>
2024-07-25 13:58:04 +0100

    Update claims start page to use dynamic values
    - Uses Current or Previous claim window to display claim window closing date
    - Uses same claim window to determine academic years

--- app/controllers/claims/pages_controller.rb
+++ app/controllers/claims/pages_controller.rb
@@ -5,5 +5,17 @@ class Claims::PagesController < Claims::ApplicationController
 
   before_action :redirect_to_after_sign_in_path, only: :start, if: :user_signed_in?
 
+  helper_method :claim_window, :academic_year
+
   def start; end
+
+  private
+
+  def claim_window
+    @claim_window ||= Claims::ClaimWindow.current || Claims::ClaimWindow.previous
+  end
+
+  def academic_year
+    @academic_year ||= claim_window.academic_year
+  end
 end
