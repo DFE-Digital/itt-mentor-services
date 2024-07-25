f3a4197682a0c639e6df7785bc5860994bb3d4bc
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-07-25 15:18:03 +0100

    Create Google Routes API lib

--- lib/google/routes_api.rb
+++ lib/google/routes_api.rb
@@ -8,40 +8,40 @@ module Google
     headers "Accept" => "application/json",
             "Content-Type" => "application/json;odata.metadata=minimal",
             "X-Goog-Api-Key" => ENV.fetch("GOOGLE_MAPS_API_KEY", "").to_s,
-            "X-Goog-FieldMask" => "*"
+            "X-Goog-FieldMask" => "localizedValues,destinationIndex"
 
-    def initialize(origins:, destinations:, travel_mode: "DRIVE")
-      @origins = origins
-      @destinations = destinations
-      @travel_mode = travel_mode
-    end
-
-    def call
-      self.class.post("/distanceMatrix/v2:computeRouteMatrix", body: options)
+    def travel_time(origin_address, destinations, travel_mode: "DRIVE")
+      self.class.post("/distanceMatrix/v2:computeRouteMatrix", body: options(origin_address, destinations, travel_mode))
     end
 
     private
 
-    attr_reader :origins, :destinations, :travel_mode
-
-    def options
+    def options(origin_address, destinations, travel_mode)
       @options ||= {
-        origins: origins.map { |origin| transform(origin) },
-        destinations: destinations.map { |destination| transform(destination) },
+        origins: [transform_origin(origin_address)],
+        destinations: destinations.map { |destination| transform_destination(destination) },
         travelMode: travel_mode,
         units: UNITS,
       }.to_json
     end
 
-    def transform(location)
+    def transform_destination(destination)
       {
         waypoint: {
           location: {
             latLng: {
-              latitude: location.latitude,
-              longitude: location.longitude,
+              latitude: destination.latitude,
+              longitude: destination.longitude,
             },
           },
+        },
+      }
+    end
+
+    def transform_origin(address)
+      {
+        waypoint: {
+          address:,
         },
       }
     end
