6f60aa6906c3ef30eae3f0d1b1eb48f041c29a00
Daniel Dye <daniel.dye@education.gov.uk>
2024-07-26 16:36:36 +0100

    Remove the restriction of requiring claim windows to be made only in the future
    There is no reason to force this limitation within the model validations.
    
    We can potentially decide to validate this within a form object in the future, but no harm can be done by making a claim window in the past.

--- spec/factories/claims/claim_windows.rb
+++ spec/factories/claims/claim_windows.rb
@@ -25,8 +25,6 @@ FactoryBot.define do
       starts_on { 2.days.ago }
       ends_on { 2.days.from_now }
       association :academic_year, :current
-
-      to_create { |instance| instance.save!(validate: false) }
     end
   end
 end
