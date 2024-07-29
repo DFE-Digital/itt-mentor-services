b118dc468baa4d4daa10920bf863fcea9dd2c559
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-07-29 15:16:01 +0100

    Improve code coverage

--- spec/wizards/placements/add_placement_wizard/preview_placement_step_spec.rb
+++ spec/wizards/placements/add_placement_wizard/preview_placement_step_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Placements::AddPlacementWizard::PreviewPlacementStep, type: :mode
   subject(:step) { described_class.new(wizard: mock_wizard, attributes: nil) }
 
   let(:mock_subject_step) { instance_double(Placements::AddPlacementWizard::SubjectStep, subject: placement_subject) }
-  let(:mock_additional_subjects_step) { instance_double(Placements::AddPlacementWizard::AdditionalSubjectsStep, additional_subjects:) }
-  let(:mock_year_group_step) { instance_double(Placements::AddPlacementWizard::YearGroupStep, year_group:) }
-  let(:mock_mentors_step) { instance_double(Placements::AddPlacementWizard::MentorsStep, mentors:) }
+  let(:mock_additional_subjects_step) { nil }
+  let(:mock_year_group_step) { nil }
+  let(:mock_mentors_step) { nil }
 
   let(:mock_wizard) do
     instance_double(Placements::AddPlacementWizard).tap do |wizard|
@@ -22,9 +22,9 @@ RSpec.describe Placements::AddPlacementWizard::PreviewPlacementStep, type: :mode
   let(:school) { create(:placements_school, phase:) }
   let(:phase) { "Primary" }
   let(:placement_subject) { create(:subject) }
-  let(:additional_subjects) { create_list(:subject, 2, parent_subject: placement_subject) }
-  let(:year_group) { "Year 1" }
-  let(:mentors) { create_list(:placements_mentor, 2) }
+  let(:additional_subjects) { [] }
+  let(:year_group) { nil }
+  let(:mentors) { [] }
 
   describe "delegations" do
     it { is_expected.to delegate_method(:school).to(:wizard) }
@@ -38,14 +38,23 @@ RSpec.describe Placements::AddPlacementWizard::PreviewPlacementStep, type: :mode
     it { is_expected.to have_attributes(subject: placement_subject) }
 
     context "when additional subjects are present" do
+      let(:mock_additional_subjects_step) { instance_double(Placements::AddPlacementWizard::AdditionalSubjectsStep, additional_subjects:) }
+      let(:additional_subjects) { create_list(:subject, 2, parent_subject: placement_subject) }
+
       it { is_expected.to have_attributes(additional_subjects:) }
     end
 
     context "when year group is present" do
+      let(:mock_year_group_step) { instance_double(Placements::AddPlacementWizard::YearGroupStep, year_group:) }
+      let(:year_group) { "Year 1" }
+
       it { is_expected.to have_attributes(year_group:) }
     end
 
     context "when mentors are present" do
+      let(:mock_mentors_step) { instance_double(Placements::AddPlacementWizard::MentorsStep, mentors:) }
+      let(:mentors) { create_list(:placements_mentor, 2) }
+
       it { is_expected.to have_attributes(mentors:) }
     end
   end
