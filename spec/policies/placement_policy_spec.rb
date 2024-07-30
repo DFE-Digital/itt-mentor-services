6e939a121006781ab2b511ebe095816f375beacc
Jamie <jamie.cleare@education.gov.uk>
2024-07-30 09:37:15 +0100

    Prevent school users deleting provider assigned placements

--- spec/policies/placement_policy_spec.rb
+++ spec/policies/placement_policy_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe PlacementPolicy do
   subject(:placement_policy) { described_class }
 
   let(:current_user) { create(:placements_user, schools: [school]) }
-  let(:placement) { Placement.new(school:) }
 
   permissions :new?, :edit_provider?, :edit_mentors?, :edit_year_group?, :update?, :add_placement_journey? do
+    let(:placement) { Placement.new(school:) }
+
     context "when the placement's school has no school contact" do
       let(:school) { create(:placements_school, with_school_contact: false) }
 
@@ -18,8 +19,28 @@ RSpec.describe PlacementPolicy do
     context "when the placement's school has a school contact" do
       let(:school) { create(:placements_school) }
 
-      it "denies access" do
+      it "grants access" do
         expect(placement_policy).to permit(current_user, placement)
+      end
+    end
+  end
+
+  permissions :destroy? do
+    let(:school) { create(:placements_school) }
+    
+    context "when a placement has no provider assigned" do
+      let(:placement) { create(:placement) }
+
+      it "grants access" do
+        expect(placement_policy).to permit(current_user, placement)
+      end
+    end
+
+    context "when a placement has a provider assigned" do
+      let(:placement) { create(:placement, provider: create(:placements_provider)) }
+
+      it "denies access" do
+        expect(placement_policy).not_to permit(current_user, placement)
       end
     end
   end
