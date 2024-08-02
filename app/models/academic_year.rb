1cf7ae37205daa4c2b0d6c415085a55db9fde0ba
dp-daly <danpatdaly@gmail.com>
2024-08-02 12:13:17 +0100

    Added validation to academic year model

--- app/models/academic_year.rb
+++ app/models/academic_year.rb
@@ -10,4 +10,10 @@
 #  updated_at :datetime         not null
 #
 class AcademicYear < ApplicationRecord
+  has_many :claim_windows, class_name: "Claims::ClaimWindow"
+  has_many :claims, through: :claim_windows
+
+  validates :name, presence: true
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true, comparison: { greater_than_or_equal_to: :starts_on }
 end
