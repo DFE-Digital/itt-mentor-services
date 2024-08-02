73795363a174fe342ef7052f298e36082b258265
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-08-02 16:59:32 +0100

    Improve readability of preview step placement

--- app/wizards/placements/add_placement_wizard/preview_placement_step.rb
+++ app/wizards/placements/add_placement_wizard/preview_placement_step.rb
@@ -1,5 +1,7 @@
 class Placements::AddPlacementWizard::PreviewPlacementStep < Placements::BaseStep
   delegate :build_placement, to: :wizard
 
-  alias_method :placement, :build_placement
+  def placement
+    @placement ||= build_placement
+  end
 end
