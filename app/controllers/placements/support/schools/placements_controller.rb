c3b509493471c81b2434f996ec63514a792aab0c
Jamie <jamie.cleare@education.gov.uk>
2024-08-06 14:40:43 +0100

    Prevent support from deleting assigned placements

--- app/controllers/placements/support/schools/placements_controller.rb
+++ app/controllers/placements/support/schools/placements_controller.rb
@@ -1,31 +1,8 @@
-class Placements::Support::Schools::PlacementsController < Placements::ApplicationController
-  before_action :set_school
-  before_action :set_placement, except: %i[index]
-
-  helper_method :edit_attribute_path, :add_provider_path, :add_mentor_path
-
-  def index
-    @pagy, placements = pagy(@school.placements.includes(:subject, :mentors).order("subjects.name"))
-    @placements = placements.decorate
-  end
-
-  def show; end
-
-  def remove; end
-
-  def destroy
-    @placement.destroy!
-    redirect_to placements_support_school_placements_path(@school), flash: { success: t(".placement_removed") }
-  end
-
+class Placements::Support::Schools::PlacementsController < Placements::Schools::PlacementsController
   private
 
   def set_school
     @school = Placements::School.find(params.fetch(:school_id))
-  end
-
-  def set_placement
-    @placement = @school.placements.find(params.fetch(:id)).decorate
   end
 
   def edit_attribute_path(attribute)
@@ -38,5 +15,9 @@ class Placements::Support::Schools::PlacementsController < Placements::Applicati
 
   def add_mentor_path
     placements_support_school_mentors_path
+  end
+
+  def index_path
+    placements_support_school_placements_path(@school)
   end
 end
--- spec/system/placements/support/schools/placements/support_user_deletes_a_placement_spec.rb
+++ spec/system/placements/support/schools/placements/support_user_deletes_a_placement_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Placements / Support / Schools / Placement / Support User removes a placement",
+RSpec.describe "Placements / Support / Schools / Placement / Support User deletes a placement",
                service: :placements, type: :system do
   let(:school) { create(:placements_school, name: "School 1", phase: "Nursery") }
   let(:placement_1) do
@@ -17,19 +17,25 @@ RSpec.describe "Placements / Support / Schools / Placement / Support User remove
   before do
     placement_2
     given_i_sign_in_as_colin
+    when_i_visit_the_support_show_page_for(school, placement_1)
   end
 
-  scenario "Support User removes a placement from a school" do
-    when_i_visit_the_support_show_page_for(school, placement_1)
-    and_i_click_on("Remove placement")
-    then_i_am_asked_to_confirm(placement_1)
+  scenario "Support User deletes a placement from a school" do
+    and_i_click_on("Delete placement")
+    then_i_am_asked_to_confirm(school, placement_1)
     when_i_click_on("Cancel")
     then_i_return_to_placement_page(school, placement_1)
-    when_i_click_on("Remove placement")
-    then_i_am_asked_to_confirm(placement_1)
-    when_i_click_on("Remove placement")
-    then_the_placement_is_removed_from_the_school(school, placement_1)
+    when_i_click_on("Delete placement")
+    then_i_am_asked_to_confirm(school, placement_1)
+    when_i_click_on("Delete placement")
+    then_the_placement_is_deleted_from_the_school(school, placement_1)
     and_a_school_placement_remains_called(placement_2.decorate.title)
+  end
+
+  scenario "User can not delete a placement assigned to a provider" do
+    given_the_placement_is_assigned_to_a_provider(placement_1)
+    and_i_click_on("Delete placement")
+    then_i_see_i_can_not_delete_the_placement(placement_1)
   end
 
   private
@@ -62,12 +68,12 @@ RSpec.describe "Placements / Support / Schools / Placement / Support User remove
   end
   alias_method :and_i_click_on, :when_i_click_on
 
-  def then_i_am_asked_to_confirm(placement)
+  def then_i_am_asked_to_confirm(school, placement)
     organisations_is_selected_in_primary_nav
     expect(page).to have_title(
-      "Are you sure you want to remove this placement? - #{placement.decorate.title} - Manage school placements",
+      "Are you sure you want to delete this placement? - #{placement.decorate.title} - #{school.name} - Manage school placements",
     )
-    expect(page).to have_content "Are you sure you want to remove this placement?"
+    expect(page).to have_content "Are you sure you want to delete this placement?"
   end
 
   def organisations_is_selected_in_primary_nav
@@ -83,12 +89,12 @@ RSpec.describe "Placements / Support / Schools / Placement / Support User remove
                                       ignore_query: true
   end
 
-  def then_the_placement_is_removed_from_the_school(school, placement)
+  def then_the_placement_is_deleted_from_the_school(school, placement)
     organisations_is_selected_in_primary_nav
     placements_is_selected_in_secondary_nav
     expect(school.placements.find_by(id: placement.id)).to be_nil
     within(".govuk-notification-banner__content") do
-      expect(page).to have_content "Placement removed"
+      expect(page).to have_content "Placement deleted"
     end
 
     expect(page).not_to have_content placement.subject.name
@@ -106,5 +112,14 @@ RSpec.describe "Placements / Support / Schools / Placement / Support User remove
 
   def and_a_school_placement_remains_called(placement_name)
     expect(page).to have_content(placement_name)
+  end
+
+  def given_the_placement_is_assigned_to_a_provider(placement)
+    placement.update!(provider: create(:placements_provider))
+  end
+
+  def then_i_see_i_can_not_delete_the_placement(placement)
+    expect(page).to have_content(placement.decorate.title)
+    expect(page).to have_content("You cannot delete this placement")
   end
 end
