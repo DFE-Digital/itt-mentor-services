3a7310bf394047316c33f79692c41e21e8fea977
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-08-13 17:00:59 +0100

    Add schools policy scope

---
+++ spec/policies/school_policy_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe SchoolPolicy do
+  subject(:school_policy) { described_class }
+
+  describe "scope" do
+    let(:scope) { School.all }
+
+    context "when the user is a support user" do
+      let(:user) { create(:placements_support_user) }
+
+      it "returns all schools" do
+        expect(school_policy::Scope.new(user, scope).resolve).to eq(scope)
+      end
+    end
+
+    context "when the user is a school user" do
+      let(:user) { create(:placements_user, schools: [school]) }
+      let(:school) { create(:placements_school) }
+
+      before do
+        user.current_organisation = school
+      end
+
+      it "returns the school's partner providers" do
+        expect(school_policy::Scope.new(user, scope).resolve).to eq(school.partner_providers)
+      end
+    end
+
+    context "when the user is a provider user" do
+      let(:user) { create(:placements_user, providers: [provider]) }
+      let(:provider) { create(:placements_provider) }
+
+      before do
+        user.current_organisation = provider
+      end
+
+      it "returns the provider's partner schools" do
+        expect(school_policy::Scope.new(user, scope).resolve).to eq(provider.partner_schools)
+      end
+    end
+  end
+end
