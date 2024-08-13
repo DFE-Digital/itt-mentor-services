766401069efd31b095612bea25d7d2260d57ed24
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-08-13 17:00:15 +0100

    Add support user policy scope

--- app/policies/placements/support_user_policy.rb
+++ app/policies/placements/support_user_policy.rb
@@ -1,4 +1,14 @@
 class Placements::SupportUserPolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      if user.support_user?
+        scope
+      else
+        scope.none
+      end
+    end
+  end
+
   def destroy?
     user != record
   end
