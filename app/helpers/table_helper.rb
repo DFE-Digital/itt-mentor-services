d1e7af98b312148b5506c2ac7cfee106788abc8f
dp-daly <danpatdaly@gmail.com>
2024-08-13 15:12:50 +0100

    Helper functions reduced to one function to apply secondary colour to 'not yet known'

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -5,11 +5,7 @@ module TableHelper
     end
   end
 
-  def provider_colour(placement)
-    placement.provider_name == I18n.t("placements.schools.placements.not_yet_known") ? "secondary-text" : ""
-  end
-
-  def mentor_colour(placement)
-    placement.mentor_names == I18n.t("placements.schools.placements.not_yet_known") ? "secondary-text" : ""
+  def make_not_yet_known_secondary_colour(placement)
+    placement.provider_name == I18n.t("placements.schools.placements.not_yet_known") || placement.mentor_names == I18n.t("placements.schools.placements.not_yet_known") ? "secondary-text" : ""
   end
 end
