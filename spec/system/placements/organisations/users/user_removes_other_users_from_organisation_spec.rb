260e638dc83c5851deb77b81b614c31e26f3b8cd
dp-daly <danpatdaly@gmail.com>
2024-08-15 14:26:02 +0100

    School user delete another user journey made more consistent (remove -> delete)

--- spec/system/placements/organisations/users/user_removes_other_users_from_organisation_spec.rb
+++ spec/system/placements/organisations/users/user_removes_other_users_from_organisation_spec.rb
@@ -21,13 +21,13 @@ RSpec.describe "Placements support user removes a user from an organisation", se
     scenario "user is removed from a school" do
       given_i_am_signed_in_as_mary
       and_i_visit_the_user_page(anne)
-      when_i_click_on("Remove user")
+      when_i_click_on("Delete user")
       then_i_am_asked_to_confirm(anne, school)
       when_i_click_on("Cancel")
       then_i_return_to_user_page(anne, school)
-      when_i_click_on("Remove user")
+      when_i_click_on("Delete user")
       then_i_am_asked_to_confirm(anne, school)
-      when_i_click_on("Remove user")
+      when_i_click_on("Delete user")
       then_the_the_user_is_removed_from_the_organisation(anne, school)
       and_message_is_sent_to_user(anne, school)
     end
@@ -53,13 +53,13 @@ RSpec.describe "Placements support user removes a user from an organisation", se
     scenario "user is removed from a provider" do
       given_i_am_signed_in_as_mary
       and_i_visit_the_user_page(anne)
-      when_i_click_on("Remove user")
+      when_i_click_on("Delete user")
       then_i_am_asked_to_confirm(anne, provider)
       when_i_click_on("Cancel")
       then_i_return_to_user_page(anne, provider)
-      when_i_click_on("Remove user")
+      when_i_click_on("Delete user")
       then_i_am_asked_to_confirm(anne, provider)
-      when_i_click_on("Remove user")
+      when_i_click_on("Delete user")
       then_the_the_user_is_removed_from_the_organisation(anne, provider)
       and_message_is_sent_to_user(anne, provider)
     end
@@ -130,11 +130,11 @@ RSpec.describe "Placements support user removes a user from an organisation", se
     end
 
     expect(page).to have_title(
-      "Are you sure you want to remove this user? - #{user.full_name} - Manage school placements",
+      "Are you sure you want to delete this user? - #{user.full_name} - Manage school placements",
     )
     expect(page).to have_content user.full_name.to_s
-    expect(page).to have_content "Are you sure you want to remove this user?"
-    expect(page).to have_content "The user will be sent an email to tell them you removed them from #{organisation.name}"
+    expect(page).to have_content "Are you sure you want to delete this user?"
+    expect(page).to have_content "The user will be sent an email to tell them you deleted them from #{organisation.name}"
   end
 
   def users_is_selected_in_schools_primary_nav
