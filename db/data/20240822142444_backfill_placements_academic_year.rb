5a99ead018acb00b970180395f1ca481dbd83e16
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-08-22 15:57:49 +0100

    Backfill the `academic_year_id` column with the current academic year

--- db/data/20240822142444_backfill_placements_academic_year.rb
+++ db/data/20240822142444_backfill_placements_academic_year.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 class BackfillPlacementsAcademicYear < ActiveRecord::Migration[7.1]
   def up
-    Placement.update_all(academic_year: )
+    Placement.update_all(academic_year_id: Placements::AcademicYear.current_academic_year.id)
   end
 
   def down
