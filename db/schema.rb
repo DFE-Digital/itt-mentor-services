1ca9cdeeac1b8a958e7e5578425df5c88af7d6bf
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-08-23 09:50:50 +0100

    Create terms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_150613) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_22_161105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -399,6 +399,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_150613) do
     t.datetime "updated_at", null: false
     t.uuid "parent_subject_id"
     t.index ["parent_subject_id"], name: "index_subjects_on_parent_subject_id"
+  end
+
+  create_table "terms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trusts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
