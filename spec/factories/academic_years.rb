7f704dc10fdb6168edc3662297c7a14a9c2b9c4b
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-08-27 14:59:44 +0100

    Remove the trait from the placement academic years factory

--- spec/factories/academic_years.rb
+++ spec/factories/academic_years.rb
@@ -19,10 +19,8 @@ FactoryBot.define do
   end
 
   factory :placements_academic_year, class: "Placements::AcademicYear", parent: :academic_year do
-    trait :current do
-      starts_on { Date.parse("1 September #{Date.current.year - 1}") }
-      ends_on { Date.parse("31 August #{Date.current.year}") }
-      name { "#{starts_on.year} to #{ends_on.year}" }
-    end
+    starts_on { Date.parse("1 September #{Date.current.year - 1}") }
+    ends_on { Date.parse("31 August #{Date.current.year}") }
+    name { "#{starts_on.year} to #{ends_on.year}" }
   end
 end
