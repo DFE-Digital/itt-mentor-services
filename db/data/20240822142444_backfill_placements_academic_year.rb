330591ee594be5ba4191142bb5105b64208f2b12
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-08-27 14:38:42 +0100

    Switch to update for callbacks, otherwise analytics does not run

--- db/data/20240822142444_backfill_placements_academic_year.rb
+++ db/data/20240822142444_backfill_placements_academic_year.rb
@@ -1,6 +1,8 @@
 class BackfillPlacementsAcademicYear < ActiveRecord::Migration[7.1]
   def up
-    Placement.update_all(academic_year_id: Placements::AcademicYear.current.id)
+    Placement.find_each do |placement|
+      placement.update(academic_year_id: Placements::AcademicYear.current.id)
+    end
   end
 
   def down
