cac850d5d3167b808adee1edb619c2dc8bf85d03
John Ake <johnake@hotmail.com>
2024-08-28 09:19:52 +0100

    Added heartbeat monitor to statuscake

--- terraform/application/statuscake.tf
+++ terraform/application/statuscake.tf
@@ -6,4 +6,7 @@ module "statuscake" {
   uptime_urls    = var.statuscake_alerts.website_url
   ssl_urls       = var.statuscake_alerts.ssl_url
   contact_groups = var.statuscake_alerts.contact_groups
+
+  heartbeat_names  = ["heartbeat-check"]
+  heartbeat_period = 600  # The period in seconds within which a heartbeat must be received
 }
