34c5f2108265cf485ba43b0a2867c3a2cf418ec8
Jamie <jamie.cleare@education.gov.uk>
2024-08-30 17:44:31 +0100

    Stop support users accessing non-support paths

--- app/controllers/placements/application_controller.rb
+++ app/controllers/placements/application_controller.rb
@@ -20,10 +20,16 @@ class Placements::ApplicationController < ApplicationController
   private
 
   def authorize_support_user!
-    user_not_authorized if support_controller? && !current_user.support_user?
+    user_not_authorized if (support_controller? && !current_user.support_user?) ||
+      (!support_controller? && restricted_placements_controller? && current_user.support_user?)
   end
 
   def support_controller?
-    self.class.name.start_with? "Placements::Support::"
+    @support_controller ||= self.class.name.start_with? "Placements::Support::"
+  end
+
+  def restricted_placements_controller?
+    # All users should be able to access routes to the PagesController
+    !instance_of?(::Placements::PagesController)
   end
 end
