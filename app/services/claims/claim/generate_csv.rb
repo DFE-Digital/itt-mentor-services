3c7a26421cab59f3849bf9038672eaf54d6d7c52
dp-daly <danpatdaly@gmail.com>
2024-09-03 14:49:51 +0100

    add status and type of establishment headers and outputs to CSV

--- app/services/claims/claim/generate_csv.rb
+++ app/services/claims/claim/generate_csv.rb
@@ -1,7 +1,7 @@
 require "csv"
 
 class Claims::Claim::GenerateCSV < ApplicationService
-  HEADERS = %w[claim_reference urn school_name local_authority claim_amount establishment_type date_submitted].freeze
+  HEADERS = %w[claim_reference urn school_name local_authority claim_amount type_of_establishment establishment_type date_submitted claim_status].freeze
 
   def initialize(claims:)
     @claims = claims
@@ -18,8 +18,10 @@ class Claims::Claim::GenerateCSV < ApplicationService
           claim.school.name,
           claim.school.local_authority_name,
           claim.amount.format(symbol: false, decimal_mark: ".", no_cents: false),
+          claim.school.type_of_establishment,
           claim.school.group,
           claim.submitted_at&.iso8601,
+          claim.status,
         ]
       end
     end
--- spec/services/claims/claim/generate_csv_spec.rb
+++ spec/services/claims/claim/generate_csv_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Claims::Claim::GenerateCSV do
   let(:claims) { Claims::Claim.all }
 
   before do
-    school1 = create(:claims_school, :claims, name: "School name 1", region: regions(:inner_london), urn: "1234", local_authority_name: "blah", local_authority_code: "BLA", group: "Academy")
-    school2 = create(:claims_school, :claims, name: "School name 2", region: regions(:outer_london), urn: "5678", local_authority_name: "blah", local_authority_code: "BLA", group: "Academy")
-    school3 = create(:claims_school, :claims, name: "School name 3", region: regions(:fringe), urn: "5679", local_authority_name: "blah", local_authority_code: "BLA", group: "Academy")
+    school1 = create(:claims_school, :claims, name: "School name 1", region: regions(:inner_london), urn: "1234", local_authority_name: "blah", local_authority_code: "BLA", type_of_establishment: "Academy converter", group: "Academy")
+    school2 = create(:claims_school, :claims, name: "School name 2", region: regions(:outer_london), urn: "5678", local_authority_name: "blah", local_authority_code: "BLA", type_of_establishment: "Academy converter", group: "Academy")
+    school3 = create(:claims_school, :claims, name: "School name 3", region: regions(:fringe), urn: "5679", local_authority_name: "blah", local_authority_code: "BLA", type_of_establishment: "Academy converter", group: "Academy")
 
     claim1 = create(:claim, status: :submitted, submitted_at: Time.zone.local(2023, 8, 29, 22, 35, 0), school: school1, reference: "12345678")
     claim2 = create(:claim, status: :submitted, submitted_at: Time.zone.local(2023, 8, 29, 22, 35, 0), school: school2, reference: "12345679")
@@ -33,17 +33,17 @@ RSpec.describe Claims::Claim::GenerateCSV do
   end
 
   it "inserts the correct headers" do
-    expect(generate_claims_csv.lines.first.chomp).to eq("claim_reference,urn,school_name,local_authority,claim_amount,establishment_type,date_submitted")
+    expect(generate_claims_csv.lines.first.chomp).to eq("claim_reference,urn,school_name,local_authority,claim_amount,type_of_establishment,establishment_type,date_submitted,claim_status")
   end
 
   it "contains all claims" do
     expect(generate_claims_csv.lines.sort).to eq([
-      "claim_reference,urn,school_name,local_authority,claim_amount,establishment_type,date_submitted\n",
-      "12345670,5679,School name 3,blah,45.10,Academy,\n",
-      "12345671,5679,School name 3,blah,90.20,Academy,2023-08-29T22:35:00Z\n",
-      "12345677,5679,School name 3,blah,676.50,Academy,2023-08-29T22:35:00Z\n",
-      "12345678,1234,School name 1,blah,536.00,Academy,2023-08-29T22:35:00Z\n",
-      "12345679,5678,School name 2,blah,482.50,Academy,2023-08-29T22:35:00Z\n",
+      "claim_reference,urn,school_name,local_authority,claim_amount,type_of_establishment,establishment_type,date_submitted,claim_status\n",
+      "12345670,5679,School name 3,blah,45.10,Academy converter,Academy,,draft\n",
+      "12345671,5679,School name 3,blah,90.20,Academy converter,Academy,2023-08-29T22:35:00Z,submitted\n",
+      "12345677,5679,School name 3,blah,676.50,Academy converter,Academy,2023-08-29T22:35:00Z,submitted\n",
+      "12345678,1234,School name 1,blah,536.00,Academy converter,Academy,2023-08-29T22:35:00Z,submitted\n",
+      "12345679,5678,School name 2,blah,482.50,Academy converter,Academy,2023-08-29T22:35:00Z,submitted\n",
     ].sort)
   end
 end
