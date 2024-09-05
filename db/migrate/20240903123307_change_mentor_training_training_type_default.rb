dde622d3afe26ec84250de8d4eabbfcbc3801f43
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-09-05 11:17:23 +0100

    Refactor data migration to be part of the column default change
    Creates a representation of the MentorTraining model so that this migration can be run regardless of what the model is called in the future.

--- db/migrate/20240903123307_change_mentor_training_training_type_default.rb
+++ db/migrate/20240903123307_change_mentor_training_training_type_default.rb
@@ -1,5 +1,16 @@
 class ChangeMentorTrainingTrainingTypeDefault < ActiveRecord::Migration[7.1]
+  class MentorTrainingForMigration < ApplicationRecord
+    self.table_name = :mentor_trainings
+  end
+
   def change
     change_column_default :mentor_trainings, :training_type, from: nil, to: "initial"
+
+    up_only do
+      # We require callbacks for BigQuery to stay in sync with the data.
+      MentorTrainingForMigration.where(training_type: nil).find_each do |mentor_training|
+        mentor_training.update(training_type: Claims::MentorTraining.training_types[:initial])
+      end
+    end
   end
 end
--- db/migrate/20240903123622_back_fill_mentor_training_training_type.rb
+++
@@ -1,8 +0,0 @@
-class BackFillMentorTrainingTrainingType < ActiveRecord::Migration[7.1]
-  def up
-    # We require callbacks for BigQuery to stay in sync with the data.
-    Claims::MentorTraining.find_each do |mentor_training|
-      mentor_training.update(training_type: Claims::MentorTraining.training_types[:initial])
-    end
-  end
-end
