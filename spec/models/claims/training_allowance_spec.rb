1475ef85193b664651d81bc15d0975179da168f5
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-09-10 12:32:58 +0100

    Refactor training_allowance_spec.rb
    Move away from testing individual methods and instead testing scenarios as all of the results are connected, this makes it easier to understand what this class is trying to achieve.

--- spec/models/claims/training_allowance_spec.rb
+++ spec/models/claims/training_allowance_spec.rb
@@ -1,16 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Claims::TrainingAllowance, type: :model do
-  subject(:training_allowance) { described_class.new(mentor:, provider:, academic_year:) }
+  subject(:training_allowance) { described_class.new(mentor:, provider:, academic_year:, claim_to_exclude:) }
 
-    let!(:mentor) { create(:claims_mentor) }
-    let!(:academic_year) do
-      create(:academic_year,
-             starts_on: Date.parse("1 September 2019"),
-             ends_on: Date.parse("31 August 2020"),
-             name: "2019 to 2020")
-    end
-    let!(:provider) { create(:claims_provider) }
+  let(:claim_to_exclude) { nil }
   let!(:mentor) { create(:claims_mentor) }
   let!(:provider) { create(:claims_provider) }
   let!(:academic_year) do
@@ -26,176 +19,149 @@ RSpec.describe Claims::TrainingAllowance, type: :model do
            name: "2018 to 2019")
   end
 
-  describe "#training_type" do
-    context "when the mentor has not received training in the previous academic year" do
-      it "returns initial" do
-        expect(training_allowance.training_type).to eq(:initial)
-      end
-    end
-
-    context "when the mentor has received training in the previous academic year" do
-      let(:claim_window) { build(:claim_window, :current, academic_year: previous_academic_year) }
-      let(:claim) { build(:claim, :submitted, claim_window:) }
-      let(:mentor_training) { create(:mentor_training, mentor:, provider:, claim:) }
-
-      before { mentor_training }
-
-      it "returns refresher" do
-        expect(training_allowance.training_type).to eq(:refresher)
-      end
-    end
-
-    context "when the provider has trained a different mentor for the academic year" do
-      let(:claim_window) { build(:claim_window, :current, academic_year:) }
-      let(:claim) { build(:claim, :submitted, claim_window:) }
-      let(:mentor_training) { create(:mentor_training, provider:, claim:) }
-
-      before { mentor_training }
-
-      it "returns initial" do
-        expect(training_allowance.training_type).to eq(:initial)
-      end
-    end
-  end
-
-  describe "#total_hours" do
-    context "when the mentor has received initial training" do
-      it "returns 20" do
-        expect(training_allowance.total_hours).to eq(20)
-      end
-    end
-
-    context "when the mentor has received refresher training" do
-      let(:claim_window) { build(:claim_window, :current, academic_year: previous_academic_year) }
-      let(:claim) { build(:claim, :submitted, claim_window:) }
-      let(:mentor_training) { create(:mentor_training, mentor:, provider:, claim:) }
-
-      before do
-        mentor_training
-      end
-
-      it "returns 6" do
-        expect(training_allowance.total_hours).to eq(6)
-      end
-    end
-  end
-
   describe "initial training" do
     let(:claim_window) { build(:claim_window, :current, academic_year:) }
     let(:claim) { build(:claim, :submitted, claim_window:) }
-    let(:hours_completed) { 5 }
     let(:mentor_training) { create(:mentor_training, mentor:, provider:, claim:, hours_completed:) }
 
-    before { mentor_training }
+    context "when the mentor has not completed any training for the academic year" do
+      it "returns the expected results" do
+        expect(training_allowance.training_type).to eq(:initial)
+        expect(training_allowance.total_hours).to eq(20)
+        expect(training_allowance.remaining_hours).to eq(20)
+        expect(training_allowance).to be_available
+      end
+    end
 
-    it "returns 15" do
-      expect(training_allowance.training_type).to eq(:initial)
-      expect(training_allowance.total_hours).to eq(20)
-      expect(training_allowance.remaining_hours).to eq(15)
-      expect(training_allowance).to be_available
+    context "when the mentor has completed training for the academic year" do
+      before { mentor_training }
+
+      context "when the mentor has completed 5 hours of training in the current academic year" do
+        let(:hours_completed) { 5 }
+
+        it "returns the expected results" do
+          expect(training_allowance.training_type).to eq(:initial)
+          expect(training_allowance.total_hours).to eq(20)
+          expect(training_allowance.remaining_hours).to eq(15)
+          expect(training_allowance).to be_available
+        end
+      end
+
+      context "when the mentor has completed 20 hours of training in the current academic year" do
+        let(:hours_completed) { 20 }
+
+        it "returns the expected results" do
+          expect(training_allowance.training_type).to eq(:initial)
+          expect(training_allowance.total_hours).to eq(20)
+          expect(training_allowance.remaining_hours).to eq(0)
+          expect(training_allowance).not_to be_available
+        end
+      end
+
+      context "when the mentor has completed 10 hours of training in the current academic year for a claim they want to exclude" do
+        let(:hours_completed) { 10 }
+        let(:claim_to_exclude) { claim }
+
+        it "returns the expected results" do
+          expect(training_allowance.training_type).to eq(:initial)
+          expect(training_allowance.total_hours).to eq(20)
+          expect(training_allowance.remaining_hours).to eq(20)
+          expect(training_allowance).to be_available
+        end
+
+        context "when a second claim has been made whilst excluding a previous claim" do
+          let(:second_claim) { build(:claim, :submitted, claim_window:, provider:) }
+          let(:second_mentor_training) { create(:mentor_training, mentor:, provider:, claim: second_claim, hours_completed: 3) }
+
+          before do
+            second_mentor_training
+          end
+
+          it "does not exclude other claims" do
+            expect(training_allowance.training_type).to eq(:initial)
+            expect(training_allowance.total_hours).to eq(20)
+            expect(training_allowance.remaining_hours).to eq(17)
+            expect(training_allowance).to be_available
+          end
+        end
+      end
     end
   end
-  
+
   describe "refresher training" do
     let(:claim_window) { build(:claim_window, :current, academic_year:) }
     let(:claim) { build(:claim, :submitted, claim_window:, provider:) }
-    let(:hours_completed) { 5 }
     let(:mentor_training) { create(:mentor_training, mentor:, provider:, claim:, hours_completed:) }
 
     let(:previous_claim_window) { build(:claim_window, starts_on: 8.days.ago, ends_on: 4.days.ago, academic_year: previous_academic_year) }
     let(:previous_claim) { build(:claim, :submitted, claim_window: previous_claim_window, provider:) }
-    let(:previous_mentor_training) { create(:mentor_training, mentor:, provider:, claim: previous_claim, hours_completed: nil) }
+    let(:previous_mentor_training) { create(:mentor_training, mentor:, provider:, claim: previous_claim, hours_completed: 20) }
 
-    before do
-      mentor_training
-      previous_mentor_training
+    context "when the mentor has not completed any training for the academic year" do
+      before { previous_mentor_training }
+
+      it "returns the expected results" do
+        expect(training_allowance.training_type).to eq(:refresher)
+        expect(training_allowance.total_hours).to eq(6)
+        expect(training_allowance.remaining_hours).to eq(6)
+        expect(training_allowance).to be_available
+      end
     end
 
-    it "returns 15" do
-      expect(training_allowance.training_type).to eq(:refresher)
-      expect(training_allowance.total_hours).to eq(6)
-      expect(training_allowance.remaining_hours).to eq(1)
-      expect(training_allowance).to be_available
-    end
-  end
+    context "when the mentor has completed training for the academic year" do
+      before do
+        mentor_training
+        previous_mentor_training
+      end
 
-  describe "#remaining_hours" do
-    let(:claim_window) { build(:claim_window, :current, academic_year: previous_academic_year) }
-    let(:claim) { build(:claim, :submitted, claim_window:) }
-    let(:mentor_training) { create(:mentor_training, mentor:, provider:, claim:, hours_completed:) }
+      context "when the mentor has completed 5 hours of training in the current academic year and 20 hours in the previous academic year" do
+        let(:hours_completed) { 5 }
 
-    context "with initial training" do
-      context "when no hours have been completed" do
-        let(:hours_completed) { nil }
-
-        before { mentor_training }
-
-        it "returns 20" do
-          expect(training_allowance.remaining_hours).to eq(20)
+        it "returns the expected results" do
+          expect(training_allowance.training_type).to eq(:refresher)
+          expect(training_allowance.total_hours).to eq(6)
+          expect(training_allowance.remaining_hours).to eq(1)
+          expect(training_allowance).to be_available
         end
       end
 
-      context "when some of the hours have been completed" do
-        let(:hours_completed) { 13 }
+      context "when the mentor has completed 6 hours of training in the current academic year and 20 hours in the previous academic year" do
+        let(:hours_completed) { 6 }
 
-        before { mentor_training }
-
-        it "returns 7" do
-          expect(training_allowance.remaining_hours).to eq(7)
-        end
-      end
-
-      context "when all of the hours have been completed" do
-        let(:hours_completed) { 20 }
-
-        before { mentor_training }
-
-        it "returns 0" do
+        it "returns the expected results" do
+          expect(training_allowance.training_type).to eq(:refresher)
+          expect(training_allowance.total_hours).to eq(6)
           expect(training_allowance.remaining_hours).to eq(0)
+          expect(training_allowance).not_to be_available
         end
       end
 
-      context "when a claim has been provided to be excluded" do
-        subject(:training_allowance) do
-          described_class.new(mentor:, provider:, academic_year:)
-        end
+      context "when the mentor has completed 3 hours of training in the current academic year and 20 hours in the previous academic year for a claim they want to exclude" do
+        let(:hours_completed) { 6 }
+        let(:claim_to_exclude) { claim }
 
-        let(:excluded_claim) { create(:claim, :submitted, claim_window:) }
-        let(:excluded_mentor_training) do
-          create(:mentor_training, mentor:, provider:, claim: excluded_claim, hours_completed: 5)
-        end
-
-        before { excluded_mentor_training }
-
-        it "does not include the hours from the excluded claim" do
+        it "excludes the excluded claim" do
+          expect(training_allowance.training_type).to eq(:refresher)
+          expect(training_allowance.total_hours).to eq(6)
           expect(training_allowance.remaining_hours).to eq(6)
+          expect(training_allowance).to be_available
         end
-      end
-    end
-  end
 
-  describe "#available?" do
-    let(:claim_window) { build(:claim_window, :current, academic_year: previous_academic_year) }
-    let(:claim) { build(:claim, :submitted, claim_window:) }
-    let(:mentor_training) { create(:mentor_training, mentor:, provider:, claim:, hours_completed:) }
+        context "when a second claim has been made whilst excluding a previous claim" do
+          let(:second_claim) { build(:claim, :submitted, claim_window:, provider:) }
+          let(:second_mentor_training) { create(:mentor_training, mentor:, provider:, claim: second_claim, hours_completed: 3) }
 
-    context "when there are remaining hours" do
-      let(:hours_completed) { 13 }
+          before do
+            second_mentor_training
+          end
 
-      before { mentor_training }
-
-      it "returns true" do
-        expect(training_allowance.available?).to be(true)
-      end
-    end
-
-    context "when there are no remaining hours" do
-      let(:hours_completed) { 20 }
-
-      before { mentor_training }
-
-      it "returns false" do
-        expect(training_allowance.available?).to be(false)
+          it "does not exclude other claims" do
+            expect(training_allowance.training_type).to eq(:refresher)
+            expect(training_allowance.total_hours).to eq(6)
+            expect(training_allowance.remaining_hours).to eq(3)
+            expect(training_allowance).to be_available
+          end
+        end
       end
     end
   end
