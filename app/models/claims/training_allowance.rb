d1fe4fa0afdc5922aa60b7895a5ffb0a16e7c148
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-09-10 12:33:24 +0100

    Use constants for symbol values and remove redundant logic

--- app/models/claims/training_allowance.rb
+++ app/models/claims/training_allowance.rb
@@ -7,15 +7,11 @@ class Claims::TrainingAllowance
   end
 
   def training_type
-    has_initial_training? ? :refresher : :initial
+    has_initial_training? ? REFRESHER_TRAINING : INITIAL_TRAINING
   end
 
   def total_hours
-    if training_type == :initial
-      20
-    elsif training_type == :refresher
-      6
-    end
+    training_type == INITIAL_TRAINING ? 20 : 6
   end
 
   def remaining_hours
@@ -30,27 +26,26 @@ class Claims::TrainingAllowance
 
   attr_reader :mentor, :provider, :academic_year, :claim_to_exclude
 
+  INITIAL_TRAINING = :initial
+  REFRESHER_TRAINING = :refresher
+
+  private_constant :INITIAL_TRAINING, :REFRESHER_TRAINING
+
   def mentor_training_scope
     @mentor_training_scope ||= provider.mentor_trainings
                                        .where(mentor:)
                                        .joins(claim: { claim_window: :academic_year })
   end
 
-  def has_initial_training?(previous: true)
-    query = if previous
-              mentor_training_scope.where(claims: { status: :submitted, claim_windows: { academic_years: { ends_on: ..academic_year.starts_on } } })
-            else
-              mentor_training_scope.where(claims: { status: :submitted, claim_windows: { academic_year: } })
-            end
-
-    query.exists?
+  def has_initial_training?
+    mentor_training_scope.where(claims: { status: :submitted, claim_windows: { academic_years: { ends_on: ..academic_year.starts_on } } }).exists?
   end
 
   def hours_completed
     mentor_training_scope
       .where(claims: { status: :submitted, claim_windows: { academic_year: } })
       .merge(Claims::Claim.active)
-      .where.not(claim_id: [claim_to_exclude.id, claim_to_exclude&.previous_revision_id])
+      .where.not(claim_id: [claim_to_exclude&.id, claim_to_exclude&.previous_revision_id])
       .sum(:hours_completed)
   end
 end
