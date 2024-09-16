1046d8cff14cd197ade609626d8e33ae3f9042db
Daniel Dye <daniel.dye@education.gov.uk>
2024-09-16 09:43:42 +0100

    Update Rails application config defaults to 7.2
    Set `config.action_controller.raise_on_missing_callback_actions = false`. This is
    because we currently set `after_action :verify_policy_scoped, only: %i[index]` in
    the `Placements::ApplicationController`. If left as the default, `true`, many
    placements system specs will fail.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,12 @@ Bundler.require(*Rails.groups)
 module IttMentorServices
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.2
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -30,7 +35,6 @@ module IttMentorServices
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.autoload_lib(ignore: %w[])
 
     config.assets.paths << Rails.root.join(
       "node_modules/govuk-frontend/dist/govuk/assets",
