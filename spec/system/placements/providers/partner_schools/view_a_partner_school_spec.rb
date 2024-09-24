b933faf28b2b7f49d1bd883b852e62e2ca4fbf13
dp-daly <danpatdaly@gmail.com>
2024-09-24 09:28:45 +0100

    merged organisation details tables

--- spec/system/placements/providers/partner_schools/view_a_partner_school_spec.rb
+++ spec/system/placements/providers/partner_schools/view_a_partner_school_spec.rb
@@ -38,12 +38,15 @@ RSpec.describe "Placements / Providers / Partner schools / Views a partner schoo
     expect(page).to have_content "Additional details"
     expect(page).to have_content "Special educational needs and disabilities (SEND)"
     expect(page).to have_content "Ofsted"
-    expect(page).to have_content "Contact details"
 
     within("#organisation-details") do
       expect(page).to have_content "Name"
       expect(page).to have_content "UK provider reference number (UKPRN)"
       expect(page).to have_content "Unique reference number (URN)"
+      expect(page).to have_content "Email address"
+      expect(page).to have_content "Telephone number"
+      expect(page).to have_content "Website"
+      expect(page).to have_content "Address"
     end
 
     within("#additional-details") do
@@ -69,13 +72,6 @@ RSpec.describe "Placements / Providers / Partner schools / Views a partner schoo
     within("#ofsted-details") do
       expect(page).to have_content "Rating"
       expect(page).to have_content "Last inspection date"
-    end
-
-    within("#contact-details") do
-      expect(page).to have_content "Email address"
-      expect(page).to have_content "Telephone"
-      expect(page).to have_content "Website"
-      expect(page).to have_content "Address"
     end
   end
 
