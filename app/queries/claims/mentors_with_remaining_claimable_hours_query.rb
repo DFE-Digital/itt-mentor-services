d58d55124067a4b6c542a9956ce3cb17f90fae35
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-09-25 12:02:30 +0100

    Refactor remaining claimable hours to use training allowance

--- app/queries/claims/mentors_with_remaining_claimable_hours_query.rb
+++ app/queries/claims/mentors_with_remaining_claimable_hours_query.rb
@@ -9,9 +9,13 @@ class Claims::MentorsWithRemainingClaimableHoursQuery < ApplicationQuery
 
   def remaining_claimable_hours_condition(scope)
     mentor_with_claimable_hours = scope.filter do |mentor|
-      Claims::Mentor::CalculateRemainingMentorTrainingHoursForProvider.call(mentor:, provider: params[:provider], claim: params[:claim]).positive?
+      training_allowance_for(mentor).available?
     end
 
     scope.where(id: mentor_with_claimable_hours)
+  end
+
+  def training_allowance_for(mentor)
+    Claims::TrainingAllowance.new(mentor:, provider: params[:provider], academic_year: Claims::ClaimWindow.current.academic_year)
   end
 end
