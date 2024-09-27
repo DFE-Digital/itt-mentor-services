a3d8f7e08b064c3c1aa9c0a8185d5737d617a194
Ollie Treend <ollie@treend.uk>
2024-09-27 17:07:14 +0100

    Add database migration for GoodJob
    GoodJob 4.3.0 includes a new database migration which needed applying.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_13_144518) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_27_160048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_13_144518) do
     t.datetime "enqueued_at"
     t.datetime "discarded_at"
     t.datetime "finished_at"
+    t.datetime "jobs_finished_at"
   end
 
   create_table "good_job_executions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
