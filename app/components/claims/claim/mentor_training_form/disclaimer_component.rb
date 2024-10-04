edd127ac32b000890339470ccd034082b9c037ef
Ollie Treend <ollie@treend.uk>
2024-10-04 18:42:35 +0100

    Move logic closer to the code that needs it
    I've relocated the logic that was originally defined as
    `Claims::Claim::MentorTrainingForm#max_hours_equals_maximum_claimable_hours?`
    because it was only needed and used in the component
    `Claims::Claim::MentorTrainingForm::DisclaimerComponent`.
    
    The component spec already covered the required behaviour, so I was able
    to simply move the code and cut out an unnecessary layer of abstraction.

--- app/components/claims/claim/mentor_training_form/disclaimer_component.rb
+++ app/components/claims/claim/mentor_training_form/disclaimer_component.rb
@@ -1,6 +1,8 @@
 class Claims::Claim::MentorTrainingForm::DisclaimerComponent < ApplicationComponent
   attr_reader :mentor_training_form
 
+  delegate :training_allowance, to: :mentor_training_form
+
   def initialize(mentor_training_form:, classes: [], html_attributes: {})
     super(classes:, html_attributes:)
 
@@ -9,13 +11,13 @@ class Claims::Claim::MentorTrainingForm::DisclaimerComponent < ApplicationCompon
 
   def call
     govuk_inset_text do
-      tag.p(sanitize(t(".disclaimer", mentor_name:, provider_name:, count: mentor_training_form.max_hours))) +
+      tag.p(sanitize(t(".disclaimer", mentor_name:, provider_name:, count: training_allowance.remaining_hours))) +
         tag.p(sanitize(t(".disclaimer_contact", support_link:)))
     end
   end
 
   def render?
-    !mentor_training_form.max_hours_equals_maximum_claimable_hours?
+    training_allowance.remaining_hours < training_allowance.total_hours
   end
 
   private
