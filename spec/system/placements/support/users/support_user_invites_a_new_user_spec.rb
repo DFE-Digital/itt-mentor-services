c87e7827f6a021cc229c09ff7d281363dadd674f
Jamie <jamie.cleare@education.gov.uk>
2024-10-14 14:24:54 +0100

    Resolve rebase issues

--- spec/system/placements/support/users/support_user_invites_a_new_user_spec.rb
+++ spec/system/placements/support/users/support_user_invites_a_new_user_spec.rb
@@ -255,22 +255,9 @@ RSpec.describe "Placements / Support / Users / Support User Invites A New User",
   end
 
   def then_i_see_support_navigation_with_organisation_selected
-<<<<<<< HEAD
-<<<<<<< HEAD
     within(".govuk-header__navigation-list") do
       expect(page).to have_link "Organisations"
       expect(page).to have_link "Support users"
     end
-=======
-    # TODO: Re-added in https://github.com/DFE-Digital/itt-mentor-services/pull/1085 
-=======
-    # TODO: Re-added in https://github.com/DFE-Digital/itt-mentor-services/pull/1085
->>>>>>> ddfffe1e (Fix linter errors)
-    # when the nav is moved to the service header
-    # within(".app-primary-navigation__nav") do
-    #   expect(page).to have_link "Organisations", current: "page"
-    #   expect(page).to have_link "Support users", current: "false"
-    # end
->>>>>>> f59e3c25 (Test coverage for moving support users to multi org views)
   end
 end
