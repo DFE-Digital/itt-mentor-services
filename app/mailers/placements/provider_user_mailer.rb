50d0f617b64b65709bdec720ac15c6fb707d360e
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-10-21 16:38:42 +0100

    Convert existing mailer calls to use new classes
    Removes service param from mailer calls and replaces it with the appropriate mailer class. Adds a method to return the correct mailer class in service objects and refactors tests to accommodate this change.

--- app/mailers/placements/provider_user_mailer.rb
+++ app/mailers/placements/provider_user_mailer.rb
@@ -1,4 +1,30 @@
-class Placements::ProviderUserMailer < Placements::UserMailer
+class Placements::ProviderUserMailer < Placements::ApplicationMailer
+  def user_membership_created_notification(user, organisation)
+    notify_email to: user.email,
+                 subject: t(".subject", service_name:),
+                 body: t(
+                   ".body",
+                   user_name: user.first_name,
+                   organisation_name: organisation.name,
+                   service_name:,
+                   heading: t(".heading"),
+                   support_email:,
+                   sign_in_url:,
+                 )
+  end
+
+  def user_membership_destroyed_notification(user, organisation)
+    notify_email to: user.email,
+                 subject: t(".subject", service_name:),
+                 body: t(
+                   ".body",
+                   user_name: user.first_name,
+                   organisation_name: organisation.name,
+                   service_name:,
+                   support_email:,
+                 )
+  end
+
   def partnership_created_notification(user, source_organisation, partner_organisation)
     notify_email(
       to: user.email,
@@ -30,15 +56,14 @@ class Placements::ProviderUserMailer < Placements::UserMailer
   def placement_provider_assigned_notification(user, provider, placement)
     notify_email(
       to: user.email,
-      subject: t(".subject", school_name: placement.school.name),
+      subject: t(".subject", school_name: school(placement).name),
       body: t(
         ".body",
         provider_name: provider.name,
         placement_name: placement.decorate.title,
-        school_name: placement.school.name,
-        school_email: placement.school.school_contact.email_address,
+        school_name: school(placement).name,
+        school_email: school(placement).school_contact.email_address,
         link: placements_provider_placement_url(provider, placement),
-        service_name:,
       ),
     )
   end
@@ -46,16 +71,21 @@ class Placements::ProviderUserMailer < Placements::UserMailer
   def placement_provider_removed_notification(user, provider, placement)
     notify_email(
       to: user.email,
-      subject: t(".subject", school_name: placement.school.name),
+      subject: t(".subject", school_name: school(placement).name),
       body: t(
         ".body",
         provider_name: provider.name,
         placement_name: placement.decorate.title,
-        school_name: placement.school.name,
-        school_email: placement.school.school_contact.email_address,
+        school_name: school(placement).name,
+        school_email: school(placement).school_contact.email_address,
         link: placements_provider_placement_url(provider, placement),
-        service_name:,
       ),
     )
+  end
+
+  private
+
+  def school(placement)
+    @school ||= placement.school
   end
 end
