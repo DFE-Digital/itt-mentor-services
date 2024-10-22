8e3757246c4cc2b61323c28914148690861e9003
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-10-22 17:19:38 +0100

    Convert school_user_mail partnership_created_notification mailer to text.erb

--- app/mailers/placements/school_user_mailer.rb
+++ app/mailers/placements/school_user_mailer.rb
@@ -6,17 +6,14 @@ class Placements::SchoolUserMailer < Placements::UserMailer
   end
 
   def partnership_created_notification(user, source_organisation, partner_organisation)
-    notify_email(
-      to: user.email,
-      subject: t(".subject", organisation: partner_organisation.name),
-      body: t(
-        ".body",
-        user_name: user.full_name,
-        source_organisation: source_organisation.name,
-        partner_organisation: partner_organisation.name,
-        link: placements_school_partner_providers_url(partner_organisation),
-      ),
-    )
+    @user_name = user.first_name
+    @school_name = partner_organisation.name
+    @provider_name = source_organisation.name
+    @provider_email_address = source_organisation.email_address
+    @sign_in_url = sign_in_url
+    @service_name = service_name
+
+    notify_email to: user.email, subject: t(".subject")
   end
 
   def partnership_destroyed_notification(user, source_organisation, partner_organisation)
@@ -32,4 +29,5 @@ class Placements::SchoolUserMailer < Placements::UserMailer
       ),
     )
   end
+
 end
