7c859d4d597fdbf4574357d25e3b88b179676900
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-10-24 16:06:40 +0100

    Moved slack url to translations and removed redundant error

--- app/mailers/placements/support_user_mailer.rb
+++ app/mailers/placements/support_user_mailer.rb
@@ -6,7 +6,7 @@ class Placements::SupportUserMailer < Placements::ApplicationMailer
       user_name: support_user.first_name,
       service_name:,
       sign_in_url:,
-      slack_url:,
+      slack_url: t("placements.support_slack_url"),
     )
 
     notify_email to: support_user.email, subject:, body:
@@ -18,15 +18,9 @@ class Placements::SupportUserMailer < Placements::ApplicationMailer
       ".body",
       user_name: support_user.first_name,
       service_name:,
-      slack_url:,
+      slack_url: t("placements.support_slack_url"),
     )
 
     notify_email to: support_user.email, subject:, body:
-  end
-
-  private
-
-  def slack_url
-    "https://ukgovernmentdfe.slack.com/archives/C04MLBVP876"
   end
 end
