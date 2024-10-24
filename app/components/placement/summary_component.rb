bc706cb271fc054ba3c7142322b53a0d8cbb5f2c
dp-daly <danpatdaly@gmail.com>
2024-10-24 14:49:50 +0100

    Placements::TravelTime call moved to placements controller for optimisation

--- app/components/placement/summary_component.rb
+++ app/components/placement/summary_component.rb
@@ -1,30 +1,18 @@
 class Placement::SummaryComponent < ApplicationComponent
   with_collection_parameter :placement
-  attr_reader :placement, :school, :provider, :location_coordinates, :search_location
+  attr_reader :placement, :school, :provider, :location_coordinates
 
-  def initialize(provider:, placement:, location_coordinates: nil, search_location: nil, classes: [], html_attributes: {})
+  def initialize(provider:, placement:, location_coordinates: nil, classes: [], html_attributes: {})
     super(classes:, html_attributes:)
 
     @provider = provider
     @placement = placement.decorate
     @school = @placement.school
     @location_coordinates = location_coordinates
-    @search_location = search_location
-
-    calculate_travel_time
   end
 
   def distance_from_location
     distance = @school.distance_to(@location_coordinates).round(1)
     I18n.t("components.placement.summary_component.distance_in_miles", distance:)
-  end
-
-  def calculate_travel_time
-    return if search_location.blank?
-
-    @calculate_travel_time ||= Placements::TravelTime.call(
-      origin_address: search_location,
-      destinations: [@school],
-    )
   end
 end
