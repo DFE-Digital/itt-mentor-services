039cfae06180b2298eb38e338db2ba36cd7cf37f
Jamie <jamie.cleare@education.gov.uk>
2024-10-28 10:40:49 +0000

    Use slack translation

--- app/mailers/placements/provider_user_mailer.rb
+++ app/mailers/placements/provider_user_mailer.rb
@@ -1,30 +1,4 @@
-class Placements::ProviderUserMailer < Placements::ApplicationMailer
-  def user_membership_created_notification(user, organisation)
-    notify_email to: user.email,
-                 subject: t(".subject", service_name:),
-                 body: t(
-                   ".body",
-                   user_name: user.first_name,
-                   organisation_name: organisation.name,
-                   service_name:,
-                   heading: t(".heading"),
-                   support_email:,
-                   sign_in_url:,
-                 )
-  end
-
-  def user_membership_destroyed_notification(user, organisation)
-    notify_email to: user.email,
-                 subject: t(".subject", service_name:),
-                 body: t(
-                   ".body",
-                   user_name: user.first_name,
-                   organisation_name: organisation.name,
-                   service_name:,
-                   support_email:,
-                 )
-  end
-
+class Placements::ProviderUserMailer < Placements::UserMailer
   def partnership_created_notification(user, source_organisation, partner_organisation)
     @user_name = user.first_name
     @school_name = source_organisation.name
--- app/mailers/placements/support_user_mailer.rb
+++ app/mailers/placements/support_user_mailer.rb
@@ -3,7 +3,7 @@ class Placements::SupportUserMailer < Placements::ApplicationMailer
     @user_name = support_user.first_name
     @service_name = service_name
     @sign_in_url = sign_in_url
-    @slack_url = slack_url
+    @slack_url = t("placements.support_slack_url")
 
     notify_email to: support_user.email, subject: t(".subject", service_name:)
   end
@@ -11,7 +11,7 @@ class Placements::SupportUserMailer < Placements::ApplicationMailer
   def support_user_removal_notification(support_user)
     @user_name = support_user.first_name
     @service_name = service_name
-    @slack_url = slack_url
+    @slack_url = t("placements.support_slack_url")
 
     notify_email to: support_user.email, subject: t(".subject", service_name:)
   end
