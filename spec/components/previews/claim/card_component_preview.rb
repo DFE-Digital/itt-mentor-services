0454a41ba886de4e9f489d0bba417f88ce97ee96
Jamie <jamie.cleare@education.gov.uk>
2024-10-29 09:51:34 +0000

    Fixes for componenent previews

--- spec/components/previews/claim/card_component_preview.rb
+++ spec/components/previews/claim/card_component_preview.rb
@@ -1,11 +1,7 @@
 class Claim::CardComponentPreview < ApplicationComponentPreview
   def default
+    claim = FactoryBot.build(:claim, :submitted, id: SecureRandom.uuid)
+
     render Claim::CardComponent.new(claim:)
-  end
-
-  private
-
-  def claim
-    Claims::Claim.submitted.first
   end
 end
--- spec/components/previews/placement/status_tag_component_preview.rb
+++ spec/components/previews/placement/status_tag_component_preview.rb
@@ -1,6 +1,15 @@
 class Placement::StatusTagComponentPreview < ApplicationComponentPreview
-  def default
-    placement = create(:placement)
+  def placement_without_provider
+    school = FactoryBot.build(:placements_school, with_school_contact: false)
+    placement = FactoryBot.build(:placement, school:)
+
+    render(Placement::StatusTagComponent.new(placement:))
+  end
+
+  def placement_with_provider
+    provider = FactoryBot.build(:placements_provider)
+    school = FactoryBot.build(:placements_school, with_school_contact: false)
+    placement = FactoryBot.build(:placement, school:, provider:)
 
     render(Placement::StatusTagComponent.new(placement:))
   end
