b5da6c0c2774c52b2c888eb4e78dcfdab0bc1187
dp-daly <danpatdaly@gmail.com>
2024-11-06 16:37:30 +0000

    Claims -> Support user: refactor add mentor form to use wizard pattern

--- app/controllers/claims/support/schools/mentors_controller.rb
+++ app/controllers/claims/support/schools/mentors_controller.rb
@@ -1,32 +1,15 @@
 class Claims::Support::Schools::MentorsController < Claims::Support::ApplicationController
   include Claims::BelongsToSchool
 
+  before_action :has_school_accepted_grant_conditions?
   before_action :set_mentor, only: %i[show remove destroy]
   before_action :set_mentor_membership, only: %i[remove destroy]
 
   before_action :authorize_mentor
   before_action :authorize_mentor_membership, only: %i[remove destroy]
 
-  helper_method :mentor_form
-
   def index
     @pagy, @mentors = pagy(@school.mentors.order_by_full_name)
-  end
-
-  def new; end
-
-  def create
-    mentor_form.save!
-
-    redirect_to claims_support_school_mentors_path(@school), flash: {
-      heading: t(".success"),
-    }
-  end
-
-  def check
-    render :new unless mentor_form.valid?
-  rescue TeachingRecord::RestClient::TeacherNotFoundError
-    render :no_results
   end
 
   def show; end
@@ -34,8 +17,7 @@ class Claims::Support::Schools::MentorsController < Claims::Support::Application
   def remove; end
 
   def destroy
-    mentor_membership = @mentor.mentor_memberships.find_by!(school: @school)
-    mentor_membership.destroy!
+    @mentor_membership.destroy!
 
     redirect_to claims_support_school_mentors_path(@school), flash: {
       heading: t(".success"),
@@ -62,20 +44,5 @@ class Claims::Support::Schools::MentorsController < Claims::Support::Application
 
   def default_params
     { school: @school }
-  end
-
-  def mentor_params
-    params.require(:claims_mentor_form)
-          .permit(:first_name, :last_name, :trn, :date_of_birth)
-          .merge(default_params)
-  end
-
-  def mentor_form
-    @mentor_form ||=
-      if params[:claims_mentor_form].present?
-        Claims::MentorForm.new(mentor_params)
-      else
-        Claims::MentorForm.new(default_params)
-      end
   end
 end
