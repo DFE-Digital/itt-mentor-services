98df0c286ef0a77da14144b55adce7167a9cd04c
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-11-14 14:04:45 +0000

    Improve method naming, line spacing and comment typo

--- spec/system/placements/schools/placements/all_through_school_user_adds_a_placement_spec.rb
+++ spec/system/placements/schools/placements/all_through_school_user_adds_a_placement_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "All-through school user adds a placement", service: :placements, type: :system do
   scenario do
-    given_that_placements_data_exists
+    given_that_placements_exist
     and_i_am_signed_in
 
     when_i_am_on_the_placements_index_page
@@ -140,7 +140,7 @@ RSpec.describe "All-through school user adds a placement", service: :placements,
     when_i_click_on_edit_placement
     then_i_see_the_check_your_answers_page
 
-    # Test that the publishing a placement works as expected
+    # Test that publishing a placement works as expected
     when_i_click_on_publish_placement
     then_i_see_the_placement_placements_index_page
     and_i_see_a_success_message
@@ -149,21 +149,24 @@ RSpec.describe "All-through school user adds a placement", service: :placements,
     then_i_see_my_placement
   end
 
-  def given_that_placements_data_exists
-    @school = create(:placements_school, name: "Malory Towers",
-                                         address1: "Westgate Street",
-                                         address2: "Hackney",
-                                         postcode: "E8 3RL",
-                                         group: "Local authority maintained schools",
-                                         phase: "All-through",
-                                         gender: "Mixed",
-                                         minimum_age: 4,
-                                         maximum_age: 16,
-                                         religious_character: "Does not apply",
-                                         admissions_policy: "Not applicable",
-                                         urban_or_rural: "(England/Wales) Urban major conurbation",
-                                         percentage_free_school_meals: 15,
-                                         rating: "Outstanding")
+  def given_that_placements_exist
+    @school = create(
+      :placements_school,
+      name: "Malory Towers",
+      address1: "Westgate Street",
+      address2: "Hackney",
+      postcode: "E8 3RL",
+      group: "Local authority maintained schools",
+      phase: "All-through",
+      gender: "Mixed",
+      minimum_age: 4,
+      maximum_age: 16,
+      religious_character: "Does not apply",
+      admissions_policy: "Not applicable",
+      urban_or_rural: "(England/Wales) Urban major conurbation",
+      percentage_free_school_meals: 15,
+      rating: "Outstanding",
+    )
 
     @primary_english_subject = create(:subject, name: "Primary with english", subject_area: :primary)
     @primary_maths_subject = create(:subject, name: "Primary with mathematics", subject_area: :primary)
--- spec/system/placements/schools/placements/primary_school_user_adds_a_placement_spec.rb
+++ spec/system/placements/schools/placements/primary_school_user_adds_a_placement_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Primary school user adds a placement", service: :placements, type: :system do
   scenario do
-    given_that_placements_data_exists
+    given_that_placements_exist
     and_i_am_signed_in
 
     when_i_am_on_the_placements_index_page
@@ -126,7 +126,7 @@ RSpec.describe "Primary school user adds a placement", service: :placements, typ
     when_i_click_on_edit_placement
     then_i_see_the_check_your_answers_page
 
-    # Test that the publishing a placement works as expected
+    # Test that publishing a placement works as expected
     when_i_click_on_publish_placement
     then_i_see_the_placement_placements_index_page
     and_i_see_a_success_message
@@ -135,21 +135,24 @@ RSpec.describe "Primary school user adds a placement", service: :placements, typ
     then_i_see_my_placement
   end
 
-  def given_that_placements_data_exists
-    @school = create(:placements_school, name: "Springfield Elementary",
-                                         address1: "Westgate Street",
-                                         address2: "Hackney",
-                                         postcode: "E8 3RL",
-                                         group: "Local authority maintained schools",
-                                         phase: "Primary",
-                                         gender: "Mixed",
-                                         minimum_age: 3,
-                                         maximum_age: 11,
-                                         religious_character: "Does not apply",
-                                         admissions_policy: "Not applicable",
-                                         urban_or_rural: "(England/Wales) Urban major conurbation",
-                                         percentage_free_school_meals: 15,
-                                         rating: "Outstanding")
+  def given_that_placements_exist
+    @school = create(
+      :placements_school,
+      name: "Springfield Elementary",
+      address1: "Westgate Street",
+      address2: "Hackney",
+      postcode: "E8 3RL",
+      group: "Local authority maintained schools",
+      phase: "Primary",
+      gender: "Mixed",
+      minimum_age: 3,
+      maximum_age: 11,
+      religious_character: "Does not apply",
+      admissions_policy: "Not applicable",
+      urban_or_rural: "(England/Wales) Urban major conurbation",
+      percentage_free_school_meals: 15,
+      rating: "Outstanding",
+    )
 
     @primary_english_subject = create(:subject, name: "Primary with english", subject_area: :primary)
     @primary_maths_subject = create(:subject, name: "Primary with mathematics", subject_area: :primary)
