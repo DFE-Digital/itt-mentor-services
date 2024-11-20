75ee034fabd0fe5b4ed01413108f714517b344bb
Daniel Dye <daniel.dye@education.gov.uk>
2024-11-20 21:42:14 +0000

    Add all claim statuses and handle their display

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_16_120228) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_20_213146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
-  create_enum "claim_status", ["internal_draft", "draft", "submitted", "payment_in_progress"]
+  create_enum "claim_status", ["internal_draft", "draft", "submitted", "payment_in_progress", "payment_information_requested", "payment_information_sent", "paid", "payment_not_approved", "sampling_in_progress", "sampling_provider_not_approved", "sampling_not_approved", "clawback_requested", "clawback_in_progress", "clawback_complete"]
   create_enum "mentor_training_type", ["refresher", "initial"]
   create_enum "placement_status", ["draft", "published"]
   create_enum "placement_year_group", ["nursery", "reception", "year_1", "year_2", "year_3", "year_4", "year_5", "year_6"]
--- spec/components/claim/status_tag_component_spec.rb
+++ spec/components/claim/status_tag_component_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe Claim::StatusTagComponent, type: :component do
     render_inline(component)
   end
 
+  context "when the claim's status is 'internal_draft'" do
+    let(:claim) { build(:claim, status: :internal_draft) }
+
+    it "renders a grey tag" do
+      expect(page).to have_css(".govuk-tag--grey", text: "Internal draft")
+    end
+  end
+
   context "when the claim's status is 'draft'" do
     let(:claim) { build(:claim, status: :draft) }
 
@@ -26,8 +34,88 @@ RSpec.describe Claim::StatusTagComponent, type: :component do
   context "when the claim's status is 'payment_in_progress'" do
     let(:claim) { build(:claim, status: :payment_in_progress) }
 
+    it "renders a turquoise tag" do
+      expect(page).to have_css(".govuk-tag--turquoise", text: "Payment in progress")
+    end
+  end
+
+  context "when the claim's status is 'payment_information_requested'" do
+    let(:claim) { build(:claim, status: :payment_information_requested) }
+
     it "renders a light blue tag" do
-      expect(page).to have_css(".govuk-tag--light-blue", text: "Payment in progress")
+      expect(page).to have_css(".govuk-tag--light-blue", text: "Information requested")
+    end
+  end
+
+  context "when the claim's status is 'payment_information_sent'" do
+    let(:claim) { build(:claim, status: :payment_information_sent) }
+
+    it "renders a yellow tag" do
+      expect(page).to have_css(".govuk-tag--yellow", text: "Information sent")
+    end
+  end
+
+  context "when the claim's status is 'paid'" do
+    let(:claim) { build(:claim, status: :paid) }
+
+    it "renders a green tag" do
+      expect(page).to have_css(".govuk-tag--green", text: "Paid")
+    end
+  end
+
+  context "when the claim's status is 'payment_not_approved'" do
+    let(:claim) { build(:claim, status: :payment_not_approved) }
+
+    it "renders a red tag" do
+      expect(page).to have_css(".govuk-tag--red", text: "Payment not approved")
+    end
+  end
+
+  context "when the claim's status is 'sampling_in_progress'" do
+    let(:claim) { build(:claim, status: :sampling_in_progress) }
+
+    it "renders a purple tag" do
+      expect(page).to have_css(".govuk-tag--purple", text: "Sampling in progress")
+    end
+  end
+
+  context "when the claim's status is 'sampling_provider_not_approved'" do
+    let(:claim) { build(:claim, status: :sampling_provider_not_approved) }
+
+    it "renders a pink tag" do
+      expect(page).to have_css(".govuk-tag--pink", text: "Provider not approved")
+    end
+  end
+
+  context "when the claim's status is 'sampling_not_approved'" do
+    let(:claim) { build(:claim, status: :sampling_not_approved) }
+
+    it "renders a pink tag" do
+      expect(page).to have_css(".govuk-tag--pink", text: "Claim not approved")
+    end
+  end
+
+  context "when the claim's status is 'clawback_requested'" do
+    let(:claim) { build(:claim, status: :clawback_requested) }
+
+    it "renders a orange tag" do
+      expect(page).to have_css(".govuk-tag--orange", text: "Clawback requested")
+    end
+  end
+
+  context "when the claim's status is 'clawback_in_progress'" do
+    let(:claim) { build(:claim, status: :clawback_in_progress) }
+
+    it "renders a orange tag" do
+      expect(page).to have_css(".govuk-tag--orange", text: "Clawback in progress")
+    end
+  end
+
+  context "when the claim's status is 'clawback_complete'" do
+    let(:claim) { build(:claim, status: :clawback_complete) }
+
+    it "renders a red tag" do
+      expect(page).to have_css(".govuk-tag--red", text: "Clawback complete")
     end
   end
 
