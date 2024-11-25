8260a343386afecb51ebf6a4f754ca7e8c2a2299
Daniel Dye <daniel.dye@education.gov.uk>
2024-11-25 13:35:00 +0000

    Wrap claims filter form into a reusable component

--- app/components/claims/claim/filter_form_component.rb
+++ app/components/claims/claim/filter_form_component.rb
@@ -1,19 +1,20 @@
 class Claims::Claim::FilterFormComponent < ApplicationComponent
   def initialize(
     filter_form:,
-    academic_years: AcademicYear.where(id: Claims::ClaimWindow.select(:academic_year_id)).order_by_date,
     statuses: Claims::Claim.statuses.values.without(*Claims::Claim::DRAFT_STATUSES.map(&:to_s)),
+    academic_years: AcademicYear.where(id: Claims::ClaimWindow.select(:academic_year_id)).order_by_date,
     providers: Claims::Provider.all,
+    schools: Claims::School.all,
     classes: [],
     html_attributes: {}
   )
     super(classes:, html_attributes:)
 
     @filter_form = filter_form
-    @academic_years = academic_years
     @statuses = statuses
-    @schools = Claims::School.all
+    @academic_years = academic_years
     @providers = providers
+    @schools = schools
   end
 
   private
