9f9346b039a043b07a98fbad55c2752a885668e6
Jamie <jamie.cleare@education.gov.uk>
2024-11-28 15:56:06 +0000

    Remove unnecessary controllers and views

--- app/controllers/claims/schools/claims_controller.rb
+++ app/controllers/claims/schools/claims_controller.rb
@@ -2,54 +2,18 @@ class Claims::Schools::ClaimsController < Claims::ApplicationController
   include Claims::BelongsToSchool
 
   before_action :has_school_accepted_grant_conditions?
-  before_action :set_claim, only: %i[show check confirmation submit edit update create_revision remove destroy]
+  before_action :set_claim, only: %i[show confirmation remove destroy]
   before_action :authorize_claim
-  before_action :get_valid_revision, only: :check
 
-  helper_method :claim_provider_form, :edit_attribute_path
+  helper_method :edit_attribute_path
 
   def index
     @pagy, @claims = pagy(@school.claims.active.order_created_at_desc)
   end
 
-  def create_revision
-    revision = Claims::Claim::CreateRevision.call(claim: @claim)
-    redirect_to edit_claims_school_claim_path(@school, revision)
-  end
-
   def show; end
 
-  def check
-    last_mentor_training = @claim.mentor_trainings.order_by_mentor_full_name.last
-    Claims::Claim::Review.call(claim: @claim)
-
-    @back_path = edit_claims_school_claim_mentor_training_path(
-      @school,
-      @claim,
-      last_mentor_training,
-    )
-    Claims::Claim::RemoveEmptyMentorTrainingHours.call(claim: @claim)
-  end
-
-  def edit; end
-
-  def update
-    if claim_provider_form.save
-      redirect_to claim_provider_form.update_success_path
-    else
-      render :edit
-    end
-  end
-
   def confirmation; end
-
-  def submit
-    return redirect_to rejected_claims_school_claim_path unless @claim.valid_mentor_training_hours?
-
-    Claims::Claim::Submit.call(claim: @claim, user: current_user)
-
-    redirect_to confirmation_claims_school_claim_path(@school, @claim)
-  end
 
   def rejected; end
 
@@ -65,25 +29,6 @@ class Claims::Schools::ClaimsController < Claims::ApplicationController
 
   private
 
-  def claim_params
-    params.require(:claims_claim_provider_form)
-      .permit(:id, :provider_id)
-      .merge(default_params)
-  end
-
-  def default_params
-    { school: @school, current_user:, claim_window: Claims::ClaimWindow.current }
-  end
-
-  def claim_provider_form
-    @claim_provider_form ||=
-      if params[:claims_claim_provider_form].present?
-        Claims::Claim::ProviderForm.new(claim_params)
-      else
-        Claims::Claim::ProviderForm.new(default_params.merge(id: claim_id))
-      end
-  end
-
   def set_claim
     @claim = @school.claims.find(claim_id)
   end
@@ -94,12 +39,6 @@ class Claims::Schools::ClaimsController < Claims::ApplicationController
 
   def authorize_claim
     authorize @claim || Claims::Claim.new
-  end
-
-  def get_valid_revision
-    revision = @claim.get_valid_revision
-
-    redirect_to check_claims_school_claim_path(@school, revision) if revision != @claim
   end
 
   def edit_attribute_path(attribute)
