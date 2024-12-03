7d085515fe55f32de4ef3f36bfabd1738bc07fa3
Jamie <jamie.cleare@education.gov.uk>
2024-12-03 15:22:05 +0000

    Testing for filtering sampled claims

---
+++ spec/system/claims/support/claims/sampling/support_user_filters_sampled_claims_by_academic_year_spec.rb
@@ -0,0 +1,196 @@
+require "rails_helper"
+
+RSpec.describe "Support user filters sampled claims by academic year", service: :claims, type: :system do
+  scenario do
+    given_claims_exist
+    and_i_am_signed_in
+
+    when_i_navigate_to_the_sampling_claims_index_page
+    then_i_see_the_claims_sampling_claims_index_page
+    and_i_see_the_sampled_claim_for_the_current_academic_year
+    and_i_see_the_sampled_claim_for_the_previous_academic_year
+
+    when_i_select_the_previous_year_from_the_academic_year_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_the_previous_academic_year
+    and_i_do_not_see_the_sampled_claim_for_the_current_academic_year
+    and_i_see_previous_academic_year_is_selected_from_the_academic_year_filter
+    and_i_do_not_see_current_academic_year_selected_from_the_academic_year_filter
+
+    when_i_select_the_current_year_from_the_academic_year_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_the_previous_academic_year
+    and_i_see_the_sampled_claim_for_the_current_academic_year
+    and_i_see_previous_academic_year_is_selected_from_the_academic_year_filter
+    and_i_see_current_academic_year_is_selected_from_the_academic_year_filter
+
+    when_i_unselect_the_previous_year_from_the_academic_year_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_the_current_academic_year
+    and_i_do_not_see_the_sampled_claim_for_the_previous_academic_year
+    and_i_see_current_academic_year_is_selected_from_the_academic_year_filter
+    and_i_do_not_see_previous_academic_year_selected_from_the_academic_year_filter
+
+    when_i_click_on_the_current_academic_year_filter_tag
+    then_i_see_the_sampled_claim_for_the_current_academic_year
+    and_i_see_the_sampled_claim_for_the_previous_academic_year
+    and_i_do_not_see_previous_academic_year_selected_from_the_academic_year_filter
+    and_i_do_not_see_current_academic_year_selected_from_the_academic_year_filter
+
+    when_i_select_the_previous_year_from_the_academic_year_filter
+    and_i_select_the_current_year_from_the_academic_year_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_the_current_academic_year
+    and_i_see_the_sampled_claim_for_the_previous_academic_year
+    and_i_see_current_academic_year_is_selected_from_the_academic_year_filter
+    and_i_see_previous_academic_year_is_selected_from_the_academic_year_filter
+
+    when_i_click_clear_filters
+    then_i_see_the_sampled_claim_for_the_current_academic_year
+    and_i_do_not_see_the_sampled_claim_for_the_previous_academic_year
+    and_i_do_not_see_previous_academic_year_selected_from_the_academic_year_filter
+    and_i_do_not_see_current_academic_year_selected_from_the_academic_year_filter
+  end
+
+  private
+
+  def given_claims_exist
+    @current_academic_year = AcademicYear.for_date(Time.current) || create(:academic_year, :current)
+    previous_year_starts_on = @current_academic_year.starts_on - 1.year
+    previous_year_ends_on = @current_academic_year.starts_on - 1.day
+    @previous_academic_year = AcademicYear.for_date(Time.current - 1.year) || create(:academic_year,
+                                                                                     starts_on: previous_year_starts_on,
+                                                                                     ends_on: previous_year_ends_on,
+                                                                                     name: "#{previous_year_starts_on.year} to #{previous_year_ends_on.year}")
+
+    current_claim_window = create(:claim_window, academic_year: @current_academic_year,
+                                                 starts_on: @current_academic_year.starts_on,
+                                                 ends_on: @current_academic_year.starts_on + 2.days)
+    previous_claim_window = create(:claim_window,
+                                   academic_year: @previous_academic_year,
+                                   starts_on: @previous_academic_year.starts_on,
+                                   ends_on: @previous_academic_year.starts_on + 2.days)
+    @current_claim = create(:claim,
+                            :submitted,
+                            status: :sampling_in_progress,
+                            claim_window: current_claim_window)
+    @previous_claim = create(:claim,
+                             :submitted,
+                             status: :sampling_in_progress,
+                             claim_window: previous_claim_window)
+  end
+
+  def and_i_am_signed_in
+    sign_in_claims_support_user
+  end
+
+  def when_i_navigate_to_the_sampling_claims_index_page
+    within primary_navigation do
+      click_on "Claims"
+    end
+
+    within secondary_navigation do
+      click_on "Sampling"
+    end
+  end
+
+  def then_i_see_the_claims_sampling_claims_index_page
+    expect(page).to have_title("Claims - Claim funding for mentor training - GOV.UK")
+    expect(page).to have_h1("Claims")
+    expect(page).to have_h2("Sampling (2)")
+    expect(primary_navigation).to have_current_item("Claims")
+    expect(secondary_navigation).to have_current_item("Sampling")
+  end
+
+  def then_i_see_the_sampled_claim_for_the_current_academic_year
+    expect(page).to have_claim_card({
+      "title" => "#{@current_claim.reference} - #{@current_claim.school.name}",
+      "url" => "/support/claims/#{@current_claim.id}",
+      "status" => "Sampling in progress",
+      "academic_year" => @current_claim.academic_year.name,
+      "provider_name" => @current_claim.provider.name,
+      "submitted_at" => I18n.l(@current_claim.submitted_at.to_date, format: :long),
+      "amount" => "£0.00",
+    })
+  end
+  alias_method :and_i_see_the_sampled_claim_for_the_current_academic_year,
+               :then_i_see_the_sampled_claim_for_the_current_academic_year
+
+  def then_i_see_the_sampled_claim_for_the_previous_academic_year
+    expect(page).to have_claim_card({
+      "title" => "#{@previous_claim.reference} - #{@previous_claim.school.name}",
+      "url" => "/support/claims/#{@previous_claim.id}",
+      "status" => "Sampling in progress",
+      "academic_year" => @previous_claim.academic_year.name,
+      "provider_name" => @previous_claim.provider.name,
+      "submitted_at" => I18n.l(@previous_claim.submitted_at.to_date, format: :long),
+      "amount" => "£0.00",
+    })
+  end
+  alias_method :and_i_see_the_sampled_claim_for_the_previous_academic_year,
+               :then_i_see_the_sampled_claim_for_the_previous_academic_year
+
+  def when_i_select_the_previous_year_from_the_academic_year_filter
+    check @previous_claim.academic_year.name
+  end
+  alias_method :and_i_select_the_previous_year_from_the_academic_year_filter,
+               :when_i_select_the_previous_year_from_the_academic_year_filter
+
+  def when_i_select_the_current_year_from_the_academic_year_filter
+    check @current_claim.academic_year.name
+  end
+  alias_method :and_i_select_the_current_year_from_the_academic_year_filter,
+               :when_i_select_the_current_year_from_the_academic_year_filter
+
+  def when_i_unselect_the_previous_year_from_the_academic_year_filter
+    uncheck @previous_claim.academic_year.name
+  end
+
+  def and_i_click_on_apply_filters
+    click_on "Apply filters"
+  end
+
+  def and_i_do_not_see_the_sampled_claim_for_the_current_academic_year
+    expect(page).not_to have_claim_card(
+      { "title" => "#{@current_claim.reference} - #{@current_claim.school.name}" },
+    )
+  end
+
+  def and_i_do_not_see_the_sampled_claim_for_the_previous_academic_year
+    expect(page).not_to have_claim_card(
+      { "title" => "#{@previous_claim.reference} - #{@previous_claim.school.name}" },
+    )
+  end
+
+  def and_i_see_previous_academic_year_is_selected_from_the_academic_year_filter
+    expect(page).to have_element(:legend, text: "Academic year", class: "govuk-fieldset__legend")
+    expect(page).to have_checked_field(@previous_academic_year.name)
+    expect(page).to have_filter_tag(@previous_academic_year.name)
+  end
+
+  def and_i_see_current_academic_year_is_selected_from_the_academic_year_filter
+    expect(page).to have_element(:legend, text: "Academic year", class: "govuk-fieldset__legend")
+    expect(page).to have_checked_field(@current_academic_year.name)
+    expect(page).to have_filter_tag(@current_academic_year.name)
+  end
+
+  def and_i_do_not_see_current_academic_year_selected_from_the_academic_year_filter
+    expect(page).not_to have_checked_field(@current_academic_year.name)
+    expect(page).not_to have_filter_tag(@current_academic_year.name)
+  end
+
+  def and_i_do_not_see_previous_academic_year_selected_from_the_academic_year_filter
+    expect(page).not_to have_checked_field(@previous_academic_year.name)
+    expect(page).not_to have_filter_tag(@previous_academic_year.name)
+  end
+
+  def when_i_click_on_the_current_academic_year_filter_tag
+    within ".app-filter-tags" do
+      click_on @current_academic_year.name
+    end
+  end
+
+  def when_i_click_clear_filters
+    click_on "Clear filters"
+  end
+end
---
+++ spec/system/claims/support/claims/sampling/support_user_filters_sampled_claims_by_school_spec.rb
@@ -0,0 +1,184 @@
+require "rails_helper"
+
+RSpec.describe "Support user filters sampled claims by school", service: :claims, type: :system do
+  scenario do
+    given_claims_exist
+    and_i_am_signed_in
+
+    when_i_navigate_to_the_sampling_claims_index_page
+    then_i_see_the_claims_sampling_claims_index_page
+    and_i_see_the_sampled_claim_for_springfield_elementary
+    and_i_see_the_sampled_claim_for_hogwarts
+
+    when_i_select_springfield_elementary_from_the_school_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_springfield_elementary
+    and_i_do_not_see_sampled_claim_for_hogwarts
+    and_i_see_springfield_elementary_selected_from_the_school_filter
+    and_i_do_not_see_hogwarts_selected_from_the_school_filter
+
+    when_i_select_hogwarts_from_the_school_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_springfield_elementary
+    and_i_see_the_sampled_claim_for_hogwarts
+    and_i_see_springfield_elementary_selected_from_the_school_filter
+    and_i_see_hogwarts_selected_from_the_school_filter
+
+    when_i_unselect_springfield_elementary_from_the_school_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_hogwarts
+    and_i_do_not_see_sampled_claim_for_springfield_elementary
+    and_i_see_hogwarts_selected_from_the_school_filter
+    and_i_do_not_see_springfield_elementary_selected_from_the_school_filter
+
+    when_i_click_on_the_hogwarts_filter_tag
+    then_i_see_the_sampled_claim_for_hogwarts
+    and_i_see_the_sampled_claim_for_springfield_elementary
+    and_i_do_not_see_hogwarts_selected_from_the_school_filter
+    and_i_do_not_see_springfield_elementary_selected_from_the_school_filter
+
+    when_i_select_hogwarts_from_the_school_filter
+    and_i_select_springfield_elementary_from_the_school_filter
+    and_i_click_on_apply_filters
+    then_i_see_the_sampled_claim_for_springfield_elementary
+    and_i_see_the_sampled_claim_for_hogwarts
+    and_i_see_springfield_elementary_selected_from_the_school_filter
+    and_i_see_hogwarts_selected_from_the_school_filter
+
+    when_i_click_clear_filters
+    then_i_see_the_sampled_claim_for_springfield_elementary
+    and_i_see_the_sampled_claim_for_hogwarts
+    and_i_do_not_see_springfield_elementary_selected_from_the_school_filter
+    and_i_do_not_see_hogwarts_selected_from_the_school_filter
+  end
+
+  private
+
+  def given_claims_exist
+    @springfield_school = create(:claims_school, name: "Springfield Elementary")
+    @springfield_claim = create(:claim,
+                                :submitted,
+                                status: :sampling_in_progress,
+                                school: @springfield_school)
+
+    @hogwarts_school = create(:claims_school, name: "Hogwarts")
+    @hogwarts_claim = create(:claim,
+                             :submitted,
+                             status: :sampling_in_progress,
+                             school: @hogwarts_school)
+  end
+
+  def and_i_am_signed_in
+    sign_in_claims_support_user
+  end
+
+  def when_i_navigate_to_the_sampling_claims_index_page
+    within primary_navigation do
+      click_on "Claims"
+    end
+
+    within secondary_navigation do
+      click_on "Sampling"
+    end
+  end
+
+  def then_i_see_the_claims_sampling_claims_index_page
+    expect(page).to have_title("Claims - Claim funding for mentor training - GOV.UK")
+    expect(page).to have_h1("Claims")
+    expect(page).to have_h2("Sampling (2)")
+    expect(primary_navigation).to have_current_item("Claims")
+    expect(secondary_navigation).to have_current_item("Sampling")
+  end
+
+  def then_i_see_the_sampled_claim_for_springfield_elementary
+    expect(page).to have_claim_card({
+      "title" => "#{@springfield_claim.reference} - #{@springfield_claim.school.name}",
+      "url" => "/support/claims/#{@springfield_claim.id}",
+      "status" => "Sampling in progress",
+      "academic_year" => @springfield_claim.academic_year.name,
+      "provider_name" => @springfield_claim.provider.name,
+      "submitted_at" => I18n.l(@springfield_claim.submitted_at.to_date, format: :long),
+      "amount" => "£0.00",
+    })
+  end
+  alias_method :and_i_see_the_sampled_claim_for_springfield_elementary,
+               :then_i_see_the_sampled_claim_for_springfield_elementary
+
+  def then_i_see_the_sampled_claim_for_hogwarts
+    expect(page).to have_claim_card({
+      "title" => "#{@hogwarts_claim.reference} - #{@hogwarts_claim.school.name}",
+      "url" => "/support/claims/#{@hogwarts_claim.id}",
+      "status" => "Sampling in progress",
+      "academic_year" => @hogwarts_claim.academic_year.name,
+      "provider_name" => @hogwarts_claim.provider.name,
+      "submitted_at" => I18n.l(@hogwarts_claim.submitted_at.to_date, format: :long),
+      "amount" => "£0.00",
+    })
+  end
+  alias_method :and_i_see_the_sampled_claim_for_hogwarts,
+               :then_i_see_the_sampled_claim_for_hogwarts
+
+  def when_i_select_springfield_elementary_from_the_school_filter
+    check "Springfield Elementary"
+  end
+  alias_method :and_i_select_springfield_elementary_from_the_school_filter,
+               :when_i_select_springfield_elementary_from_the_school_filter
+
+  def when_i_select_hogwarts_from_the_school_filter
+    check "Hogwarts"
+  end
+  alias_method :and_i_select_hogwarts_from_the_school_filter,
+               :when_i_select_hogwarts_from_the_school_filter
+
+  def when_i_unselect_springfield_elementary_from_the_school_filter
+    uncheck "Springfield Elementary"
+  end
+
+  def and_i_click_on_apply_filters
+    click_on "Apply filters"
+  end
+
+  def and_i_do_not_see_sampled_claim_for_hogwarts
+    expect(page).not_to have_claim_card(
+      { "title" => "#{@hogwarts_claim.reference} - #{@hogwarts_claim.school.name}" },
+    )
+  end
+
+  def and_i_do_not_see_sampled_claim_for_springfield_elementary
+    expect(page).not_to have_claim_card(
+      { "title" => "#{@springfield_claim.reference} - #{@springfield_claim.school.name}" },
+    )
+  end
+
+  def and_i_see_springfield_elementary_selected_from_the_school_filter
+    expect(page).to have_element(:legend, text: "School", class: "govuk-fieldset__legend")
+    expect(page).to have_checked_field("Springfield Elementary")
+    expect(page).to have_filter_tag("Springfield Elementary")
+  end
+
+  def and_i_see_hogwarts_selected_from_the_school_filter
+    expect(page).to have_element(:legend, text: "School", class: "govuk-fieldset__legend")
+    expect(page).to have_checked_field("Hogwarts")
+    expect(page).to have_filter_tag("Hogwarts")
+  end
+
+  def and_i_do_not_see_hogwarts_selected_from_the_school_filter
+    expect(page).not_to have_checked_field("Hogwarts")
+    expect(page).not_to have_filter_tag("Hogwarts")
+  end
+
+  def and_i_do_not_see_springfield_elementary_selected_from_the_school_filter
+    expect(page).not_to have_checked_field("Springfield Elementary")
+    expect(page).not_to have_filter_tag("Springfield Elementary")
+  end
+
+  def when_i_click_on_the_hogwarts_filter_tag
+    within ".app-filter-tags" do
+      click_on "Hogwarts"
+    end
+  end
+
+  def when_i_click_clear_filters
+    click_on "Clear filters"
+  end
+end
