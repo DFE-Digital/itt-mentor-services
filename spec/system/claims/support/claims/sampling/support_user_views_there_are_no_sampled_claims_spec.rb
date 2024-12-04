33e563d6e3ebd73bd3bd1065bae88c0ddbc86d5e
Jamie <jamie.cleare@education.gov.uk>
2024-12-04 16:50:20 +0000

    Add test for no claims waiting to be processed

---
+++ spec/system/claims/support/claims/sampling/support_user_views_there_are_no_sampled_claims_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "Support user views there are no sampled sampled claims", service: :claims, type: :system do
+  scenario do
+    when_i_am_signed_in
+
+    when_i_navigate_to_the_sampling_claims_index_page
+    then_i_see_the_claims_sampling_claims_index_page
+    and_i_see_there_are_no_claims_waiting_to_be_processed
+  end
+
+  private
+
+  def when_i_am_signed_in
+    sign_in_claims_support_user
+  end
+
+  def when_i_navigate_to_the_sampling_claims_index_page
+    within primary_navigation do
+      click_on "Claims"
+    end
+
+    within secondary_navigation do
+      click_on "Sampling"
+    end
+  end
+
+  def then_i_see_the_claims_sampling_claims_index_page
+    expect(page).to have_title("Claims - Claim funding for mentor training - GOV.UK")
+    expect(page).to have_h1("Claims")
+    expect(page).to have_h2("Sampling")
+    expect(primary_navigation).to have_current_item("Claims")
+    expect(secondary_navigation).to have_current_item("Sampling")
+  end
+
+  def and_i_see_there_are_no_claims_waiting_to_be_processed
+    expect(page).to have_element(:p, text: "There are no claims waiting to be processed.", class: "govuk-body")
+  end
+end
