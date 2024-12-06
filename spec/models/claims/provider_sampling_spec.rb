d6ab935b1ea0b97caf6e5c2df4ec777a62408a55
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-12-06 16:29:40 +0000

    Create the `provider_samplings` table and associated files.

---
+++ spec/models/claims/provider_sampling_spec.rb
@@ -0,0 +1,34 @@
+# == Schema Information
+#
+# Table name: provider_samplings
+#
+#  id          :uuid             not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  provider_id :uuid             not null
+#  sampling_id :uuid             not null
+#
+# Indexes
+#
+#  index_provider_samplings_on_provider_id  (provider_id)
+#  index_provider_samplings_on_sampling_id  (sampling_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (provider_id => providers.id)
+#  fk_rails_...  (sampling_id => samplings.id)
+#
+require "rails_helper"
+
+RSpec.describe Claims::ProviderSampling, type: :model do
+  describe "associations" do
+    it { is_expected.to belong_to(:sampling) }
+    it { is_expected.to belong_to(:provider) }
+
+    it { is_expected.to have_many(:claims) }
+  end
+
+  describe "attachments" do
+    it { is_expected.to have_one_attached(:csv_file) }
+  end
+end
