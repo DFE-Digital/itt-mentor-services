b8cf7e5afbd4eeefb133b0c378e24030c3d9de87
Jamie <jamie.cleare@education.gov.uk>
2024-12-10 14:53:05 +0000

    UI for provider responses

---
+++ app/wizards/claims/upload_provider_response_wizard.rb
@@ -0,0 +1,26 @@
+module Claims
+  class UploadProviderResponseWizard < BaseWizard
+    def define_steps
+      if sampled_claims.present?
+        add_step(UploadStep)
+        add_step(ConfirmationStep)
+      else
+        add_step(NoClaimsStep)
+      end
+    end
+
+    def upload_provider_responses
+      raise "Invalid wizard state" unless valid?
+    end
+
+    def sampled_claims
+      Claims::Claim.sampling_in_progress
+    end
+
+    def claim_update_details
+      return [] if steps[:upload].blank?
+
+      steps.fetch(:upload).claim_update_details
+    end
+  end
+end
