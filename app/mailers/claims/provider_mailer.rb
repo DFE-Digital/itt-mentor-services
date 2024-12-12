f764c40799a57735702279d78a71dc85f8ec33d7
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-12-12 09:27:10 +0000

    Add provider mailer for sampling email

---
+++ app/mailers/claims/provider_mailer.rb
@@ -0,0 +1,7 @@
+class Claims::ProviderMailer < Claims::ApplicationMailer
+  def sampling_checks_required(provider, url_for_csv)
+    notify_email to: provider.email_address,
+                 subject: t(".subject"),
+                 body: t(".body", provider_name: provider.name, url_for_csv:)
+  end
+end
