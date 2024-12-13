b6014c4825c9ef30b40901467b9cf298975748f7
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2024-12-13 15:06:23 +0000

    Update mailer content to match design history

--- app/mailers/claims/provider_mailer.rb
+++ app/mailers/claims/provider_mailer.rb
@@ -2,6 +2,6 @@ class Claims::ProviderMailer < Claims::ApplicationMailer
   def sampling_checks_required(provider, url_for_csv)
     notify_email to: provider.email_address,
                  subject: t(".subject"),
-                 body: t(".body", provider_name: provider.name, url_for_csv:)
+                 body: t(".body", provider_name: provider.name, url_for_csv:, support_email:, service_name:)
   end
 end
--- spec/mailers/claims/provider_mailer_spec.rb
+++ spec/mailers/claims/provider_mailer_spec.rb
@@ -6,56 +6,40 @@ RSpec.describe Claims::ProviderMailer, type: :mailer do
 
     let(:provider) { create(:claims_provider, email_address: "aes_sedai_trust@example.com") }
     let(:url_for_csv) { "https://example.com" }
+    let(:service_name) { "Claim funding for mentor training" }
+    let(:support_email) { "ittmentor.funding@education.gov.uk" }
 
     it "sends the sampling checks required email" do
       expect(sampling_checks_required_email.to).to contain_exactly(provider.email_address)
-      expect(sampling_checks_required_email.subject).to eq("Checks needed – Claim funding for mentor training service")
+      expect(sampling_checks_required_email.subject).to eq("ITT mentor claims need to be assured")
       expect(sampling_checks_required_email.body.to_s.squish).to eq(<<~EMAIL.squish)
-        To #{provider.name},
+        Dear #{provider.name},
 
-        As part of the Claim funding for mentor training (Claim) service assurance, we have sent you a sample of claims to confirm they are correct and approved — the link to the CSV file is valid for 7 days:
+        These claims from the Claim funding for mentor training service (Claim) are ready for post-payment assurance. The link to the latest CSV file is valid for 7 days.
 
         #{url_for_csv}
 
-        # What you need to check
+        What you need to do:
 
-        You need to check the claim details match your own records and are correct. You should check BOTH:
+        1. Check the information detailed in the claims matches your information, specifically the mentor names and the hours of training. If it does, mark them as ‘yes’ in the ‘claim_assured’ column.
 
-        - training hours that have been claimed
-        - mentors' names
+        2. If you disagree with the information, please contact the placement school to discuss it. They may have additional evidence.
 
-        # If all claims are correct
+        3. If they cannot provide any additional information or cannot provide information that you are not content with, mark the claim as ‘no’ in the ‘claim_assured’ column. In the ‘claim_not_assured_reason’ column, indicate why the claim has not been assured. For example, the mentor’s name is wrong, or the number of hours exceeds the evidence that you have.
 
-        If the mentors’ names and training hours are all correct, mark them as ‘approved’ in the CSV file and email back the updated file.
+        4. If the placement school provides any additional information you are content with, mark the claim as ‘yes’ in the ‘claim_assured’ column.
 
-        # If any claims are incorrect
+        5. Reply to this email and attach the updated CSV.
 
-        If any of the training hours or mentor names are incorrect you need to:
+        The Claim Support team will follow up with the placement schools on the claims that have not been assured. You will not need to take further action.
 
-        1. contact the placement school to agree changes to claim details
-        2. add an explanation of agreed changes in the ‘claim_change_reason’ column of the CSV file. For example, incorrect claim amount: claim should be for 8 not 10 hours
-        3. mark the claim as ‘not approved’ and email back the updated file
-        4. the Claim Support team will update the details in the system and process any payment adjustments
+        If you have a problem opening the link or it has expired, reply to this email and request that it be sent again.
 
-        # If you and the placement schools do not agree on changes to a claim
+        # Give feedback or report a problem
+        If you have any questions or feedback, please contact the team at [#{support_email}](mailto:#{support_email}).
 
-        If the placement schools do not agree with you about changes to claim details, you need to:
-
-        1. add an explanation in the ‘claim_change_reason’ column of the CSV file. For example, School training hours different – evidence not provided
-        2. mark the claim as ‘information needed’ and email back the updated file
-        3. the Claim Support team will contact the placement school and aim to resolve issues
-
-        # What to do if the link does not work
-
-        The link in this email is valid for 7 days. If you have a problem opening the link, or it has expired, reply to this email and ask for the link to be sent again.
-
-        # When is the deadline to complete the checks
-
-        Please complete your checks and email back an updated file within 30 days.
-
-        Regards,
-
-        Claim funding for mentor training team
+        Regards
+        #{service_name} team
       EMAIL
     end
   end
