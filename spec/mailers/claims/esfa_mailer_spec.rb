3e9d70358a0d087fd4d0f57d4e3f66818de8e3ad
Jamie <jamie.cleare@education.gov.uk>
2024-12-17 14:33:53 +0000

    Fix for mailer leakage

--- spec/mailers/claims/esfa_mailer_spec.rb
+++ spec/mailers/claims/esfa_mailer_spec.rb
@@ -9,38 +9,36 @@ RSpec.describe Claims::ESFAMailer, type: :mailer do
     let(:service_name) { "Claim funding for mentor training" }
     let(:support_email) { "ittmentor.funding@education.gov.uk" }
 
-    before do
-      stub_const("ENV", { "CLAIMS_ESFA_EMAIL_ADDRESSES" => esfa_emails })
-    end
-
     it "sends the claims require clawback email" do
-      expect(claims_require_clawback_email.to).to match_array(esfa_emails)
-      expect(claims_require_clawback_email.subject).to eq("Claims requiring clawback - Claim funding for mentor training")
-      expect(claims_require_clawback_email.body.to_s.squish).to eq(<<~EMAIL.squish)
-        To ESFA,
+      ClimateControl.modify CLAIMS_ESFA_EMAIL_ADDRESSES: esfa_emails.join(",") do
+        expect(claims_require_clawback_email.to).to match_array(esfa_emails)
+        expect(claims_require_clawback_email.subject).to eq("Claims requiring clawback - Claim funding for mentor training")
+        expect(claims_require_clawback_email.body.to_s.squish).to eq(<<~EMAIL.squish)
+          To ESFA,
 
-        The claims in the CSV file link are ready for clawback— the link to the latest CSV file is valid for 7 days:
+          The claims in the CSV file link are ready for clawback— the link to the latest CSV file is valid for 7 days:
 
-        #{url_for_csv}
+          #{url_for_csv}
 
-        What you need to do:
+          What you need to do:
 
-        1. Check and validate the claims in the CSV file by marking them as ‘clawback_in_progress’ or ‘clawback_complete’ in the ‘claim_status’ column.
+          1. Check and validate the claims in the CSV file by marking them as ‘clawback_in_progress’ or ‘clawback_complete’ in the ‘claim_status’ column.
 
-        2. If you mark a claim as ‘clawback_in_progress’, add the reason to the ‘clawback_unsuccessful_reason’ column.
+          2. If you mark a claim as ‘clawback_in_progress’, add the reason to the ‘clawback_unsuccessful_reason’ column.
 
-        3. Reply to this email and attach the updated CSV file.
+          3. Reply to this email and attach the updated CSV file.
 
-        The Claim Support team will follow up on the reasons for the claims that you could not claw back and email you an updated version.
+          The Claim Support team will follow up on the reasons for the claims that you could not claw back and email you an updated version.
 
-        If you have a problem opening the link or it has expired, reply to this email and request that it be sent again.
+          If you have a problem opening the link or it has expired, reply to this email and request that it be sent again.
 
-        # Give feedback or report a problem
-        If you have any questions or feedback, please contact the team at [#{support_email}](mailto:#{support_email}).
+          # Give feedback or report a problem
+          If you have any questions or feedback, please contact the team at [#{support_email}](mailto:#{support_email}).
 
-        Regards
-        #{service_name} team
-      EMAIL
+          Regards
+          #{service_name} team
+        EMAIL
+      end
     end
   end
 end
