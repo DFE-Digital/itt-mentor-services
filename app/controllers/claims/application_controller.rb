0817e1b522ccb6762fcf108d9a4f4276862ac827
Daniel Dye <daniel.dye@education.gov.uk>
2024-12-19 11:15:53 +0000

    Add PunditNamespaces module
    The PunditNamespaces module adds a class method, `append_pundit_namespace`, to conveniently configure the nested namespace configuration of pundit methods within controllers.
    
    This allows us to easily add a pundit namespace to any controller.

--- app/controllers/claims/application_controller.rb
+++ app/controllers/claims/application_controller.rb
@@ -1,19 +1,9 @@
 class Claims::ApplicationController < ApplicationController
+  append_pundit_namespace :claims
+
   after_action :verify_authorized
 
-  def authorize(record, query = nil, policy_class: nil)
-    super([:claims, *unwrap_pundit_scope(record)], query, policy_class:)
-  end
-
-  def policy(record)
-    super([:claims, *unwrap_pundit_scope(record)])
-  end
-
   private
-
-  def pundit_policy_scope(scope)
-    super([:claims, *unwrap_pundit_scope(scope)])
-  end
 
   def has_school_accepted_grant_conditions?
     return true if current_user.support_user?
--- app/controllers/claims/support/application_controller.rb
+++ app/controllers/claims/support/application_controller.rb
@@ -1,19 +1,9 @@
 class Claims::Support::ApplicationController < Claims::ApplicationController
+  append_pundit_namespace :support
+
   before_action :authorize_user!
 
-  def authorize(record, query = nil, policy_class: nil)
-    super([:support, *unwrap_pundit_scope(record)], query, policy_class:)
-  end
-
-  def policy(record)
-    super([:support, *unwrap_pundit_scope(record)])
-  end
-
   private
-
-  def pundit_policy_scope(scope)
-    super([:support, *unwrap_pundit_scope(scope)])
-  end
 
   def authorize_user!
     return if current_user.support_user?
