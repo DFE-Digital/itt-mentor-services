1f104a1ab24b306c1f8dc77dc0844eccb04a46c9
dp-daly <danpatdaly@gmail.com>
2024-12-20 16:16:53 +0000

    Rollback migration and rerun using change_table block with bulk -> true

--- db/migrate/20241220141331_update_mentor_trainings_for_clawbacks.rb
+++ db/migrate/20241220141331_update_mentor_trainings_for_clawbacks.rb
@@ -1,7 +1,11 @@
 class UpdateMentorTrainingsForClawbacks < ActiveRecord::Migration[7.2]
   def change
-    safety_assured { remove_column :mentor_trainings, :hours_rejected }
-    add_column :mentor_trainings, :hours_clawed_back, :integer
-    add_column :mentor_trainings, :reason_clawed_back, :text
+    safety_assured do
+      change_table(:mentor_trainings, bulk: true) do |t|
+        t.remove :hours_rejected, type: :integer
+        t.integer :hours_clawed_back
+        t.text :reason_clawed_back
+      end
+    end
   end
 end
