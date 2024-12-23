40691f16d3b27a172926009e385767bc506b3ebf
Daniel Dye <daniel.dye@education.gov.uk>
2024-12-23 12:37:03 +0000

    Rename Claims::Support::ActivityLogController to Claims::Support::ClaimActivitiesController
    This is to match the resource to the controller.
    
    Previously, when the placeholder was introduced, the naming convention for ClaimActivity records was generalised as ActivityLog records for all kinds of activity. This was premature.

--- app/controllers/claims/support/claims/activity_logs_controller.rb
+++
@@ -1,3 +0,0 @@
-class Claims::Support::Claims::ActivityLogsController < Claims::Support::ApplicationController
-  before_action :skip_authorization
-end
---
+++ app/controllers/claims/support/claims/claim_activities_controller.rb
@@ -0,0 +1,3 @@
+class Claims::Support::Claims::ClaimActivitiesController < Claims::Support::ApplicationController
+  before_action :skip_authorization
+end
