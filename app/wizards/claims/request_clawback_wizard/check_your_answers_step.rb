64d2fe847fe322a1bf90f22e71026db346024d1a
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-01-03 09:54:49 +0000

    Add clawback logic to check your answers step

--- app/wizards/claims/request_clawback_wizard/check_your_answers_step.rb
+++ app/wizards/claims/request_clawback_wizard/check_your_answers_step.rb
@@ -1,2 +1,29 @@
 class Claims::RequestClawbackWizard::CheckYourAnswersStep < BaseStep
+  delegate :mentor_trainings, :steps, :step_name_for_mentor_training_clawback, :claim, to: :wizard
+  delegate :school, to: :claim
+  delegate :region_funding_available_per_hour, to: :school
+
+  def mentor_training_clawback_data(mentor_training)
+    steps.fetch(step_name_for_mentor_training_clawback(mentor_training))
+  end
+
+  def mentor_training_clawback_hours(mentor_training)
+    mentor_training_clawback_data(mentor_training).number_of_hours
+  end
+
+  def mentor_training_clawback_amount(mentor_training)
+    mentor_training_clawback_hours(mentor_training) * region_funding_available_per_hour
+  end
+
+  def mentor_training_clawback_reason(mentor_training)
+    mentor_training_clawback_data(mentor_training).reason_for_clawback
+  end
+
+  def total_clawback_hours
+    mentor_trainings.sum { |mentor_training| mentor_training_clawback_hours(mentor_training) }
+  end
+
+  def total_clawback_amount
+    mentor_trainings.sum { |mentor_training| mentor_training_clawback_hours(mentor_training) * region_funding_available_per_hour }
+  end
 end
