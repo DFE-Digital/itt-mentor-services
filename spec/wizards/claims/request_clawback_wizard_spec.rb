880bb7a14f92029310efab6f3e5824f4359b361b
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-01-03 12:03:54 +0000

    Add missed coverage

--- spec/wizards/claims/request_clawback_wizard_spec.rb
+++ spec/wizards/claims/request_clawback_wizard_spec.rb
@@ -29,4 +29,63 @@ RSpec.describe Claims::RequestClawbackWizard do
       expect(claim).to have_received(:save!)
     end
   end
+
+  describe "#mentor_trainings" do
+    it "returns the claim's mentor trainings that are not assured and orders them by mentor full name" do
+      expect(wizard.mentor_trainings).to eq([mentor_training])
+    end
+  end
+
+  describe "#step_name_for_mentor_training_clawback" do
+    it "returns the step name for the mentor training clawback step" do
+      expect(wizard.step_name_for_mentor_training_clawback(mentor_training)).to eq("mentor_training_clawback_#{mentor_training.id}".to_sym)
+    end
+  end
+
+  describe "#esfa_responses_for_mentor_trainings" do
+    context "when the mentor training clawback step is valid" do
+      let(:state) do
+        {
+          "mentor_training_clawback_#{mentor_training.id}" => {
+            "number_of_hours" => 5,
+            "reason_for_clawback" => "Some reason",
+          },
+        }
+      end
+
+      it "returns an array of hashes containing the mentor training id, number of hours and reason for clawback" do
+        expect(wizard.esfa_responses_for_mentor_trainings).to eq([{ id: mentor_training.id, number_of_hours: 5, reason_for_clawback: "Some reason" }])
+      end
+    end
+  end
+
+  describe "#submit_esfa_responses" do
+    let(:esfa_responses) { [{ id: mentor_training.id, number_of_hours: 5, reason_for_clawback: "Some reason" }] }
+
+    before do
+      allow(Claims::Claim::Clawback::ClawbackRequested).to receive(:call)
+    end
+
+    context "when the wizard is valid" do
+      let(:state) do
+        {
+          "mentor_training_clawback_#{mentor_training.id}" => {
+            "number_of_hours" => 5,
+            "reason_for_clawback" => "Some reason",
+          },
+        }
+      end
+
+      it "calls the ClawbackRequested service with the claim and esfa responses" do
+        wizard.submit_esfa_responses
+        expect(Claims::Claim::Clawback::ClawbackRequested).to have_received(:call).with(claim:, esfa_responses:)
+      end
+    end
+
+    context "when the wizard is invalid" do
+      it "raises an error" do
+        expect { wizard.submit_esfa_responses }.to raise_error("Invalid wizard state")
+      end
+    end
+  end
 end
