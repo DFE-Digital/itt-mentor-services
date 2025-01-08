679d12bc8b30c34cd0b0deb2296c7b3dac458044
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2025-01-08 17:00:34 +0000

    Output terraform external urls
    This is the new format as defined in the template. Used by
    teacher-services-cloud for their platform smoke tests.

--- terraform/application/outputs.tf
+++ terraform/application/outputs.tf
@@ -1,3 +1,7 @@
 output "ingress_hostnames" {
   value = [local.ingress_domain_map["INGRESS_CLAIMS_HOST"], local.ingress_domain_map["INGRESS_PLACEMENTS_HOST"]]
 }
+
+output "external_urls" {
+  value = [for domain in ["CLAIMS_HOST", "PLACEMENTS_HOST"] : "https://${local.app_env_values[domain]}/"]
+}
