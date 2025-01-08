b8fd86c805088f92a889651ce54a0b2308f1f493
Jamie <jamie.cleare@education.gov.uk>
2025-01-08 13:30:49 +0000

    Validate presence of action in activity log and conditionally show CSV file

--- spec/models/claims/claim_activity_spec.rb
+++ spec/models/claims/claim_activity_spec.rb
@@ -22,6 +22,10 @@
 require "rails_helper"
 
 RSpec.describe Claims::ClaimActivity, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:action) }
+  end
+
   describe "associations" do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:record) }
