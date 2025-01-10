f0eb921afa33aa8a70b66b08ea5605ac87a0c790
Jamie <jamie.cleare@education.gov.uk>
2025-01-10 09:58:33 +0000

    Changes made to clawback generate csv file

--- app/services/claims/clawback/generate_csv_file.rb
+++ app/services/claims/clawback/generate_csv_file.rb
@@ -7,11 +7,9 @@ class Claims::Clawback::GenerateCSVFile < ApplicationService
     school_name
     school_local_authority
     school_type_of_establishment
-    school_group
+    clawback_amouht
     claim_submission_date
-    mentor_full_name
-    reason_clawed_back
-    hours_clawed_back
+    claim_status
   ].freeze
 
   def initialize(claims:)
@@ -23,20 +21,16 @@ class Claims::Clawback::GenerateCSVFile < ApplicationService
       csv << HEADERS
 
       claims.each do |claim|
-        claim.mentor_trainings.not_assured.order_by_mentor_full_name.each do |mentor_training|
-          csv << [
-            claim.reference,
-            claim.school.urn,
-            claim.school_name,
-            claim.school.local_authority_name,
-            claim.school.type_of_establishment,
-            claim.school.group,
-            claim.submitted_at.iso8601,
-            mentor_training.mentor_full_name,
-            mentor_training.reason_clawed_back,
-            mentor_training.hours_clawed_back,
-          ]
-        end
+        csv << [
+          claim.reference,
+          claim.school.urn,
+          claim.school_name,
+          claim.school.local_authority_name,
+          claim.school.type_of_establishment,
+          claim.total_clawback_amount,
+          claim.submitted_at.iso8601,
+          claim.status,
+        ]
       end
 
       csv
