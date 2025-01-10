f31c8d59f243a346f0c06e6af628689c620c28d5
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-01-10 11:27:23 +0000

    Process the sampling reason and save it to the claim

--- app/services/claims/claim/sampling/in_progress.rb
+++ app/services/claims/claim/sampling/in_progress.rb
@@ -1,13 +1,14 @@
 class Claims::Claim::Sampling::InProgress < ApplicationService
-  def initialize(claim:)
+  def initialize(claim:, sampling_reason:)
     @claim = claim
+    @sampling_reason = sampling_reason
   end
 
   def call
-    claim.update!(status: :sampling_in_progress)
+    claim.update!(status: :sampling_in_progress, sampling_reason:)
   end
 
   private
 
-  attr_reader :claim
+  attr_reader :claim, :sampling_reason
 end
--- spec/services/claims/claim/sampling/in_progress_spec.rb
+++ spec/services/claims/claim/sampling/in_progress_spec.rb
@@ -2,14 +2,21 @@ require "rails_helper"
 
 describe Claims::Claim::Sampling::InProgress do
   let!(:claim) { create(:claim, :submitted, status: :paid) }
+  let(:sampling_reason) { "ABCD" }
 
   describe "#call" do
-    subject(:call) { described_class.call(claim:) }
+    subject(:call) { described_class.call(claim:, sampling_reason:) }
 
     it "changes to status of the claim to provider not approved" do
       expect { call }.to change(claim, :status)
         .from("paid")
         .to("sampling_in_progress")
+    end
+
+    it "updates the sampling reason of the claim" do
+      expect { call }.to change(claim, :sampling_reason)
+        .from(nil)
+        .to("ABCD")
     end
   end
 end
