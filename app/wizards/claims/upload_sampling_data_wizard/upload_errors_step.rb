17fe6b020abc9ca7834125d50f31036b18157f7a
dp-daly <danpatdaly@gmail.com>
2025-01-29 18:54:00 +0000

    [WIP] Add table to upload errors view

--- app/wizards/claims/upload_sampling_data_wizard/upload_errors_step.rb
+++ app/wizards/claims/upload_sampling_data_wizard/upload_errors_step.rb
@@ -1,2 +1,20 @@
 class Claims::UploadSamplingDataWizard::UploadErrorsStep < BaseStep
+  delegate :invalid_claim_rows,
+           :file_name,
+           :csv,
+           to: :upload_step
+
+  def row_indexes_with_errors
+    invalid_claim_rows.uniq.sort
+  end
+
+  def error_count
+    invalid_claim_rows.count
+  end
+
+  private
+
+  def upload_step
+    @upload_step ||= wizard.steps.fetch(:upload)
+  end
 end
