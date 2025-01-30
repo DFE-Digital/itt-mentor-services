c1d068c1ea8c8a1b8eb71f854c67e52f2e6bc059
Jamie <jamie.cleare@education.gov.uk>
2025-01-30 16:24:07 +0000

    WIP

--- app/wizards/claims/upload_payer_payment_response_wizard/upload_step.rb
+++ app/wizards/claims/upload_payer_payment_response_wizard/upload_step.rb
@@ -1,2 +1,12 @@
 class Claims::UploadPayerPaymentResponseWizard::UploadStep < BaseStep
+  attribute :csv_upload
+  attribute :csv_content
+  attribute :file_name
+  # input validation attributes
+  attribute :invalid_claim_rows, default: []
+  attribute :invalid_claim_status_rows, default: []
+
+  def csv_inputs_valid?
+    true
+  end
 end
