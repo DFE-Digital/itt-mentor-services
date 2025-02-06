eca5d6ee4fda985016496a9278c23df928c5b36c
dp-daly <danpatdaly@gmail.com>
2025-02-06 16:40:45 +0000

    Replace claim_ids attribute with update_claim_details

--- app/wizards/claims/upload_sampling_data_wizard/upload_step.rb
+++ app/wizards/claims/upload_sampling_data_wizard/upload_step.rb
@@ -2,7 +2,6 @@ class Claims::UploadSamplingDataWizard::UploadStep < BaseStep
   attribute :csv_upload
   attribute :csv_content
   attribute :file_name
-  attribute :claim_ids, default: []
   attribute :invalid_claim_rows, default: []
   attribute :missing_sample_reason_rows, default: []
 
@@ -61,18 +60,8 @@ class Claims::UploadSamplingDataWizard::UploadStep < BaseStep
     validate_csv_file
     return if errors.present?
 
-    reset_input_attributes
-
-    self.file_name = csv_upload.original_filename
-
-    CSV.parse(read_csv, headers: true) do |row|
-      claim = paid_claims.find_by(reference: row["claim_reference"])
-      break if claim.blank?
-
-      claim_ids << claim.id
-    end
-
     assign_csv_content
+    self.file_name = csv_upload.original_filename
 
     self.csv_upload = nil
   end
