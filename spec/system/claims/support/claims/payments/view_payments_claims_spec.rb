095feb8c9801bb1cbe9d7594928905dd29e7b39e
Jamie <jamie.cleare@education.gov.uk>
2025-02-11 15:05:45 +0000

    Add actions calculator to the payments index

--- spec/system/claims/support/claims/payments/view_payments_claims_spec.rb
+++ spec/system/claims/support/claims/payments/view_payments_claims_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "View payments claims", :js, service: :claims, type: :system do
+RSpec.describe "View payments claims", service: :claims, type: :system do
   let!(:support_user) { create(:claims_support_user) }
 
   before do
@@ -13,11 +13,36 @@ RSpec.describe "View payments claims", :js, service: :claims, type: :system do
     then_i_see("There are no claims waiting to be processed.")
   end
 
-  scenario "Support user visits the claims index page with claims due for payments processing" do
-    information_requested_claim, information_sent_claim = given_there_are_claims_due_for_payments_processing
+  scenario "Support user visits the claims index page when there are submitted claims" do
+    given_there_are_submitted_claims
     when_i_visit_claims_payments_index_page
-    then_i_see("2 claims need processing")
-    and_i_see_a_list_of_claims([information_requested_claim, information_sent_claim])
+    then_i_see_claims_are_waiting_to_be_sent_to_payer
+  end
+
+  scenario "Support user visits the claims index page when there are claims waiting for a payer response" do
+    given_there_are_payment_in_progress_claims
+    when_i_visit_claims_payments_index_page
+    then_i_see_claims_are_awaiting_a_response_from_payer
+  end
+
+  scenario "Support user visits the claims index page with claims due for payments processing" do
+    given_there_are_claims_due_for_payments_processing
+    when_i_visit_claims_payments_index_page
+    then_i_see_claims_require_more_information
+    and_i_see_an_information_requested_claim
+    and_i_see_an_information_sent_claim
+  end
+
+  scenario "Support user visits the claims index page with claims in all stages of the payments process" do
+    given_there_are_claims_due_for_payments_processing
+    and_there_are_submitted_claims
+    and_there_are_payment_in_progress_claims
+    when_i_visit_claims_payments_index_page
+    then_i_see_claims_are_waiting_to_be_sent_to_payer
+    and_i_see_claims_are_waiting_to_be_sent_to_payer
+    and_i_see_claims_require_more_information
+    and_i_see_an_information_requested_claim
+    and_i_see_an_information_sent_claim
   end
 
   private
@@ -28,10 +53,8 @@ RSpec.describe "View payments claims", :js, service: :claims, type: :system do
   end
 
   def given_there_are_claims_due_for_payments_processing
-    [
-      create(:claim, :payment_information_requested),
-      create(:claim, :payment_information_sent),
-    ]
+    @information_requested_claim = create(:claim, :payment_information_requested)
+    @information_sent_claim = create(:claim, :payment_information_sent)
   end
 
   def when_i_visit_claims_payments_index_page
@@ -43,11 +66,72 @@ RSpec.describe "View payments claims", :js, service: :claims, type: :system do
     expect(page).to have_content(message)
   end
 
-  def and_i_see_a_list_of_claims(claims)
-    claims.each_with_index do |claim, index|
-      within(".claim-card:nth-child(#{index + 1})") do
-        expect(page).to have_content(claim.reference)
-      end
+  def and_i_see_an_information_requested_claim
+    expect(page).to have_claim_card({
+      "title" => "#{@information_requested_claim.reference} - #{@information_requested_claim.school.name}",
+      "url" => "/support/claims/payments/claims/#{@information_requested_claim.id}",
+      "status" => "Payer needs information",
+      "academic_year" => @information_requested_claim.academic_year.name,
+      "provider_name" => @information_requested_claim.provider.name,
+      "submitted_at" => I18n.l(@information_requested_claim.submitted_at.to_date, format: :long),
+      "amount" => "£0.00",
+    })
+  end
+
+  def and_i_see_an_information_sent_claim
+    expect(page).to have_claim_card({
+      "title" => "#{@information_sent_claim.reference} - #{@information_sent_claim.school.name}",
+      "url" => "/support/claims/payments/claims/#{@information_sent_claim.id}",
+      "status" => "Information sent to payer",
+      "academic_year" => @information_sent_claim.academic_year.name,
+      "provider_name" => @information_sent_claim.provider.name,
+      "submitted_at" => I18n.l(@information_sent_claim.submitted_at.to_date, format: :long),
+      "amount" => "£0.00",
+    })
+  end
+
+  def given_there_are_submitted_claims
+    create_list(:claim, 3, :submitted)
+  end
+  alias_method :and_there_are_submitted_claims, :given_there_are_submitted_claims
+
+  def given_there_are_payment_in_progress_claims
+    create_list(:claim, 5, :payment_in_progress)
+  end
+  alias_method :and_there_are_payment_in_progress_claims,
+               :given_there_are_payment_in_progress_claims
+
+  def then_i_see_claims_are_awaiting_a_response_from_payer
+    within("#action_calculator") do
+      expect(page).to have_element(
+        :p,
+        text: "5 claims awaiting a response from payer",
+        class: "govuk-body",
+      )
     end
   end
+
+  def then_i_see_claims_are_waiting_to_be_sent_to_payer
+    within("#action_calculator") do
+      expect(page).to have_element(
+        :p,
+        text: "3 claims waiting to be sent to payer",
+        class: "govuk-body",
+      )
+    end
+  end
+  alias_method :and_i_see_claims_are_waiting_to_be_sent_to_payer,
+               :then_i_see_claims_are_waiting_to_be_sent_to_payer
+
+  def then_i_see_claims_require_more_information
+    within("#action_calculator") do
+      expect(page).to have_element(
+        :p,
+        text: "2 claims require more information",
+        class: "govuk-body",
+      )
+    end
+  end
+  alias_method :and_i_see_claims_require_more_information,
+               :then_i_see_claims_require_more_information
 end
