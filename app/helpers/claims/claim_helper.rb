e7445aebdc5f082a5b19acf0f4c4105490ec190a
Jamie <jamie.cleare@education.gov.uk>
2025-02-14 09:29:39 +0000

    Remove unnecessary claim helper

--- app/helpers/claims/claim_helper.rb
+++ app/helpers/claims/claim_helper.rb
@@ -4,20 +4,4 @@ module Claims::ClaimHelper
       Claims::Claim::DRAFT_STATUSES.map(&:to_s).include?(status)
     }.sort
   end
-
-  def claim_provider_response(claim)
-    not_assured_mentor_trainings = claim.mentor_trainings.not_assured
-    return "" if not_assured_mentor_trainings.blank?
-
-    content_tag(:ul, class: "govuk-list") do
-      not_assured_mentor_trainings.order_by_mentor_full_name.each do |mentor_training|
-        concat(
-          content_tag(
-            :li,
-            "#{mentor_training.mentor_full_name}: #{mentor_training.reason_not_assured}",
-          ),
-        )
-      end
-    end
-  end
 end
