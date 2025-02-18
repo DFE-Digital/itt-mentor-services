724a9106b34ee81598a46a46e2801cd1c23711ef
Jamie <jamie.cleare@education.gov.uk>
2025-02-18 22:14:32 +0000

    Fix all mailer previews

--- spec/mailers/previews/claims/support_user_mailer_preview.rb
+++ spec/mailers/previews/claims/support_user_mailer_preview.rb
@@ -1,9 +1,15 @@
 class Claims::SupportUserMailerPreview < ActionMailer::Preview
   def support_user_invitation
-    Claims::SupportUserMailer.support_user_invitation(Claims::SupportUser.first)
+    Claims::SupportUserMailer.support_user_invitation(support_user)
   end
 
   def support_user_removal_notification
-    Claims::SupportUserMailer.support_user_removal_notification(Claims::SupportUser.first)
+    Claims::SupportUserMailer.support_user_removal_notification(support_user)
+  end
+
+  private
+
+  def support_user
+    FactoryBot.build_stubbed(:claims_support_user)
   end
 end
--- spec/mailers/previews/placements/support_user_mailer_preview.rb
+++ spec/mailers/previews/placements/support_user_mailer_preview.rb
@@ -1,9 +1,15 @@
 class Placements::SupportUserMailerPreview < ActionMailer::Preview
   def support_user_invitation
-    Placements::SupportUserMailer.support_user_invitation(Placements::SupportUser.first)
+    Placements::SupportUserMailer.support_user_invitation(support_user)
   end
 
   def support_user_removal_notification
-    Placements::SupportUserMailer.support_user_removal_notification(Placements::SupportUser.first)
+    Placements::SupportUserMailer.support_user_removal_notification(support_user)
+  end
+
+  private
+
+  def support_user
+    FactoryBot.build_stubbed(:placements_support_user)
   end
 end
