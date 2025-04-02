d782a029183e76f5d2569bb6ea7c8aa86b1234d1
Jamie <jamie.cleare@education.gov.uk>
2025-04-02 12:47:49 +0100

    Select any provider when assigning to a placement

---
+++ app/wizards/placements/edit_placement_wizard/provider_selection_step.rb
@@ -0,0 +1,25 @@
+class Placements::EditPlacementWizard::ProviderSelectionStep < BaseStep
+  attribute :provider_id
+
+  validate :id_presence
+
+  def provider_name
+    @provider_name ||= provider&.name
+  end
+
+  def provider
+    @provider ||= Provider.find_by(id: provider_id)
+  end
+
+  def scope
+    wizard_name = class_to_path(wizard.class)
+    step_name = class_to_path(self.class)
+    "placements_#{wizard_name}_#{step_name}"
+  end
+
+  private
+
+  def class_to_path(klass)
+    klass.name.demodulize.underscore
+  end
+end
