ede5bc078ab7939de101850b5fe12c1a5cf30ac0
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-04-08 09:58:49 +0100

    Create interest tag component

---
+++ app/components/placements/schools/interest_tag_component.rb
@@ -0,0 +1,68 @@
+class Placements::Schools::InterestTagComponent < ApplicationComponent
+  INTEREST_COLOURS = {
+    "open" => "yellow",
+    "unfilled_placements" => "green",
+    "filled_placements" => "blue",
+    "not_open" => "red",
+  }.freeze
+
+  INTEREST_TEXT = {
+    "open" => I18n.t("components.placements.schools.interest_tag_component.open"),
+    "unfilled_placements" => I18n.t("components.placements.schools.interest_tag_component.unfilled_placements"),
+    "filled_placements" => I18n.t("components.placements.schools.interest_tag_component.filled_placements"),
+    "not_open" => I18n.t("components.placements.schools.interest_tag_component.not_open"),
+  }.freeze
+
+  private_constant :INTEREST_COLOURS, :INTEREST_TEXT
+
+  def initialize(school:, classes: [], html_attributes: {})
+    super(classes:, html_attributes:)
+
+    @school = school
+  end
+
+  private
+
+  attr_reader :school
+
+  def interest_colour
+    INTEREST_COLOURS[calculated_status]
+  end
+
+  def interest_text
+    INTEREST_TEXT[calculated_status]
+  end
+
+  def calculated_status
+    case
+    when actively_looking? && only_has_unavailable_placements?
+      "filled_placements"
+    when actively_looking? && has_available_placements?
+      "unfilled_placements"
+    when actively_looking?
+      "open"
+    when not_looking?
+      "not_open"
+    end
+  end
+
+  def render_interest_tag?
+    school.current_hosting_interest_appetite.present?
+  end
+
+  def actively_looking?
+    school.current_hosting_interest_appetite == "actively_looking"
+  end
+
+  def not_looking?
+    school.current_hosting_interest_appetite == "not_open"
+  end
+
+  def only_has_unavailable_placements?
+    @only_has_unavailable_placements ||= school.unavailable_placements.exists? && school.available_placements.empty?
+  end
+
+  def has_available_placements?
+    @has_available_placements ||= school.available_placements.exists?
+  end
+end
---
+++ spec/components/placements/schools/interest_tag_component_spec.rb
@@ -0,0 +1,88 @@
+require "rails_helper"
+
+RSpec.describe Placements::Schools::InterestTagComponent, type: :component do
+  subject(:component) do
+    described_class.new(school:)
+  end
+
+  let(:school) { create(:placements_school, hosting_interests:, placements:) }
+
+  before do
+    school
+    render_inline(component)
+  end
+
+  context "when the school is open to hosting" do
+    let(:hosting_interests) { [build(:hosting_interest, appetite: "actively_looking")] }
+
+    context "when the school has available placements" do
+      let(:placements) { [build(:placement)] }
+
+      it "renders the correct text" do
+        expect(page).to have_content "Has unfilled placements"
+      end
+
+      it "renders the correct tag" do
+        expect(page).to have_css(".govuk-tag--green")
+      end
+    end
+
+    context "when the school has available and unavailable placements" do
+      let(:placements) { [build(:placement), build(:placement, provider: build(:provider))] }
+
+      it "renders the correct text" do
+        expect(page).to have_content "Has unfilled placements"
+      end
+
+      it "renders the correct tag" do
+        expect(page).to have_css(".govuk-tag--green")
+      end
+
+    end
+
+    context "when the school only has unavailable placements" do
+      let(:placements) { [build(:placement, provider: build(:provider))] }
+
+      it "renders the correct text" do
+        expect(page).to have_content "Already organised placements"
+      end
+
+      it "renders the correct tag" do
+        expect(page).to have_css(".govuk-tag--blue")
+      end
+    end
+
+    context "when the school has no placements" do
+      let(:placements) { [] }
+
+      it "renders the correct text" do
+        expect(page).to have_content "Open to hosting"
+      end
+
+      it "renders the correct tag" do
+        expect(page).to have_css(".govuk-tag--yellow")
+      end
+    end
+  end
+
+  context "when the school is not open to hosting" do
+    let(:hosting_interests) { [build(:hosting_interest, appetite: "not_open")] }
+    let(:placements) { [] }
+
+    it "renders the correct text" do
+      expect(page).to have_content "Not hosting"
+    end
+
+    it "renders the correct tag" do
+      expect(page).to have_css(".govuk-tag--red")
+    end
+  end
+
+  context "when the school has not specified a hosting interest" do
+    let(:school) { create(:placements_school) }
+
+    it "does not render the tag" do
+      expect(page).not_to have_css(".govuk-tag")
+    end
+  end
+end
