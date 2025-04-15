777d05408a316c22363afa6ead906f13434979b2
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-04-15 13:14:14 +0100

    Add find a school routes

--- config/routes/placements.rb
+++ config/routes/placements.rb
@@ -140,6 +140,14 @@ scope module: :placements,
       end
 
       resources :placements, only: %i[index show]
+
+      resources :find, only: %i[index show] do
+        member do
+          get "placements", to: "find#placements"
+          get "placement_information", to: "find#placement_information"
+          get "school_details", to: "find#school_details"
+        end
+      end
     end
   end
 end
