cb9e38f22fd7c1bd6363ef6832105eaa6a7d2e15
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-04-15 13:42:21 +0100

    Add school summary component

---
+++ app/components/placements/schools/summary_component.rb
@@ -0,0 +1,82 @@
+class Placements::Schools::SummaryComponent < ApplicationComponent
+  with_collection_parameter :school
+  attr_reader :school, :provider, :location_coordinates
+
+  delegate :school_contact, :current_hosting_interest_appetite, to: :school, allow_nil: true
+  delegate :first_name, :last_name, :email_address, to: :school_contact, prefix: true, allow_nil: true
+  delegate :available_placements, :unavailable_placements, to: :school, prefix: true, allow_nil: true
+
+  def initialize(school:, provider:, location_coordinates: nil, classes: [], html_attributes: {})
+    super(classes:, html_attributes:)
+
+    @provider = provider
+    @school = school.decorate
+    @location_coordinates = location_coordinates
+  end
+
+  def distance_from_location
+    distance = school.distance_to(location_coordinates).round(1)
+    I18n.t("components.placement.summary_component.distance_in_miles", distance:)
+  end
+
+  def available_placements_count
+    @available_placements_count ||= school_available_placements.count
+  end
+
+  def unavailable_placements_count
+    @unavailable_placements_count ||= school_unavailable_placements.count
+  end
+
+  def placement_status
+    I18n.t(
+      "components.school.summary_component.#{latest_academic_year_name ? "placements_hosted" : "placements_never_hosted"}",
+      academic_year_name: latest_academic_year_name,
+    )
+  end
+
+  def unfilled_subjects
+    @unfilled_subjects ||= Subject.where(id: unfilled_subject_ids)
+  end
+
+  def filled_subjects
+    @filled_subjects ||= Subject.where(id: filled_subject_ids)
+  end
+
+  def open_to_hosting?
+    current_hosting_interest_appetite == "actively_looking"
+  end
+
+  def not_open_to_hosting?
+    current_hosting_interest_appetite.blank? || current_hosting_interest_appetite == "not_open"
+  end
+
+  private
+
+  def latest_academic_year
+    @latest_academic_year ||= academic_years_for_school.order_by_date.first
+  end
+
+  def latest_academic_year_name
+    latest_academic_year&.name
+  end
+
+  def previous_academic_year_placements_count
+    @previous_academic_year_placements_count ||= school.placements.where(academic_year: Placements::AcademicYear.current.previous).count
+  end
+
+  def academic_years_for_school
+    Placements::AcademicYear.where(id: school.placements.pluck(:academic_year_id), ends_on: ..current_academic_year_start)
+  end
+
+  def current_academic_year_start
+    @current_academic_year_start ||= Placements::AcademicYear.current.starts_on
+  end
+
+  def unfilled_subject_ids
+    school.placements.where(academic_year_id: Placements::AcademicYear.current.id, provider_id: nil).pluck(:subject_id)
+  end
+
+  def filled_subject_ids
+    school.placements.where(academic_year_id: Placements::AcademicYear.current.id).where.not(provider_id: nil).pluck(:subject_id)
+  end
+end
---
+++ spec/components/placements/schools/summary_component_spec.rb
@@ -0,0 +1,410 @@
+require "rails_helper"
+
+RSpec.describe Placements::Schools::SummaryComponent, type: :component do
+  subject(:component) do
+    described_class.new(school:, provider:)
+  end
+
+  let(:provider) { create(:placements_provider) }
+  let(:hosting_interests) { [] }
+  let(:placements) { [] }
+  let(:school_contact) { build(:school_contact) }
+  let(:school) do
+    create(:placements_school,
+           hosting_interests:,
+           placements:,
+           school_contact:,
+           phase: "All-through",
+           name: "Hogwarts",
+           group: "Local authority maintained schools",
+           minimum_age: 5,
+           maximum_age: 11,
+           longitude: -3.0581722753,
+           latitude: 52.8409318812,
+           address1: "Magical Lane",
+           postcode: "SW1A 1AA",
+           town: "London")
+  end
+
+  before do
+    allow(school).to receive_messages(distance_to: 42, drive_travel_duration: "15 mins", transit_travel_duration: "30 mins", walk_travel_duration: "45 mins")
+    render_inline(component)
+  end
+
+  context "when the school is open to hosting" do
+    let(:hosting_interests) { build_list(:hosting_interest, 1, appetite: "actively_looking") }
+
+    context "when the school does not have any placements" do
+      it "displays the school's hosting interest" do
+        expect(page).to have_content("Open to hosting")
+      end
+
+      it "displays a link to the school's details page" do
+        expect(page).to have_link("Hogwarts, London", href: "/providers/#{provider.id}/find/#{school.id}/placement_information")
+      end
+
+      it "displays the school's name" do
+        expect(page).to have_content("Hogwarts")
+      end
+
+      it "displays the school's details", :aggregate_failures do
+        expect(page).to have_content("School details")
+        expect(page).to have_content("Phase")
+        expect(page).to have_content("All-through")
+        expect(page).to have_content("Age range")
+        expect(page).to have_content("5 to 11")
+        expect(page).to have_content("Establishment group")
+        expect(page).to have_content("Local authority maintained schools")
+      end
+
+      it "displays placement information", :aggregate_failures do
+        expect(page).to have_content("Placement information")
+        expect(page).to have_content("Last offered")
+        expect(page).to have_content("This school has not previously hosted placements")
+        expect(page).to have_content("Contact")
+        expect(page).to have_content("Placement Coordinator")
+        expect(page).to have_content("placement_coordinator@example.school")
+      end
+
+      context "when location coordinates have been provided" do
+        subject(:component) do
+          described_class.new(school:, provider:, location_coordinates: "41.40338, 2.17403")
+        end
+
+        it "displays the getting there section", :aggregate_failures do
+          expect(page).to have_content("Getting there")
+          expect(page).to have_content("Address")
+          expect(page).to have_content("Magical Lane")
+          expect(page).to have_content("London")
+          expect(page).to have_content("SW1A 1AA")
+          expect(page).to have_content("Distance")
+          expect(page).to have_content("42 miles")
+          expect(page).to have_content("Travel time")
+          expect(page).to have_content("30 minutes by public transport")
+          expect(page).to have_content("15 minutes drive")
+          expect(page).to have_content("45 minutes walk")
+        end
+      end
+    end
+
+    context "when the school has unfilled placements" do
+      let(:placements) { build_list(:placement, 1) }
+
+      it "displays the school's hosting interest" do
+        expect(page).to have_content("Has unfilled placements")
+      end
+
+      it "displays a link to the school's details page" do
+        expect(page).to have_link("Hogwarts, London", href: "/providers/#{provider.id}/find/#{school.id}/placements")
+      end
+
+      it "displays the school's name" do
+        expect(page).to have_content("Hogwarts")
+      end
+
+      it "displays the school's details", :aggregate_failures do
+        expect(page).to have_content("School details")
+        expect(page).to have_content("Phase")
+        expect(page).to have_content("All-through")
+        expect(page).to have_content("Age range")
+        expect(page).to have_content("5 to 11")
+        expect(page).to have_content("Establishment group")
+        expect(page).to have_content("Local authority maintained schools")
+      end
+
+      it "displays placement information", :aggregate_failures do
+        expect(page).to have_content("Placement information")
+        expect(page).to have_content("Unfilled placements")
+        expect(page).to have_link("1 unfilled placement", href: "/providers/#{provider.id}/find/#{school.id}/placements")
+        expect(page).to have_content("Hosting subjects")
+        expect(page).to have_content("0 filled placements")
+        expect(page).to have_content("Last offered")
+        expect(page).to have_content("This school has not previously hosted placements")
+        expect(page).to have_content("Contact")
+        expect(page).to have_content("Placement Coordinator")
+        expect(page).to have_content("placement_coordinator@example.school")
+      end
+
+      context "when location coordinates have been provided" do
+        subject(:component) do
+          described_class.new(school:, provider:, location_coordinates: "41.40338, 2.17403")
+        end
+
+        it "displays the getting there section", :aggregate_failures do
+          expect(page).to have_content("Getting there")
+          expect(page).to have_content("Address")
+          expect(page).to have_content("Magical Lane")
+          expect(page).to have_content("London")
+          expect(page).to have_content("SW1A 1AA")
+          expect(page).to have_content("Distance")
+          expect(page).to have_content("42 miles")
+          expect(page).to have_content("Travel time")
+          expect(page).to have_content("30 minutes by public transport")
+          expect(page).to have_content("15 minutes drive")
+          expect(page).to have_content("45 minutes walk")
+        end
+      end
+    end
+
+    context "when the school has unfilled and filled placements" do
+      let(:placements) { [build(:placement, provider:), build(:placement)] }
+
+      it "displays the school's hosting interest" do
+        expect(page).to have_content("Has unfilled placements")
+      end
+
+      it "displays a link to the school's details page" do
+        expect(page).to have_link("Hogwarts, London", href: "/providers/#{provider.id}/find/#{school.id}/placements")
+      end
+
+      it "displays the school's name" do
+        expect(page).to have_content("Hogwarts")
+      end
+
+      it "displays the school's details", :aggregate_failures do
+        expect(page).to have_content("School details")
+        expect(page).to have_content("Phase")
+        expect(page).to have_content("All-through")
+        expect(page).to have_content("Age range")
+        expect(page).to have_content("5 to 11")
+        expect(page).to have_content("Establishment group")
+        expect(page).to have_content("Local authority maintained schools")
+      end
+
+      it "displays placement information", :aggregate_failures do
+        expect(page).to have_content("Placement information")
+        expect(page).to have_content("Unfilled placements")
+        expect(page).to have_link("1 unfilled placement", href: "/providers/#{provider.id}/find/#{school.id}/placements")
+        expect(page).to have_content("Hosting subjects")
+        expect(page).to have_content("1 filled placement")
+        expect(page).to have_content("Last offered")
+        expect(page).to have_content("This school has not previously hosted placements")
+        expect(page).to have_content("Contact")
+        expect(page).to have_content("Placement Coordinator")
+        expect(page).to have_content("placement_coordinator@example.school")
+      end
+
+      context "when location coordinates have been provided" do
+        subject(:component) do
+          described_class.new(school:, provider:, location_coordinates: "41.40338, 2.17403")
+        end
+
+        it "displays the getting there section", :aggregate_failures do
+          expect(page).to have_content("Getting there")
+          expect(page).to have_content("Address")
+          expect(page).to have_content("Magical Lane")
+          expect(page).to have_content("London")
+          expect(page).to have_content("SW1A 1AA")
+          expect(page).to have_content("Distance")
+          expect(page).to have_content("42 miles")
+          expect(page).to have_content("Travel time")
+          expect(page).to have_content("30 minutes by public transport")
+          expect(page).to have_content("15 minutes drive")
+          expect(page).to have_content("45 minutes walk")
+        end
+      end
+    end
+
+    context "when the school only has filled placements" do
+      let(:placements) { build_list(:placement, 1, provider:) }
+
+      it "displays the school's hosting interest" do
+        expect(page).to have_content("Already organised placements")
+      end
+
+      it "displays a link to the school's details page" do
+        expect(page).to have_link("Hogwarts, London", href: "/providers/#{provider.id}/find/#{school.id}/placements")
+      end
+
+      it "displays the school's name" do
+        expect(page).to have_content("Hogwarts")
+      end
+
+      it "displays the school's details", :aggregate_failures do
+        expect(page).to have_content("School details")
+        expect(page).to have_content("Phase")
+        expect(page).to have_content("All-through")
+        expect(page).to have_content("Age range")
+        expect(page).to have_content("5 to 11")
+        expect(page).to have_content("Establishment group")
+        expect(page).to have_content("Local authority maintained schools")
+      end
+
+      it "displays placement information", :aggregate_failures do
+        expect(page).to have_content("Placement information")
+        expect(page).to have_content("Hosting subjects")
+        expect(page).to have_content("1 filled placement")
+        expect(page).to have_content("Last offered")
+        expect(page).to have_content("This school has not previously hosted placements")
+        expect(page).to have_content("Contact")
+        expect(page).to have_content("Placement Coordinator")
+        expect(page).to have_content("placement_coordinator@example.school")
+      end
+
+      context "when location coordinates have been provided" do
+        subject(:component) do
+          described_class.new(school:, provider:, location_coordinates: "41.40338, 2.17403")
+        end
+
+        it "displays the getting there section", :aggregate_failures do
+          expect(page).to have_content("Getting there")
+          expect(page).to have_content("Address")
+          expect(page).to have_content("Magical Lane")
+          expect(page).to have_content("London")
+          expect(page).to have_content("SW1A 1AA")
+          expect(page).to have_content("Distance")
+          expect(page).to have_content("42 miles")
+          expect(page).to have_content("Travel time")
+          expect(page).to have_content("30 minutes by public transport")
+          expect(page).to have_content("15 minutes drive")
+          expect(page).to have_content("45 minutes walk")
+        end
+      end
+    end
+
+    context "when the school has previously hosted placements" do
+      let(:previous_academic_year) { Placements::AcademicYear.current.previous }
+      let(:placements) { build_list(:placement, 1, academic_year: previous_academic_year, provider:) }
+
+      it "displays the school's hosting interest" do
+        expect(page).to have_content("Open to hosting")
+      end
+
+      it "displays a link to the school's details page" do
+        expect(page).to have_link("Hogwarts, London", href: "/providers/#{provider.id}/find/#{school.id}/placement_information")
+      end
+
+      it "displays the school's name" do
+        expect(page).to have_content("Hogwarts")
+      end
+
+      it "displays the school's details", :aggregate_failures do
+        expect(page).to have_content("School details")
+        expect(page).to have_content("Phase")
+        expect(page).to have_content("All-through")
+        expect(page).to have_content("Age range")
+        expect(page).to have_content("5 to 11")
+        expect(page).to have_content("Establishment group")
+        expect(page).to have_content("Local authority maintained schools")
+      end
+
+      it "displays placement information", :aggregate_failures do
+        expect(page).to have_content("Placement information")
+        expect(page).to have_content("Last offered")
+        expect(page).to have_content("1 subject in #{previous_academic_year.name}")
+        expect(page).to have_content("Contact")
+        expect(page).to have_content("Placement Coordinator")
+        expect(page).to have_content("placement_coordinator@example.school")
+      end
+
+      context "when location coordinates have been provided" do
+        subject(:component) do
+          described_class.new(school:, provider:, location_coordinates: "41.40338, 2.17403")
+        end
+
+        it "displays the getting there section", :aggregate_failures do
+          expect(page).to have_content("Getting there")
+          expect(page).to have_content("Address")
+          expect(page).to have_content("Magical Lane")
+          expect(page).to have_content("London")
+          expect(page).to have_content("SW1A 1AA")
+          expect(page).to have_content("Distance")
+          expect(page).to have_content("42 miles")
+          expect(page).to have_content("Travel time")
+          expect(page).to have_content("30 minutes by public transport")
+          expect(page).to have_content("15 minutes drive")
+          expect(page).to have_content("45 minutes walk")
+        end
+      end
+    end
+  end
+
+  context "when the school is not open to hosting" do
+    let(:hosting_interests) { build_list(:hosting_interest, 1, appetite: "not_open") }
+
+    it "displays the school's hosting interest" do
+      expect(page).to have_content("Not hosting")
+    end
+
+    it "displays the school's name" do
+      expect(page).to have_content("Hogwarts")
+    end
+
+    it "displays the school's details", :aggregate_failures do
+      expect(page).to have_content("School details")
+      expect(page).to have_content("Phase")
+      expect(page).to have_content("All-through")
+      expect(page).to have_content("Age range")
+      expect(page).to have_content("5 to 11")
+      expect(page).to have_content("Establishment group")
+      expect(page).to have_content("Local authority maintained schools")
+    end
+
+    it "displays placement information", :aggregate_failures do
+      expect(page).to have_content("Placement information")
+      expect(page).to have_content("Placement subjects")
+      expect(page).to have_content("Not open to hosting placements")
+      expect(page).to have_content("Placement status")
+      expect(page).to have_content("This school has not previously hosted placements")
+    end
+
+    it "displays a message indicating the school doesn't want to be contacted" do
+      expect(page).to have_content("This school does not wish to be contacted this academic year.")
+    end
+
+    context "when location coordinates have been provided" do
+      subject(:component) do
+        described_class.new(school:, provider:, location_coordinates: "41.40338, 2.17403")
+      end
+
+      it "does not display the getting there section" do
+        expect(page).not_to have_content("Getting there")
+      end
+    end
+  end
+
+  context "when the school has not registered a hosting interest" do
+    it "does not display a hosting interest" do
+      expect(page).not_to have_content("Not hosting")
+      expect(page).not_to have_content("Open to hosting")
+      expect(page).not_to have_content("Already organised placements")
+      expect(page).not_to have_content("Unfilled placements")
+    end
+
+    it "displays the school's name" do
+      expect(page).to have_content("Hogwarts")
+    end
+
+    it "displays the school's details", :aggregate_failures do
+      expect(page).to have_content("School details")
+      expect(page).to have_content("Phase")
+      expect(page).to have_content("All-through")
+      expect(page).to have_content("Age range")
+      expect(page).to have_content("5 to 11")
+      expect(page).to have_content("Establishment group")
+      expect(page).to have_content("Local authority maintained schools")
+    end
+
+    it "displays placement information", :aggregate_failures do
+      expect(page).to have_content("Placement information")
+      expect(page).to have_content("Placement subjects")
+      expect(page).to have_content("Not open to hosting placements")
+      expect(page).to have_content("Placement status")
+      expect(page).to have_content("This school has not previously hosted placements")
+    end
+
+    it "displays a message indicating the school doesn't want to be contacted" do
+      expect(page).to have_content("This school does not wish to be contacted this academic year.")
+    end
+
+    context "when location coordinates have been provided" do
+      subject(:component) do
+        described_class.new(school:, provider:, location_coordinates: "41.40338, 2.17403")
+      end
+
+      it "does not display the getting there section" do
+        expect(page).not_to have_content("Getting there")
+      end
+    end
+  end
+end
