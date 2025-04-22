5276ea301af858a7bdea51e00030a245e4a9bae5
Jamie <jamie.cleare@education.gov.uk>
2025-04-22 14:45:23 +0100

    Add mixed year groups option to the placements wizards

--- app/wizards/placements/add_placement_wizard/year_group_step.rb
+++ app/wizards/placements/add_placement_wizard/year_group_step.rb
@@ -4,6 +4,18 @@ class Placements::AddPlacementWizard::YearGroupStep < BaseStep
   validates :year_group, presence: true
 
   def year_groups_for_selection
-    Placement.year_groups_as_options
+    year_groups_as_options
+      .reject { |option| option.value == "mixed_year_groups" }
+  end
+
+  def mixed_year_group_option
+    @mixed_year_group_option ||= year_groups_as_options
+        .find { |option| option.value == "mixed_year_groups" }
+  end
+
+  private
+
+  def year_groups_as_options
+    @year_groups_as_options ||= Placement.year_groups_as_options
   end
 end
--- spec/wizards/placements/add_placement_wizard/year_group_step_spec.rb
+++ spec/wizards/placements/add_placement_wizard/year_group_step_spec.rb
@@ -15,8 +15,33 @@ RSpec.describe Placements::AddPlacementWizard::YearGroupStep, type: :model do
   end
 
   describe "#year_groups_for_selection" do
-    subject { step.year_groups_for_selection }
+    subject(:year_groups_for_selection) { step.year_groups_for_selection }
 
-    it { is_expected.to eq(Placement.year_groups_as_options) }
+    it "returns objects for all year groups, except 'mixed year groups'" do
+      expect(year_groups_for_selection).to eq(
+        [
+          OpenStruct.new(value: "nursery", name: "Nursery", description: "3 to 4 years"),
+          OpenStruct.new(value: "reception", name: "Reception", description: "4 to 5 years"),
+          OpenStruct.new(value: "year_1", name: "Year 1", description: "5 to 6 years"),
+          OpenStruct.new(value: "year_2", name: "Year 2", description: "6 to 7 years"),
+          OpenStruct.new(value: "year_3", name: "Year 3", description: "7 to 8 years"),
+          OpenStruct.new(value: "year_4", name: "Year 4", description: "8 to 9 years"),
+          OpenStruct.new(value: "year_5", name: "Year 5", description: "9 to 10 years"),
+          OpenStruct.new(value: "year_6", name: "Year 6", description: "10 to 11 years"),
+        ],
+      )
+    end
+  end
+
+  describe "#mixed_year_group_option" do
+    subject(:mixed_year_group_option) { step.mixed_year_group_option }
+
+    it "returns an object for the mixed year groups" do
+      expect(mixed_year_group_option).to eq(OpenStruct.new(
+                                              value: "mixed_year_groups",
+                                              name: "Mixed year groups",
+                                              description: "",
+                                            ))
+    end
   end
 end
