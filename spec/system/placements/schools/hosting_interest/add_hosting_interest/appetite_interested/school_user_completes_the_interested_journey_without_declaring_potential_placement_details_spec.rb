cceb87056e905580f6a3e42e08d78a529fc0335e
Jamie <jamie.cleare@education.gov.uk>
2025-05-01 10:15:05 +0100

    All pre-existing tests passing

--- spec/system/placements/schools/hosting_interest/add_hosting_interest/appetite_interested/school_user_completes_the_interested_journey_without_declaring_potential_placement_details_spec.rb
+++ spec/system/placements/schools/hosting_interest/add_hosting_interest/appetite_interested/school_user_completes_the_interested_journey_without_declaring_potential_placement_details_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "School user completes the interested journey",
+RSpec.describe "School user completes the interested journey without declaring potential placement details",
                service: :placements,
                type: :system do
   scenario do
@@ -12,17 +12,28 @@ RSpec.describe "School user completes the interested journey",
 
     when_i_select_interested_in_hosting_placements
     and_i_click_on_continue
+    then_i_see_the_phase_known_page
+
+    when_i_select_i_dont_know
+    and_i_click_on_continue
+    then_i_see_the_provider_note_page
+
+    when_i_click_on_continue
     then_i_see_the_school_contact_form
 
     when_i_fill_in_the_school_contact_details
     and_i_click_on_continue
     then_i_see_the_confirmation_page
+    and_i_see_the_entered_school_contact_details
+    and_i_see_the_education_phase_is_not_known
+    and_i_see_the_message_to_provider_is_empty
 
-    when_i_click_on_continue
+    when_i_click_on_confirm
     then_i_see_my_responses_with_successfully_updated
     and_i_see_the_whats_next_page
     and_the_schools_contact_has_been_updated
     and_the_schools_hosting_interest_for_the_next_year_is_updated
+    and_the_schools_potential_placement_details_have_been_updated
   end
 
   private
@@ -38,24 +49,6 @@ RSpec.describe "School user completes the interested journey",
     @school = create(:placements_school, with_school_contact: false)
     sign_in_placements_user(organisations: [@school])
   end
-
-  def when_i_am_on_the_placements_index_page
-    expect(page).to have_title("Placements - Manage school placements - GOV.UK")
-    expect(primary_navigation).to have_current_item("Placements")
-    expect(page).to have_h1("Placements")
-    expect(secondary_navigation).to have_current_item("This year (#{@current_academic_year_name}")
-    expect(page).to have_link("Bulk add placements")
-  end
-
-  alias_method :then_i_am_on_the_placements_index_page,
-               :when_i_am_on_the_placements_index_page
-
-  def when_i_click_on_bulk_add_placements
-    click_on "Bulk add placements"
-  end
-
-  alias_method :and_i_click_on_bulk_add_placements,
-               :when_i_click_on_bulk_add_placements
 
   def when_i_visit_the_add_hosting_interest_page
     visit new_add_hosting_interest_placements_school_hosting_interests_path(@school)
@@ -87,37 +80,23 @@ RSpec.describe "School user completes the interested journey",
   alias_method :and_i_click_on_continue,
                :when_i_click_on_continue
 
-  def then_i_see_the_help_available_to_you_page
-    expect(page).to have_title(
-      "Help available to you - Manage school placements - GOV.UK",
-    )
-    expect(primary_navigation).to have_current_item("Placements")
-    expect(page).to have_h1("Help available to you")
-  end
-
-  def then_i_see_the_are_you_interested_in_listing_placements_form
-    expect(page).to have_title(
-      "Would you like to list some placements to be seen by providers? - Manage school placements - GOV.UK",
-    )
-    expect(primary_navigation).to have_current_item("Placements")
-    expect(page).to have_element(
-      :h1,
-      text: "Would you like to list some placements to be seen by providers?",
-      class: "govuk-fieldset__heading",
-    )
-  end
-
   def then_i_see_the_confirmation_page
     expect(page).to have_title(
-      "Confirm you’re interested in hosting - Manage school placements - GOV.UK",
+      "Confirm and share your approximate information with providers - Manage school placements - GOV.UK",
     )
     expect(primary_navigation).to have_current_item("Placements")
-    expect(page).to have_h1("Confirm you’re interested in hosting", class: "govuk-heading-l")
-    expect(page).to have_element(:span, text: "Expression of interest", class: "govuk-caption-l")
-  end
-
-  def when_i_select_no
-    choose "No"
+    expect(page).to have_h1("Confirm and share your approximate information with providers", class: "govuk-heading-l")
+    expect(page).to have_element(:span, text: "Potential placement details", class: "govuk-caption-l")
+    expect(page).to have_element(
+      :p,
+      text: "Providers will be able to see that you may be able to offer placements for trainee teachers.",
+      class: "govuk-body",
+    )
+    expect(page).to have_element(
+      :p,
+      text: "They will be able to see your approximate information and will be able to use your email to contact you.",
+      class: "govuk-body",
+    )
   end
 
   def then_i_see_the_school_contact_form
@@ -164,7 +143,83 @@ RSpec.describe "School user completes the interested journey",
   end
 
   def and_i_see_the_whats_next_page
+    expect(page).to have_panel(
+      "Approximate information added",
+      "Providers can see that you may offer placements",
+    )
     expect(page).to have_h1("What happens next?", class: "govuk-heading-l")
-    expect(page).to have_h1("Addtional help from your school teaching hub", class: "govuk-heading-l")
+    expect(page).to have_element(
+      :p,
+      text: "Providers who are looking for schools to work with can contact you on joe_bloggs@example.com.",
+      class: "govuk-body",
+    )
+    expect(page).to have_element(
+      :p,
+      text: "Once you know which placements you can offer, you can add placements to help providers know what trainees you need.",
+      class: "govuk-body",
+    )
+    expect(page).to have_link("add placements", href: placements_school_placements_path(@school))
+  end
+
+  def then_i_see_the_phase_known_page
+    expect(page).to have_title(
+      "Do you know what phase of education your placements will be? - Manage school placements - GOV.UK",
+    )
+    expect(page).to have_element(
+      :legend,
+      text: "Do you know what phase of education your placements will be?",
+      class: "govuk-fieldset__legend",
+    )
+    expect(page).to have_field("Primary", type: :checkbox)
+    expect(page).to have_field("Secondary", type: :checkbox)
+    expect(page).to have_field("I don’t know", type: :checkbox)
+  end
+
+  def when_i_select_i_dont_know
+    check "I don’t know"
+  end
+
+  def then_i_see_the_provider_note_page
+    expect(page).to have_title(
+      "Is there anything about your school you would like providers to know? (optional) - Manage school placements - GOV.UK",
+    )
+    expect(page).to have_element(
+      :label,
+      text: "Is there anything about your school you would like providers to know? (optional)",
+    )
+    expect(page).to have_element(
+      :span,
+      text: "Expression of interest",
+      class: "govuk-caption-l",
+    )
+  end
+
+  def and_i_see_the_entered_school_contact_details
+    expect(page).to have_h2("Your information", class: "govuk-heading-m")
+    expect(page).to have_summary_list_row("First name", "Joe")
+    expect(page).to have_summary_list_row("Last name", "Bloggs")
+    expect(page).to have_summary_list_row("Email address", "joe_bloggs@example.com")
+  end
+
+  def and_i_see_the_education_phase_is_not_known
+    expect(page).to have_h2("Education phase", class: "govuk-heading-m")
+    expect(page).to have_summary_list_row("Phase", "I don’t know")
+  end
+
+  def and_i_see_the_message_to_provider_is_empty
+    expect(page).to have_h2("Additional information", class: "govuk-heading-m")
+    expect(page).to have_summary_list_row("Message to providers", "")
+  end
+
+  def when_i_click_on_confirm
+    click_on "Confirm"
+  end
+
+  def and_the_schools_potential_placement_details_have_been_updated
+    potential_placement_details = @school.reload.potential_placement_details
+    expect(potential_placement_details["phase"]).to eq({ "phases" => %w[unknown] })
+    expect(potential_placement_details["note_to_providers"]).to eq(
+      { "note" => "" },
+    )
   end
 end
