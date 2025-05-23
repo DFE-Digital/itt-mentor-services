e2674bf6dfaa4d55a1092018f8dd4a294cb140ec
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-05-23 10:22:09 +0100

    Update status tag component to differentiate between providers and schools

--- app/components/placement/status_tag_component.rb
+++ app/components/placement/status_tag_component.rb
@@ -1,19 +1,44 @@
 class Placement::StatusTagComponent < ApplicationComponent
-  def initialize(placement:, classes: [], html_attributes: {})
+  def initialize(placement:, viewed_by_organisation:, classes: [], html_attributes: {})
     super(classes:, html_attributes:)
 
     @placement = placement
+    @viewed_by_organisation = viewed_by_organisation
   end
 
   private
 
-  attr_reader :placement
+  attr_reader :placement, :viewed_by_organisation
+
+  def status
+    if assigned_to_viewed_by_organisation?
+      { colour: "blue", text: I18n.t("placements.schools.placements.assigned_to_you") }
+    elsif placement_has_provider? && viewed_by_organisation_type == :school
+      { colour: "blue", text: I18n.t("placements.schools.placements.assigned_to_provider") }
+    elsif placement_has_provider?
+      { colour: "red", text: I18n.t("placements.schools.placements.unavailable") }
+    else
+      { colour: "green", text: I18n.t("placements.schools.placements.available") }
+    end
+  end
 
   def status_colour
-    placement.provider.present? ? "orange" : "turquoise"
+    status[:colour]
   end
 
   def status_text
-    placement.provider.present? ? I18n.t("placements.schools.placements.unavailable") : I18n.t("placements.schools.placements.available")
+    status[:text]
+  end
+
+  def assigned_to_viewed_by_organisation?
+    placement_has_provider? && placement.provider.id == viewed_by_organisation.id
+  end
+
+  def placement_has_provider?
+    placement.provider.present?
+  end
+
+  def viewed_by_organisation_type
+    @viewed_by_organisation_type ||= viewed_by_organisation.is_a?(Placements::School) ? :school : :provider
   end
 end
--- spec/components/placement/status_tag_component_spec.rb
+++ spec/components/placement/status_tag_component_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Placement::StatusTagComponent, type: :component do
   subject(:component) do
-    described_class.new(placement:)
+    described_class.new(placement:, viewed_by_organisation:)
   end
 
   let(:placement) { create(:placement) }
@@ -11,33 +11,87 @@ RSpec.describe Placement::StatusTagComponent, type: :component do
     placement
   end
 
-  context "when the placement does not have a provider" do
-    it "renders the status as available" do
-      render_inline(component)
+  context "when viewed by a school" do
+    let(:viewed_by_organisation) { create(:placements_school) }
 
-      expect(page).to have_content("Available")
+    context "when the placement does not have a provider" do
+      it "renders the status as available" do
+        render_inline(component)
+
+        expect(page).to have_content("Available")
+      end
+
+      it "renders the status as green" do
+        render_inline(component)
+
+        expect(page).to have_css(".govuk-tag--green")
+      end
     end
 
-    it "renders the status as turquoise" do
-      render_inline(component)
+    context "when the placement has a provider" do
+      let(:placement) { create(:placement, provider: create(:provider)) }
 
-      expect(page).to have_css(".govuk-tag--turquoise")
+      it "renders the status as assigned to provider" do
+        render_inline(component)
+
+        expect(page).to have_content("Assigned to provider")
+      end
+
+      it "renders the status as blue" do
+        render_inline(component)
+
+        expect(page).to have_css(".govuk-tag--blue")
+      end
     end
   end
 
-  context "when the placement has a provider" do
-    let(:placement) { create(:placement, provider: create(:provider)) }
+  context "when viewed by a provider" do
+    let(:viewed_by_organisation) { create(:placements_provider) }
 
-    it "renders the status as unavailable" do
-      render_inline(component)
+    context "when the placement does not have a provider" do
+      it "renders the status as available" do
+        render_inline(component)
 
-      expect(page).to have_content("Unavailable")
+        expect(page).to have_content("Available")
+      end
+
+      it "renders the status as green" do
+        render_inline(component)
+
+        expect(page).to have_css(".govuk-tag--green")
+      end
     end
 
-    it "renders the status as orange" do
-      render_inline(component)
+    context "when the placement has a provider" do
+      let(:placement) { create(:placement, provider: create(:placements_provider)) }
 
-      expect(page).to have_css(".govuk-tag--orange")
+      it "renders the status as unavailable" do
+        render_inline(component)
+
+        expect(page).to have_content("Unavailable")
+      end
+
+      it "renders the status as red" do
+        render_inline(component)
+
+        expect(page).to have_css(".govuk-tag--red")
+      end
+    end
+
+    context "when the placement is assigned to the organisation" do
+      let(:placement) { create(:placement, provider: viewed_by_organisation) }
+
+      it "renders the status as assigned to you" do
+        render_inline(component)
+
+        expect(page).to have_content("Assigned to you")
+      end
+
+      it "renders the status as blue" do
+        render_inline(component)
+
+        expect(page).to have_css(".govuk-tag--blue")
+      end
     end
   end
 end
