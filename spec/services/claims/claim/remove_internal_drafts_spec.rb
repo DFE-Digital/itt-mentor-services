2fdd5a96f4ddb642902e3bcf6e2c4d86eb1803f4
Richard Pattinson <richie.pattinson@googlemail.com>
2025-05-29 16:01:17 +0100

    Fix flakey remove internal drafts spec Change equality matcher to match array. This prevents random ordering causing a failed spec

--- spec/services/claims/claim/remove_internal_drafts_spec.rb
+++ spec/services/claims/claim/remove_internal_drafts_spec.rb
@@ -12,11 +12,8 @@ describe Claims::Claim::RemoveInternalDrafts do
   describe "#call" do
     it "removes internal draft claims that haven't changed in at least 24 hours" do
       create(:claim, status: :internal_draft, updated_at: 25.hours.ago)
-
       expect { service }.to change(Claims::Claim, :count).from(4).to(3)
-      expect(Claims::Claim.all).to eq(
-        [draft_claim, active_internal_draft_claim, submitted_claim],
-      )
+      expect(Claims::Claim.all).to contain_exactly(draft_claim, active_internal_draft_claim, submitted_claim)
     end
   end
 end
