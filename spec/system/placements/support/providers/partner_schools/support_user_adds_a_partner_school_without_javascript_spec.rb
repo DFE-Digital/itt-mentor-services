12e3070e72e13be634d2b7f87d90b0fa2a80b9fa
Richard Pattinson <richie.pattinson@googlemail.com>
2025-06-11 17:42:54 +0100

    Refactor support user adds a partner school without javascript

--- spec/system/placements/support/providers/partner_schools/support_user_adds_a_partner_school_without_javascript_spec.rb
+++ spec/system/placements/support/providers/partner_schools/support_user_adds_a_partner_school_without_javascript_spec.rb
@@ -1,177 +1,234 @@
 require "rails_helper"
 
-RSpec.describe "Placements / Support / Providers / Partner schools / Support user adds a partner school without JavaScript",
+RSpec.describe "Support user adds a partner school without JavaScript",
                service: :placements, type: :system do
   include ActiveJob::TestHelper
 
-  around do |example|
-    perform_enqueued_jobs { example.run }
-  end
-
-  let!(:school) { create(:school, :placements, name: "Manchester 1") }
-  let!(:provider) { create(:placements_provider) }
-
-  before do
-    create(:school, name: "Manchester 2")
-    create(:school, :placements, name: "London")
-    create(:school, :claims, name: "Claims")
-
+  scenario do
+    given_two_schools_exist
+    and_a_provider_exists
     given_i_am_signed_in_as_a_placements_support_user
-  end
 
-  scenario "Support user adds a partner school" do
-    when_i_visit_the_add_partner_school_page_for(provider)
-    and_i_enter_a_school_named("Manch")
-    and_i_click_on("Continue")
-    then_i_see_list_of_placements_schools
-    when_i_choose("Manchester 1")
-    and_i_click_on("Continue")
-    then_i_see_the_check_details_page_for_school("Manchester 1")
-    and_i_click_on("Confirm and add school")
-    then_i_return_to_partner_school_index_for(provider)
-    and_a_school_is_listed(school_name: "Manchester 1")
-    and_i_see_success_message
-  end
+    when_i_click_on_westbrook_provider
+    then_i_see_the_find_page
 
-  scenario "Support user adds a partner school which already exists" do
-    given_a_partnership_exists_between(school, provider)
-    when_i_visit_the_add_partner_school_page_for(provider)
-    and_i_enter_a_school_named("Manch")
-    and_i_click_on("Continue")
-    then_i_see_list_of_placements_schools
-    when_i_choose("Manchester 1")
-    and_i_click_on("Continue")
-    then_i_see_an_error("Manchester 1 has already been added. Try another school")
-  end
+    when_i_navigate_to_the_provider_schools_page
+    then_i_see_the_provider_schools_index_with_no_schools
 
-  scenario "Support user submits the search form without selecting a school" do
-    when_i_visit_the_add_partner_school_page_for(provider)
-    and_i_click_on("Continue")
-    then_i_see_an_error("Enter a school name, unique reference number (URN) or postcode")
-  end
+    when_i_click_on_add_school
+    then_i_see_the_add_school_page
 
-  scenario "Support user submits the options form without selecting a school" do
-    when_i_visit_the_add_partner_school_page_for(provider)
-    and_i_enter_a_school_named("Manch")
-    and_i_click_on("Continue")
-    then_i_see_list_of_placements_schools
-    and_i_click_on("Continue")
-    then_i_see_an_error("Select a school")
-  end
+    when_i_click_on_continue
+    then_i_see_an_error_that_i_must_enter_a_name_urn_or_postcode
 
-  scenario "Support user reconsiders selecting a school" do
-    when_i_visit_the_add_partner_school_page_for(provider)
-    and_i_enter_a_school_named("Manch")
-    and_i_click_on("Continue")
-    then_i_see_list_of_placements_schools
-    when_i_choose("Manchester 1")
-    and_i_click_on("Continue")
-    then_i_see_the_check_details_page_for_school("Manchester 1")
-    when_i_click_on("Back")
-    then_i_see_list_of_placements_schools
-    and_the_option_for_school_has_been_pre_selected("Manchester 1")
-    when_i_click_on("Back")
-    then_i_see_the_search_input_pre_filled_with("Manch")
-    and_i_click_on("Continue")
-    then_i_see_list_of_placements_schools
-    when_i_choose("Manchester 1")
-    and_i_click_on("Continue")
-    then_i_see_the_check_details_page_for_school("Manchester 1")
-  end
+    when_i_click_on_back
+    then_i_see_the_provider_schools_index_with_no_schools
 
-  scenario "Support user adds a partner school, which is not onboarded on the placements service" do
-    given_the_school_is_not_onboarded_on_placements_service(school)
-    when_i_visit_the_add_partner_school_page_for(provider)
-    and_i_enter_a_school_named("Manch")
-    and_i_click_on("Continue")
-    then_i_see_list_of_placements_schools
-    when_i_choose("Manchester 1")
-    and_i_click_on("Continue")
-    then_i_see_the_check_details_page_for_school("Manchester 1")
-    and_i_click_on("Confirm and add school")
-    then_i_return_to_partner_school_index_for(provider)
-    and_a_school_is_listed(school_name: "Manchester 1")
-    and_i_see_success_message
+    when_i_click_on_add_school
+    then_i_see_the_add_school_page
+
+    when_i_type_in_the_incomplete_name_shelb
+    and_i_click_on_continue
+    then_i_see_the_multiple_school_results_page
+
+    when_i_choose_shelbyville_elementary
+    and_i_click_on_continue
+    then_i_see_the_check_details_page_for_shelbyville_school
+
+    when_i_click_on_back
+    then_i_see_the_multiple_school_results_page_with_shelbyville_elementary_selected
+
+    when_i_click_on_continue
+    then_i_see_the_check_details_page_for_shelbyville_school
+
+    when_i_click_on_change_name
+    then_i_see_the_add_school_page_with_shelb_prepopulated
+
+    when_i_click_on_continue
+    then_i_see_the_multiple_school_results_page
+
+    when_i_choose_shelbyville_elementary
+    and_i_click_on_continue
+    then_i_see_the_check_details_page_for_shelbyville_school
+
+    when_i_click_on_confirm_and_add_school
+    then_i_return_to_the_partner_school_index_page_with_a_new_partner_school_and_a_success_banner
+
+    when_i_click_on_add_school
+    then_i_see_the_add_school_page
+
+    when_i_type_in_the_incomplete_name_shelb
+    and_i_click_on_continue
+    then_i_see_the_multiple_school_results_page
+
+    when_i_choose_shelbyville_elementary
+    and_i_click_on_continue
+    then_i_see_an_error_that_shelbyville_elementary_has_already_been_added
   end
 
   private
 
-  def when_i_visit_the_add_partner_school_page_for(provider)
-    visit new_add_partner_school_placements_provider_partner_schools_path(provider)
-
-    then_i_see_support_navigation_with_organisation_selected
+  def given_two_schools_exist
+    @user_anne = build(:placements_user, first_name: "Anne", last_name: "Wilson", email: "anne_wilson@education.gov.uk")
+    @shelbyville_school = create(
+      :placements_school,
+      name: "Shelbyville Elementary",
+      urn: "12345",
+      email_address: "shelbyville_elementary@sample.com",
+      ukprn: "54321",
+      address1: "44 Langton Way",
+      website: "www.shelbyville_elementary.com",
+      telephone: "02083334444",
+    )
+    @shelberg_high_school = create(
+      :placements_school,
+      name: "Shelberg High School",
+    )
   end
 
-  def then_i_see_support_navigation_with_organisation_selected
-    within(".govuk-header__navigation-list") do
-      expect(page).to have_link "Organisations"
-      expect(page).to have_link "Support users"
+  def and_a_provider_exists
+    @provider = create(:placements_provider, name: "Westbrook Provider", users: [@user_anne])
+  end
+
+  def when_i_click_on_westbrook_provider
+    click_on "Westbrook Provider"
+  end
+
+  def then_i_see_the_find_page
+    expect(page).to have_title("Find schools hosting placements - Manage school placements - GOV.UK")
+    expect(page).to have_element(:span, text: "Westbrook Provider", class: "govuk-heading-s")
+    expect(primary_navigation).to have_current_item("Find")
+    expect(page).to have_h1("Find schools hosting placements")
+  end
+
+  def when_i_navigate_to_the_provider_schools_page
+    within(primary_navigation) do
+      click_on "Schools"
     end
   end
 
-  def when_i_click_on(text)
-    click_on text
-  end
-  alias_method :and_i_click_on, :when_i_click_on
-
-  def and_i_enter_a_school_named(school_name)
-    fill_in "Add a school", with: school_name
-  end
-
-  def then_i_see_list_of_placements_schools
-    expect(page).to have_content("Manchester 1")
-    expect(page).to have_content("Manchester 2")
-    expect(page).not_to have_content("London")
-    expect(page).not_to have_content("Claims")
+  def then_i_see_the_provider_schools_index_with_no_schools
+    expect(page).to have_title("Schools you work with - Manage school placements - GOV.UK")
+    expect(primary_navigation).to have_current_item("Schools")
+    expect(page).to have_h1("Schools you work with")
+    expect(page).to have_element(:p, text: "View all placements your schools have published.")
+    expect(page).to have_element(:p, text: "Only schools you work with are able to assign you their placements.")
+    expect(page).to have_link("Add school", href: new_add_partner_school_placements_provider_partner_schools_path(@provider))
+    expect(page).to have_element(:p, text: "There are no partner schools for Westbrook Provider")
   end
 
-  def then_i_choose(selection_name)
-    choose selection_name
-  end
-  alias_method :when_i_choose, :then_i_choose
-
-  def then_i_see_the_check_details_page_for_school(school_name)
-    expect(page).to have_content("Confirm school details")
-    org_name_row = page.all(".govuk-summary-list__row")[0]
-    expect(org_name_row).to have_content(school_name)
+  def when_i_click_on_add_school
+    click_on "Add school"
   end
 
-  def then_i_return_to_partner_school_index_for(_provider)
-    expect(page.find(".govuk-heading-l")).to have_content("Schools you work with")
+  def then_i_see_the_add_school_page
+    expect(page).to have_title("Add a school - School details - Manage school placements - GOV.UK")
+    expect(primary_navigation).to have_current_item("Schools")
+    expect(page).to have_span_caption("School details")
+    expect(page).to have_element(:span, class: "govuk-caption-l", text: "School details")
+    expect(page).to have_hint("Enter a school name, unique reference number (URN) or postcode")
+    expect(page).to have_button("Continue")
+    expect(page).to have_link("Cancel")
   end
 
-  def and_a_school_is_listed(school_name:)
-    expect(page).to have_content(school_name)
+  def when_i_click_on_back
+    click_on "Back"
   end
 
-  def and_i_see_success_message
-    expect(page).to have_content "School added"
+  def when_i_click_on_continue
+    click_on "Continue"
+  end
+  alias_method :and_i_click_on_continue, :when_i_click_on_continue
+
+  def then_i_see_an_error_that_i_must_enter_a_name_urn_or_postcode
+    expect(page).to have_validation_error("Enter a school name, unique reference number (URN) or postcode")
   end
 
-  def given_a_partnership_exists_between(school, provider)
-    create(:placements_partnership, school:, provider:)
-  end
-  alias_method :and_a_partnership_exists_between, :given_a_partnership_exists_between
-
-  def then_i_see_an_error(error_message)
-    # Error summary
-    expect(page.find(".govuk-error-summary")).to have_content(
-      "There is a problem",
-    )
-    expect(page.find(".govuk-error-summary")).to have_content(error_message)
-    # Error above input
-    expect(page.find(".govuk-error-message")).to have_content(error_message)
+  def when_i_type_in_the_incomplete_name_shelb
+    fill_in "Add a school", with: "Shelb"
   end
 
-  def then_i_see_the_search_input_pre_filled_with(school_name)
-    find_field "Add a school", with: school_name
+  def then_i_see_the_multiple_school_results_page
+    expect(page).to have_title("2 results found for ‘Shelb’ - School details - Manage school placements - GOV.UK")
+    expect(primary_navigation).to have_current_item("Schools")
+    expect(page).to have_span_caption("School details")
+    expect(page).to have_link("Change your search")
+    expect(page).to have_hint("Change your search if the school you’re looking for is not listed.")
+    expect(page).to have_field("Shelbyville Elementary", type: :radio)
+    expect(page).to have_field("Shelberg High School", type: :radio)
+    expect(page).to have_button("Continue")
+    expect(page).to have_link("Cancel")
   end
 
-  def given_the_school_is_not_onboarded_on_placements_service(school)
-    school.update!(placements_service: false)
+  def when_i_choose_shelbyville_elementary
+    choose "Shelbyville Elementary"
   end
 
-  def and_the_option_for_school_has_been_pre_selected(school_name)
-    expect(page).to have_checked_field(school_name)
+  def then_i_see_the_multiple_school_results_page_with_shelbyville_elementary_selected
+    expect(page).to have_title("2 results found for ‘Shelb’ - School details - Manage school placements - GOV.UK")
+    expect(primary_navigation).to have_current_item("Schools")
+    expect(page).to have_span_caption("School details")
+    expect(page).to have_link("Change your search")
+    expect(page).to have_hint("Change your search if the school you’re looking for is not listed.")
+    expect(page).to have_checked_field("Shelbyville Elementary", type: :radio)
+    expect(page).to have_field("Shelberg High School", type: :radio)
+    expect(page).to have_button("Continue")
+    expect(page).to have_link("Cancel")
+  end
+
+  def then_i_see_the_add_school_page_with_shelb_prepopulated
+    expect(page).to have_title("Add a school - School details - Manage school placements - GOV.UK")
+    expect(primary_navigation).to have_current_item("Schools")
+    expect(page).to have_span_caption("School details")
+    expect(page).to have_element(:span, class: "govuk-caption-l", text: "School details")
+    expect(page).to have_element(:input, class: "govuk-input", value: "Shelb")
+    expect(page).to have_hint("Enter a school name, unique reference number (URN) or postcode")
+    expect(page).to have_button("Continue")
+    expect(page).to have_link("Cancel")
+  end
+
+  def then_i_see_the_check_details_page_for_shelbyville_school
+    expect(page).to have_title("Confirm school details - School details - Manage school placements - GOV.UK")
+    expect(primary_navigation).to have_current_item("Schools")
+    expect(page).to have_h1("Confirm school details")
+    expect(page).to have_element(:p, text: "Once added, they will be able to assign you to their placements.")
+    expect(page).to have_element(:p, text: "We will send them an email to let them know you have added them.")
+    expect(page).to have_summary_list_row("Name", "Shelbyville Elementary")
+    expect(page).to have_summary_list_row("UK provider reference number (UKPRN)", "54321")
+    expect(page).to have_summary_list_row("Unique reference number (URN)", "12345")
+    expect(page).to have_summary_list_row("Email address", "shelbyville_elementary@sample.com")
+    expect(page).to have_summary_list_row("Telephone number", "02083334444")
+    expect(page).to have_summary_list_row("Website", "http://www.shelbyville_elementary.com")
+    expect(page).to have_summary_list_row("Address", "44 Langton Way")
+    expect(page).to have_button("Confirm and add school")
+    expect(page).to have_link("Cancel")
+  end
+
+  def then_i_see_the_search_input_pre_filled_with_shelbyville_school
+    find_field "Add a school", with: "Shelb"
+  end
+
+  def when_i_click_on_change_name
+    click_on "Change Name"
+  end
+
+  def when_i_click_on_confirm_and_add_school
+    click_on "Confirm and add school"
+  end
+
+  def then_i_return_to_the_partner_school_index_page_with_a_new_partner_school_and_a_success_banner
+    expect(page).to have_title("Schools you work with - Manage school placements - GOV.UK")
+    expect(primary_navigation).to have_current_item("Schools")
+    expect(page).to have_h1("Schools you work with")
+    expect(page).to have_element(:p, text: "View all placements your schools have published.")
+    expect(page).to have_element(:p, text: "Only schools you work with are able to assign you their placements.")
+    expect(page).to have_link("Add school", href: new_add_partner_school_placements_provider_partner_schools_path(@provider))
+    expect(page).to have_table_row({ "Name": "Shelbyville Elementary",
+                                     "Unique reference number (URN)": "12345" })
+    expect(page).to have_success_banner("School added", "Shelbyville Elementary can now assign you to their placements.")
+  end
+
+  def then_i_see_an_error_that_shelbyville_elementary_has_already_been_added
+    expect(page).to have_validation_error("Shelbyville Elementary has already been added. Try another school")
   end
 end
