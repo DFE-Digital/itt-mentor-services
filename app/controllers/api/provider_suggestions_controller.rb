3c2e6c6f733d8a30e92e5fd7e8eda88ac1043f70
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-06-11 16:09:00 +0100

    Add placement and claims scope to provider search ahead

--- app/controllers/api/provider_suggestions_controller.rb
+++ app/controllers/api/provider_suggestions_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProviderSuggestionsController < ApplicationController
   def index
-    render json: Provider.search_name_urn_ukprn_postcode(query_params)
+    render json: providers_to_render.search_name_urn_ukprn_postcode(query_params)
                          .select(:id, :name, :postcode, :code)
                          .limit(50)
   end
@@ -9,5 +9,17 @@ class Api::ProviderSuggestionsController < ApplicationController
 
   def query_params
     params.require(:query)&.downcase
+  end
+
+  def providers_to_render
+    current_service == :claims ? claims_providers_scope : placements_providers_scope
+  end
+
+  def claims_providers_scope
+    @claims_providers_scope ||= Provider.excluding_niot_providers
+  end
+
+  def placements_providers_scope
+    @placements_providers_scope ||= Provider
   end
 end
