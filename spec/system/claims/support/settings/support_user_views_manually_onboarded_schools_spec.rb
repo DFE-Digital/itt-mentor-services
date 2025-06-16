1792899dd8c06a8f923ac0d0a9242384e4a41555
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-06-16 16:49:33 +0100

    Add page to display manually onboarded schools

---
+++ spec/system/claims/support/settings/support_user_views_manually_onboarded_schools_spec.rb
@@ -0,0 +1,63 @@
+require "rails_helper"
+
+RSpec.describe "Support user views manually onboarded schools", service: :claims, type: :system do
+  scenario do
+    given_manually_onboarded_schools_exist
+    and_i_am_signed_in
+    when_i_navigate_to_the_settings_index_page
+    then_i_see_the_manually_onboarded_schools_link
+
+    when_i_click_on_the_manually_onboarded_schools_link
+    then_i_see_the_manually_onboarded_schools_page
+  end
+
+  private
+
+  def given_manually_onboarded_schools_exist
+    @current_claim_window = create(:claim_window, :current)
+    @previous_claim_window = create(:claim_window, :historic)
+
+    @london_onboarding_user = create(:claims_support_user, first_name: "London", last_name: "Onboarder User", email: "london_onboarder@education.gov.uk")
+    @london_school = create(:claims_school, name: "London School", urn: 111_111, manually_onboarded_by: @london_onboarding_user)
+    create(:eligibility, school: @london_school, claim_window: @current_claim_window)
+    create(:eligibility, school: @london_school, claim_window: @previous_claim_window)
+
+    @guildford_onboarding_user = create(:claims_support_user, first_name: "Guildford", last_name: "Onboarder User", email: "guildford_onboarder@education.gov.uk")
+    @guildford_school = create(:claims_school, name: "Guildford School", urn: 222_222, manually_onboarded_by: @guildford_onboarding_user)
+    create(:eligibility, school: @guildford_school, claim_window: @current_claim_window)
+  end
+
+  def and_i_am_signed_in
+    sign_in_claims_support_user
+  end
+
+  def when_i_navigate_to_the_settings_index_page
+    within(primary_navigation) do
+      click_on "Settings"
+    end
+  end
+
+  def then_i_see_the_manually_onboarded_schools_link
+    expect(page).to have_link("Manually onboarded schools", href: "/support/manually_onboarded_schools")
+  end
+
+  def when_i_click_on_the_manually_onboarded_schools_link
+    click_on "Manually onboarded schools"
+  end
+
+  def then_i_see_the_manually_onboarded_schools_page
+    expect(page).to have_title("Claim funding for mentor training - GOV.UK")
+    expect(primary_navigation).to have_link("Settings", href: "/support/settings")
+    expect(page).to have_h1("Manually onboarded schools", class: "govuk-heading-l")
+    expect(page).to have_table_row({
+      "School name" => "London School",
+      "Onboarded by" => "London Onboarder User",
+      "Eligible to claim dates" => "#{I18n.l(@current_claim_window.starts_on, format: :long)} to #{I18n.l(@current_claim_window.ends_on, format: :long)} (current) #{I18n.l(@previous_claim_window.starts_on, format: :long)} to #{I18n.l(@previous_claim_window.ends_on, format: :long)}",
+    })
+    expect(page).to have_table_row({
+      "School name" => "Guildford School",
+      "Onboarded by" => "Guildford Onboarder User",
+      "Eligible to claim dates" => "#{I18n.l(@current_claim_window.starts_on, format: :long)} to #{I18n.l(@current_claim_window.ends_on, format: :long)} (current)",
+    })
+  end
+end
