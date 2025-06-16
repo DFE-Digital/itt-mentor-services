091b79e9f7f4aef5ecacd1ee9fca88b4cb4af684
Richard Pattinson <richie.pattinson@googlemail.com>
2025-06-16 10:55:40 +0100

    Add deliver now to daily round up job

--- spec/jobs/claims/slack/daily_roundup_job_spec.rb
+++ spec/jobs/claims/slack/daily_roundup_job_spec.rb
@@ -7,11 +7,14 @@ RSpec.describe Claims::Slack::DailyRoundupJob, type: :job do
     let(:yesterday_claims) { create_list(:claim, 2, claim_window:, created_at: Time.current.yesterday.change(hour: 16)) }
     let(:slack_notifier) { instance_double(Claims::ClaimSlackNotifier) }
 
+    let(:slack_message) { instance_double(SlackNotifier::Message, deliver_now: true) }
+
     before do
       claims
       yesterday_claims
+
       allow(Claims::ClaimSlackNotifier).to receive(:new).and_return(slack_notifier)
-      allow(slack_notifier).to receive(:claim_submitted_notification)
+      allow(slack_notifier).to receive(:claim_submitted_notification).and_return(slack_message)
     end
 
     it "sends the daily claims notification" do
@@ -23,6 +26,8 @@ RSpec.describe Claims::Slack::DailyRoundupJob, type: :job do
         provider_count: 0,
         total_claims_count: 5,
       )
+
+      expect(slack_message).to have_received(:deliver_now)
     end
   end
 end
