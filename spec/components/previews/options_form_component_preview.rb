c85e7135ab094455a0db66fc80f5c102ac4523cb
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-06-18 14:33:43 +0100

    Add manually onboarded by to add school wizard

--- spec/components/previews/options_form_component_preview.rb
+++ spec/components/previews/options_form_component_preview.rb
@@ -1,7 +1,8 @@
 class OptionsFormComponentPreview < ApplicationComponentPreview
   def less_than_15_schools
+    current_user = FactoryBot.build(:claims_support_user)
     schools = FactoryBot.build_list(:school, 2)
-    wizard = Claims::AddSchoolWizard.new(state: {}, params: {})
+    wizard = Claims::AddSchoolWizard.new(current_user:, state: {}, params: {})
     step = Claims::AddSchoolWizard::SchoolOptionsStep.new(
       wizard:, attributes: {},
     )
@@ -17,8 +18,9 @@ class OptionsFormComponentPreview < ApplicationComponentPreview
   end
 
   def more_than_15_schools
+    current_user = FactoryBot.build(:claims_support_user)
     schools = FactoryBot.build_list(:school, 16)
-    wizard = Claims::AddSchoolWizard.new(state: {}, params: {})
+    wizard = Claims::AddSchoolWizard.new(current_user:, state: {}, params: {})
     step = Claims::AddSchoolWizard::SchoolOptionsStep.new(
       wizard:, attributes: {},
     )
