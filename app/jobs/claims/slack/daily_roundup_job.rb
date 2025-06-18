d99cac274bab126921caeaa3f336380aee0c43ee
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-06-18 13:16:39 +0100

    Add daily and total amount claimed to Slack claims roundup

--- app/jobs/claims/slack/daily_roundup_job.rb
+++ app/jobs/claims/slack/daily_roundup_job.rb
@@ -1,18 +1,21 @@
 class Claims::Slack::DailyRoundupJob < ApplicationJob
   queue_as :default
+  include MoneyRails::ActionViewExtension
 
   def perform
     current_claim_window = Claims::ClaimWindow.current
     todays_claims = claims_since(Time.current.yesterday.change(hour: 16))
     new_schools = new_organisations(todays_claims, :school_id)
     new_providers = new_organisations(todays_claims, :provider_id)
-    total_claims = Claims::Claim.where(claim_window: current_claim_window).count
+    total_claims = Claims::Claim.where(claim_window: current_claim_window)
 
     Claims::ClaimSlackNotifier.claim_submitted_notification(
       claim_count: todays_claims.count,
       school_count: new_schools.count,
       provider_count: new_providers.count,
-      total_claims_count: total_claims,
+      claim_amount: humanized_money_with_symbol(todays_claims.map(&:amount).sum),
+      total_claims_count: total_claims.count,
+      total_claims_amount: humanized_money_with_symbol(total_claims.map(&:amount).sum),
     ).deliver_now
   end
 
