a488511e88f4f572b8046f93e977afe4283e7ca4
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-06-27 12:58:05 +0100

    Update filter form component to utilise async search

--- app/components/claims/claim/filter_form_component.rb
+++ app/components/claims/claim/filter_form_component.rb
@@ -3,8 +3,8 @@ class Claims::Claim::FilterFormComponent < ApplicationComponent
     filter_form:,
     statuses: Claims::Claim.statuses.values.without(*Claims::Claim::DRAFT_STATUSES.map(&:to_s)),
     academic_years: AcademicYear.where(id: Claims::ClaimWindow.select(:academic_year_id)).order_by_date,
-    providers: Claims::Provider.all,
-    schools: Claims::School.all,
+    providers: nil,
+    schools: nil,
     classes: [],
     html_attributes: {}
   )
@@ -13,11 +13,23 @@ class Claims::Claim::FilterFormComponent < ApplicationComponent
     @filter_form = filter_form
     @statuses = statuses
     @academic_years = academic_years
-    @providers = providers
-    @schools = schools
+    @providers = providers || limit_records(Claims::Provider)
+    @schools = schools || limit_records(Claims::School)
   end
 
   private
 
   attr_reader :claims, :filter_form
+
+  def limit_records(klass)
+    ids = Array(filter_form.public_send("#{klass.name.demodulize.underscore}_ids"))
+    scope = klass.limit(25)
+
+    if ids.present? && ids.count < 25
+      extra_ids = klass.where.not(id: ids).limit(25 - ids.count).ids
+      scope = klass.where(id: ids + extra_ids)
+    end
+
+    scope.order(:name)
+  end
 end
