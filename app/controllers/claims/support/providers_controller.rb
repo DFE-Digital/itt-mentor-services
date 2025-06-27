f009e5367047005677f15bfafef9e28859cf61ae
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-06-27 12:48:19 +0100

    Add provider controller and policy for search

---
+++ app/controllers/claims/support/providers_controller.rb
@@ -0,0 +1,14 @@
+class Claims::Support::ProvidersController < Claims::Support::ApplicationController
+  before_action :authorize_provider
+  def search
+    limit = params[:limit].to_i.clamp(25, 100)
+    providers = Claims::Provider.where("name ILIKE ?", "%#{params[:q]}%") if params[:q].presence
+    providers ||= Claims::Provider
+
+    render json: providers.limit(limit).as_json(only: [:id, :name])
+  end
+
+  def authorize_provider
+    authorize Claims::Provider
+  end
+end
