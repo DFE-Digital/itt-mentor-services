d2f264664555667857c24ca1c9632ea398ffebc5
paul senior <paul.senior@education.gov.uk>
2025-07-15 16:20:17 +0100

    feat(2381): testing pulling non-service images from ghcr.io

--- global_config/dv_review.sh
+++ global_config/dv_review.sh
@@ -4,3 +4,4 @@ AZURE_SUBSCRIPTION=s189-teacher-services-cloud-development
 AZURE_RESOURCE_PREFIX=s189d01
 KV_PURGE_PROTECTION=false
 TERRAFORM_MODULES_TAG=2381-aks-spike-image-pull-using-ghcr
+ENVIRONMENT=dv_review
