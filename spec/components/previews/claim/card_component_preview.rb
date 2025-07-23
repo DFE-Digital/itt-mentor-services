01a5e3ce0c91ac62ae8be6350e50581ae5e51516
Jamie <jamiecleare2525@hotmail.com>
2025-07-23 15:36:32 +0100

    Fix component preview

--- spec/components/previews/claim/card_component_preview.rb
+++ spec/components/previews/claim/card_component_preview.rb
@@ -1,7 +1,8 @@
 class Claim::CardComponentPreview < ApplicationComponentPreview
   def default
     claim = FactoryBot.build_stubbed(:claim, :submitted)
+    current_user = FactoryBot.build_stubbed(:claims_support_user)
 
-    render Claim::CardComponent.new(claim:)
+    render Claim::CardComponent.new(claim:, href: "", current_user:)
   end
 end
