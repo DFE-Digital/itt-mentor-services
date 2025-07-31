c46c5421c59abbf7c36743ebd63a3b8ebe1f9750
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-31 14:00:43 +0100

    Update notify rate limiter to accept args and kwargs

--- app/jobs/notify_rate_limiter_job.rb
+++ app/jobs/notify_rate_limiter_job.rb
@@ -1,9 +1,9 @@
 class NotifyRateLimiterJob < ApplicationJob
   queue_as :default
 
-  def perform(wait_time, batch, mailer:, mailer_method:)
-    batch.each do |record|
-      mailer.public_send(mailer_method, record).deliver_later(wait: wait_time)
+  def perform(wait_time, batch, mailer_class, mailer_method, *mailer_args, **mailer_kwargs)
+    batch.each do |item|
+      mailer_class.constantize.public_send(mailer_method, item, *mailer_args, **mailer_kwargs).deliver_later(wait: wait_time)
     end
   end
 end
--- spec/jobs/notify_rate_limiter_job_spec.rb
+++ spec/jobs/notify_rate_limiter_job_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe NotifyRateLimiterJob, type: :job do
   describe "#perform" do
     let(:wait_time) { 1.minute }
     let(:batch) { create_list(:claims_user, 5) }
-    let(:mailer) { Claims::UserMailer }
+    let(:mailer) { "Claims::UserMailer" }
     let(:mailer_method) { :claims_have_not_been_submitted }
+    let(:mailer_args) { %w[arg1] }
+    let(:mailer_kwargs) { { name: "Bob" } }
 
     before do
       create(:claim_window, :current)
@@ -16,8 +18,25 @@ RSpec.describe NotifyRateLimiterJob, type: :job do
 
     it "enqueues a mailer job for each record with wait time", :aggregate_failures do
       expect {
-        described_class.perform_now(wait_time, batch, mailer:, mailer_method:)
+        described_class.perform_now(wait_time, batch, mailer, mailer_method)
       }.to have_enqueued_job(ActionMailer::MailDeliveryJob).exactly(batch.size).times
+    end
+
+    context "when the mailer method has additional arguments" do
+      let(:batch) { create_list(:claims_user, 1) }
+
+      it "enqueues a mailer job with the correct arguments", :aggregate_failures do
+        described_class.perform_now(wait_time, batch, mailer, mailer_method, mailer_args, mailer_kwargs)
+
+        batch.each do |item|
+          expect(ActionMailer::MailDeliveryJob).to have_been_enqueued.with(
+            mailer.to_s,
+            mailer_method.to_s,
+            "deliver_now",
+            { args: [item, mailer_args, mailer_kwargs] },
+          )
+        end
+      end
     end
   end
 end
