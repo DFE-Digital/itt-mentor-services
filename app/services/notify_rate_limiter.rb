36a765939b74650cc68a89c1bb3f2562ae33bf1c
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-31 14:00:43 +0100

    Update notify rate limiter to accept args and kwargs

--- app/services/notify_rate_limiter.rb
+++ app/services/notify_rate_limiter.rb
@@ -1,17 +1,19 @@
 class NotifyRateLimiter < ApplicationService
-  attr_accessor :collection, :mailer, :mailer_method, :batch_size, :interval
+  attr_accessor :collection, :mailer, :mailer_method, :batch_size, :interval, :mailer_args, :mailer_kwargs
 
-  def initialize(collection:, mailer:, mailer_method:, batch_size: 100, interval: 1.minute)
+  def initialize(collection:, mailer:, mailer_method:, batch_size: 100, interval: 1.minute, mailer_args: [], mailer_kwargs: {})
     @collection = collection
     @mailer = mailer
     @mailer_method = mailer_method
+    @mailer_args = mailer_args
+    @mailer_kwargs = mailer_kwargs
     @batch_size = batch_size
     @interval = interval
   end
 
   def call
     collection.find_in_batches(batch_size:) do |batch|
-      NotifyRateLimiterJob.perform_later(wait_time, batch, mailer:, mailer_method:)
+      NotifyRateLimiterJob.perform_now(wait_time, batch, mailer, mailer_method, mailer_args, mailer_kwargs)
       @wait_time += interval
     end
   end
--- spec/services/notify_rate_limiter_spec.rb
+++ spec/services/notify_rate_limiter_spec.rb
@@ -4,12 +4,14 @@ describe NotifyRateLimiter do
   let(:collection) { Claims::User.all }
   let(:mailer) { "Claims::UserMailer" }
   let(:mailer_method) { :claims_have_not_been_submitted }
+  let(:mailer_args) { [] }
+  let(:mailer_kwargs) { {} }
 
   before do
     create_list(:claims_user, 150)
 
-    allow(NotifyRateLimiterJob).to receive(:perform_later).with(0.minutes, anything, mailer:, mailer_method:)
-    allow(NotifyRateLimiterJob).to receive(:perform_later).with(1.minute, anything, mailer:, mailer_method:)
+    allow(NotifyRateLimiterJob).to receive(:perform_later).with(0.minutes, anything, mailer, mailer_method, mailer_args, mailer_kwargs)
+    allow(NotifyRateLimiterJob).to receive(:perform_later).with(1.minute, anything, mailer, mailer_method, mailer_args, mailer_kwargs)
   end
 
   it_behaves_like "a service object" do
@@ -20,8 +22,20 @@ describe NotifyRateLimiter do
     it "calls NotifyRateLimiterJob for each batch of users" do
       described_class.call(collection:, mailer:, mailer_method:)
 
-      expect(NotifyRateLimiterJob).to have_received(:perform_later).with(0.minutes, anything, mailer:, mailer_method:)
-      expect(NotifyRateLimiterJob).to have_received(:perform_later).with(1.minute, anything, mailer:, mailer_method:)
+      expect(NotifyRateLimiterJob).to have_received(:perform_later).with(0.minutes, anything, mailer, mailer_method, mailer_args, mailer_kwargs)
+      expect(NotifyRateLimiterJob).to have_received(:perform_later).with(1.minute, anything, mailer, mailer_method, mailer_args, mailer_kwargs)
+    end
+
+    context "when mailer has additional arguments" do
+      let(:mailer_args) { %w[arg1 arg2] }
+      let(:mailer_kwargs) { { name: "Bob" } }
+
+      it "passes additional arguments to NotifyRateLimiterJob" do
+        described_class.call(collection:, mailer:, mailer_method:, mailer_args:, mailer_kwargs:)
+
+        expect(NotifyRateLimiterJob).to have_received(:perform_later).with(0.minutes, anything, mailer, mailer_method, %w[arg1 arg2], { name: "Bob" })
+        expect(NotifyRateLimiterJob).to have_received(:perform_later).with(1.minute, anything, mailer, mailer_method, %w[arg1 arg2], { name: "Bob" })
+      end
     end
   end
 end
