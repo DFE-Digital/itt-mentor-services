26f8130b5491b0b71c8b239c3765de95ae563454
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-08-29 14:53:10 +0100

    Add clawback not required service

---
+++ spec/services/claims/claim/clawback/clawback_not_required_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+describe Claims::Claim::Clawback::ClawbackNotRequired do
+  let!(:claim) { create(:claim, :submitted, status: :sampling_not_approved) }
+
+  describe "#call" do
+    subject(:call) { described_class.call(claim:, esfa_responses:) }
+
+    context "when given an esfa responses" do
+      let(:mentor_training_1) { create(:mentor_training, claim:, not_assured: true, reason_not_assured: "reason") }
+      let(:mentor_training_2) { create(:mentor_training, claim:, not_assured: true, reason_not_assured: "reason") }
+      let(:esfa_responses) do
+        [
+          { id: mentor_training_1.id, hours_clawed_back: 0, reason_for_clawback: "Some reason" },
+          { id: mentor_training_2.id, hours_clawed_back: 0, reason_for_clawback: "Another reason" },
+        ]
+      end
+
+      it "updates the mentor trainings with the given response" do
+        expect { call }.to change(claim, :status)
+          .from("sampling_not_approved")
+          .to("paid")
+
+        mentor_training_1.reload
+        mentor_training_2.reload
+
+        expect(mentor_training_1.hours_clawed_back).to be_nil
+        expect(mentor_training_1.reason_clawed_back).to be_nil
+        expect(mentor_training_2.hours_clawed_back).to be_nil
+        expect(mentor_training_2.reason_clawed_back).to be_nil
+      end
+
+      context "when an esfa response is not given for a mentor training associated with this claim" do
+        let(:esfa_responses) do
+          [
+            { id: mentor_training_1.id, hours_clawed_back: 20, reason_for_clawback: "Some reason" },
+          ]
+        end
+
+        before do
+          mentor_training_1
+          mentor_training_2
+        end
+
+        it "skips the mentor training not associated with the claim" do
+          expect { call }.not_to change(mentor_training_1, :hours_clawed_back).from(nil)
+        end
+      end
+    end
+  end
+end
