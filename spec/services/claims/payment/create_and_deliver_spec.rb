887a93cc63e36b31406555cc08495a73dea1a3b3
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-09-03 14:24:05 +0100

    Update create and deliver to accept a claim window

--- spec/services/claims/payment/create_and_deliver_spec.rb
+++ spec/services/claims/payment/create_and_deliver_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 describe Claims::Payment::CreateAndDeliver do
-  subject(:create_and_deliver) { described_class.call(current_user:) }
+  subject(:create_and_deliver) { described_class.call(current_user:, claim_window:) }
 
+  let(:claim_window) { Claims::ClaimWindow.current || create(:claim_window, :current) }
   let(:current_user) { create(:claims_support_user) }
 
   describe "#call" do
@@ -13,16 +14,20 @@ describe Claims::Payment::CreateAndDeliver do
     end
 
     context "when there are submitted claims" do
-      before do
-        create(:claim, :submitted)
-      end
+      let(:historic_claim_window) { build(:claim_window, :historic) }
+      let!(:current_claim) { create(:claim, :submitted, claim_window:) }
+      let!(:historic_claim) { create(:claim, :submitted, claim_window: historic_claim_window) }
 
       it "creates a payment, activity, updates claims statuses to 'payment_in_progress',
         and enqueues the delivery of an email to the ESFA" do
         expect { create_and_deliver }.to change(Claims::Payment, :count).by(1)
           .and change(Claims::ClaimActivity, :count).by(1)
-          .and change { Claims::Claim.pluck(:status).uniq }.from(%w[submitted]).to(%w[payment_in_progress])
+          .and change { current_claim.reload.status }.from("submitted").to("payment_in_progress")
           .and enqueue_mail(Claims::PaymentMailer, :payment_created_notification)
+      end
+
+      it "does not deliver claims from a different claim window" do
+        expect { create_and_deliver }.not_to change { historic_claim.reload.status }.from("submitted")
       end
     end
   end
