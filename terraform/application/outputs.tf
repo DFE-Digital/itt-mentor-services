6e9dd0ba0cfdb6728f5aba075419946364a1cd6b
RMcVelia <rick.mcvelia@gmail.com>
2025-09-30 12:45:35 +0100

    Decom manage school placements

--- terraform/application/outputs.tf
+++ terraform/application/outputs.tf
@@ -1,7 +1,7 @@
 output "ingress_hostnames" {
-  value = [local.ingress_domain_map["INGRESS_CLAIMS_HOST"], local.ingress_domain_map["INGRESS_PLACEMENTS_HOST"]]
+  value = [local.ingress_domain_map["INGRESS_CLAIMS_HOST"]]
 }
 
 output "external_urls" {
-  value = [for domain in ["CLAIMS_HOST", "PLACEMENTS_HOST"] : "https://${local.app_env_values[domain]}/"]
+  value = [for domain in ["CLAIMS_HOST"] : "https://${local.app_env_values[domain]}/"]
 }
