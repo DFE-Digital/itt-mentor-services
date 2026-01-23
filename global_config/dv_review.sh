c3d5664a974aa74babe53734f7f96ac68f24607e
paul senior <paul.senior@education.gov.uk>
2026-01-23 16:49:57 +0000

    feat(2615): test istio gateway against itt dv service

--- global_config/dv_review.sh
+++ global_config/dv_review.sh
@@ -3,4 +3,4 @@ CONFIG_SHORT=rv
 AZURE_SUBSCRIPTION=s189-teacher-services-cloud-development
 AZURE_RESOURCE_PREFIX=s189d01
 KV_PURGE_PROTECTION=false
-TERRAFORM_MODULES_TAG=main
+TERRAFORM_MODULES_TAG=2615-spike-nginx-ingress-controller-replacement-istio
