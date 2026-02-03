224feecbc9194d593e35ac5214de862c6b84f4ed
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-02-03 16:48:36 +0000

    Add short name method to claim decorator

--- spec/decorators/claims/claim_window_decorator_spec.rb
+++ spec/decorators/claims/claim_window_decorator_spec.rb
@@ -25,4 +25,29 @@ RSpec.describe Claims::ClaimWindowDecorator do
       )
     end
   end
+
+  describe "#short_name" do
+    let(:academic_year) do
+      create(
+        :academic_year,
+        starts_on: Date.parse("1 September 2025"),
+        ends_on: Date.parse("31 August 2026"),
+        name: "2025 to 2026",
+      )
+    end
+    let(:claim_window) do
+      create(
+        :claim_window,
+        starts_on: Date.parse("1 October 2025"),
+        ends_on: Date.parse("31 December 2025"),
+        academic_year:,
+      )
+    end
+
+    it "returns a short formatted name based on the start and ends on dates" do
+      expect(claim_window.decorate.short_name).to eq(
+        "01/10/2025 to 31/12/2025",
+      )
+    end
+  end
 end
