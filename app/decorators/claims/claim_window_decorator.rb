0002a93869ca45277624861eaa6e6e8eaa1012bf
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-02-03 16:48:36 +0000

    Add short name method to claim decorator

--- app/decorators/claims/claim_window_decorator.rb
+++ app/decorators/claims/claim_window_decorator.rb
@@ -4,4 +4,8 @@ class Claims::ClaimWindowDecorator < Draper::Decorator
   def name
     "#{I18n.l(starts_on, format: :long)} to #{I18n.l(ends_on, format: :long)}"
   end
+
+  def short_name
+    "#{I18n.l(starts_on, format: :short)} to #{I18n.l(ends_on, format: :short)}"
+  end
 end
