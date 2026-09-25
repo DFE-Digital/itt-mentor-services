e3b2b92bbbb243190143f04ce975ff6e7e336175
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-09-25 13:10:47 +0100

    Revert "Revert "Capture payment date and recipient from the payer response""
    This reverts commit 45fe6118c95f317dc26bbdd6c7667f4385fbc9be.

--- app/jobs/claims/payment/update_claim_with_payer_response_job.rb
+++ app/jobs/claims/payment/update_claim_with_payer_response_job.rb
@@ -3,7 +3,11 @@ class Claims::Payment::UpdateClaimWithPayerResponseJob < ApplicationJob
 
   def perform(claim_update_details)
     if claim_update_details.fetch(:status) == "paid"
-      Claims::Claim::Payment::Paid.call(claim: claim(claim_update_details.fetch(:id)))
+      Claims::Claim::Payment::Paid.call(
+        claim: claim(claim_update_details.fetch(:id)),
+        paid_to_la: claim_update_details[:paid_to_la],
+        date_paid: claim_update_details[:date_paid],
+      )
     elsif claim_update_details.fetch(:status) == "unpaid"
       Claims::Claim::Payment::InformationRequested.call(
         claim: claim(claim_update_details.fetch(:id)),
