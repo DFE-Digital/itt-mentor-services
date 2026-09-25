46d3a8a951f1a4d03a54f91172469aca1cd45701
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-09-25 12:09:22 +0100

    Revert "Add payment date and recipient columns to claims"
    This reverts commit 66b01b7f9ad61ddcd717625de0b016e6f9c46c83.

--- db/migrate/20260921140000_add_payment_details_to_claims.rb
+++
@@ -1,8 +0,0 @@
-class AddPaymentDetailsToClaims < ActiveRecord::Migration[8.0]
-  def change
-    change_table :claims, bulk: true do |t|
-      t.boolean :paid_to_la
-      t.datetime :date_paid
-    end
-  end
-end
