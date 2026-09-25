bb6eeff162e49be358e994aa67e8d46838363b90
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-09-25 12:09:22 +0100

    Revert "Update copy for audit requested email"
    This reverts commit 7ebc2ffaee6ece6822d1e62536db74c62e239188.

--- spec/mailers/claims/provider_mailer_spec.rb
+++ spec/mailers/claims/provider_mailer_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Claims::ProviderMailer, type: :mailer do
   let(:service_name) { "Claim funding for mentor training" }
   let(:support_email) { "ittmentor.funding@education.gov.uk" }
   let(:service_url) { claims_root_url }
+  let(:completion_date) { "10 February 2025" }
   let(:download_access_token_double) { instance_double(Claims::DownloadAccessToken) }
 
   before do
@@ -17,98 +18,173 @@ RSpec.describe Claims::ProviderMailer, type: :mailer do
   describe "#sampling_checks_required" do
     subject(:sampling_checks_required_email) { described_class.sampling_checks_required(provider_sampling, email_address: provider.primary_email_address) }
 
-    let(:number_of_claims) { "2 claims" }
-    let(:expected_body) do
-      <<~EMAIL
-        #{provider.name},
-
-        You are required by the Department for Education (DfE) to audit #{number_of_claims} for initial teacher training (ITT) general mentor funding associated with #{provider.name}.
-
-        One or more schools submitted funding requests to DfE due to you providing training for their staff to become ITT mentors.
-
-        # You must audit claims by #{completion_date}
-
-        If you do not audit these claims by 11:59pm on #{completion_date}, we may escalate the audit process. This can include removing funding from schools you worked with.
-
-        ------------
-
-        # What you need to do
-
-        You have been added to the Claim funding for mentor training service to audit claims. If you are not the right person in your organisation, please:
-
-        - access the service using DfE sign-in and add an appropriate colleague in the Users section
-        - forward this email to the appropriate colleague after adding them as a user
-
-        Sign in to the Claim service using your DfE sign-in account. Review the claims selected for audit, then select each claim and follow the steps on the page.
-
-        - If the claims are accurate, select the ‘Approve’ button.
-        - If one or more claims are not accurate, select the ‘Amend’ button and, when prompted, provide the reason why the claim is not accurate.
-
-        Some reasons for amending a claim may include that a mentor is:
-
-        - an Early Career Teacher Entitlement mentor, rather than ITT
-        - claiming too many hours
-        - not known to you
-        - not employed at the school
-
-        --------
-
-        ## After you complete the audit
-
-        If you have amended any claims, schools will receive a notification of this change. Schools will then have 30 days to respond to the audit.
-
-        Make sure you speak to the school about any amended claims before you submit them on the service. This will avoid any confusion about their eligibility for funding.
-
-        --------
-
-        ## Contact us
-
-        If you need any help with signing in to the service, or completing the audit, contact the team at [#{support_email}](mailto:#{support_email})
-
-        Learn more about [funding for mentor training on GOV.UK](http://claims.localhost/?utm_campaign=provider&utm_medium=notification&utm_source=email)
-
-        Claim funding for mentor training team
-      EMAIL
-    end
-
-    before do
-      create_list(:claims_provider_sampling_claim, 2, provider_sampling:)
-      Timecop.freeze(Time.zone.parse("#{current_date} 00:00"))
-    end
-
-    after do
-      Timecop.return
-    end
-
     context "when the completion date is a weekday" do
       let(:current_date) { "20/01/2025" }
-      let(:completion_date) { "19 February 2025" }
+
+      before do
+        Timecop.freeze(Time.zone.parse("#{current_date} 00:00"))
+      end
+
+      after do
+        Timecop.return
+      end
 
       it "sends the sampling checks required email" do
         expect(sampling_checks_required_email.to).to match_array(provider.primary_email_address)
-        expect(sampling_checks_required_email.subject).to eq("ITT mentor claims need to be audited")
-        expect(sampling_checks_required_email.body.to_s.squish).to eq(expected_body.squish)
+        expect(sampling_checks_required_email.subject).to eq("ITT mentor claims need to be quality assured")
+        expect(sampling_checks_required_email.body.to_s.squish).to eq(<<~EMAIL.squish)
+          #{provider.name},
+
+          You are required by Department for Education (DfE) to complete quality assurance on funding claims associated with #{provider.name}.
+
+          One or more schools submitted funding requests to DfE due to you providing training for their staff to become initial teacher training (ITT) mentors.
+
+          # You must complete quality assurance by #{completion_date}
+
+          If you do not check these claims by 11:59pm on #{completion_date}, we may escalate the assurance process. This can include removing funding from schools you worked with.
+
+          ------------
+
+          # What you need to do
+
+          Use the CSV file to check the accuracy of the claims associated with you and record your answers in the file. It is in a spreadsheet format for you to fill out.#{"  "}
+
+          Visit the GOV.UK claim funding for mentor training website to download the file:
+
+          [http://claims.localhost/sampling/claims?token=token&utm_campaign=provider&utm_medium=notification&utm_source=email](http://claims.localhost/sampling/claims?token=token&utm_campaign=provider&utm_medium=notification&utm_source=email)
+
+          Before you download this link, please note that it will expire as soon as one person in your organisation has downloaded it. If it is not opened in 7 days, it will expire due to data security. To request a new link, reply to this email
+
+          To complete the CSV, you must:
+
+            - fill in ‘yes’ or ‘no’ in the ‘claim_accepted’ column
+            - for any ‘no’ answers, give us a reason for rejection validated by the school in the ‘rejection_reason’ column
+            - reply to this email and attach the updated file by #{completion_date}
+
+          ## If the claims are accurate
+          If the mentors, hours and number of claims are correct, mark the claims as ‘yes’ in the ‘claim_accepted’ column of the CSV file.
+
+          ## If you disagree with a claim
+          If you disagree with the information a school submitted to us, contact the school to discuss it. They may have additional evidence or a reason.
+
+          ## If the school gives you valid evidence after speaking to them
+          If you accept the evidence the placement school provides, mark the claim as ‘yes’ in the ‘claim_accepted’ column.
+
+          ## If the school does not give you valid evidence after speaking to them
+          If the school cannot provide any additional information or cannot provide information that you accept, mark the claim as ‘no’ in the ‘claim_accepted’ column.
+
+          You must give a reason why a claim is incorrect. Write this in the ‘rejection_reason’ column.
+
+          Some reasons may include that a mentor is:
+
+            - on the Early Career Framework (ECF), rather than ITT
+            - claiming too many hours
+            - claiming too few hours
+            - not known to you
+            - not employed at the school
+
+          --------
+
+          ## After you complete quality assurance
+
+          For any rejected claims, we will contact schools to confirm they agree. Make sure you speak to the school about rejected claims before you submit your answers to us. This will avoid any confusion about their eligibility for funding.
+
+          --------
+
+          ## Contact us
+
+          If you need any help with completing the quality assurance, contact the team at [#{support_email}](mailto:#{support_email})
+
+          Learn more about [funding for mentor training on GOV.UK](http://claims.localhost/?utm_campaign=provider&utm_medium=notification&utm_source=email)
+
+
+          Claim funding for mentor training team
+        EMAIL
       end
     end
 
     context "when the completion date is a weekend" do
-      let(:current_date) { "24/01/2025" }
-      let(:completion_date) { "24 February 2025" }
+      let(:current_date) { "18/01/2025" }
 
-      it "sends the sampling checks required email with the next weekday as the completion date" do
-        expect(sampling_checks_required_email.body.to_s.squish).to eq(expected_body.squish)
+      before do
+        Timecop.freeze(Time.zone.parse("#{current_date} 00:00"))
       end
-    end
 
-    context "when the provider has a single claim to audit" do
-      let(:current_date) { "20/01/2025" }
-      let(:completion_date) { "19 February 2025" }
-      let(:number_of_claims) { "1 claim" }
+      after do
+        Timecop.return
+      end
 
-      before { provider_sampling.provider_sampling_claims.last.destroy! }
+      it "sends the sampling checks required email" do
+        expect(sampling_checks_required_email.to).to match_array(provider.primary_email_address)
+        expect(sampling_checks_required_email.subject).to eq("ITT mentor claims need to be quality assured")
+        expect(sampling_checks_required_email.body.to_s.squish).to eq(<<~EMAIL.squish)
+          #{provider.name},
 
-      it "uses the singular form of claim" do
-        expect(sampling_checks_required_email.body.to_s.squish).to eq(expected_body.squish)
+          You are required by Department for Education (DfE) to complete quality assurance on funding claims associated with #{provider.name}.
+
+          One or more schools submitted funding requests to DfE due to you providing training for their staff to become initial teacher training (ITT) mentors.
+
+          # You must complete quality assurance by #{completion_date}
+
+          If you do not check these claims by 11:59pm on #{completion_date}, we may escalate the assurance process. This can include removing funding from schools you worked with.
+
+          ------------
+
+          # What you need to do
+
+          Use the CSV file to check the accuracy of the claims associated with you and record your answers in the file. It is in a spreadsheet format for you to fill out.#{"  "}
+
+          Visit the GOV.UK claim funding for mentor training website to download the file:
+
+          [http://claims.localhost/sampling/claims?token=token&utm_campaign=provider&utm_medium=notification&utm_source=email](http://claims.localhost/sampling/claims?token=token&utm_campaign=provider&utm_medium=notification&utm_source=email)
+
+          Before you download this link, please note that it will expire as soon as one person in your organisation has downloaded it. If it is not opened in 7 days, it will expire due to data security. To request a new link, reply to this email
+
+          To complete the CSV, you must:
+
+            - fill in ‘yes’ or ‘no’ in the ‘claim_accepted’ column
+            - for any ‘no’ answers, give us a reason for rejection validated by the school in the ‘rejection_reason’ column
+            - reply to this email and attach the updated file by #{completion_date}
+
+          ## If the claims are accurate
+          If the mentors, hours and number of claims are correct, mark the claims as ‘yes’ in the ‘claim_accepted’ column of the CSV file.
+
+          ## If you disagree with a claim
+          If you disagree with the information a school submitted to us, contact the school to discuss it. They may have additional evidence or a reason.
+
+          ## If the school gives you valid evidence after speaking to them
+          If you accept the evidence the placement school provides, mark the claim as ‘yes’ in the ‘claim_accepted’ column.
+
+          ## If the school does not give you valid evidence after speaking to them
+          If the school cannot provide any additional information or cannot provide information that you accept, mark the claim as ‘no’ in the ‘claim_accepted’ column.
+
+          You must give a reason why a claim is incorrect. Write this in the ‘rejection_reason’ column.
+
+          Some reasons may include that a mentor is:
+
+            - on the Early Career Framework (ECF), rather than ITT
+            - claiming too many hours
+            - claiming too few hours
+            - not known to you
+            - not employed at the school
+
+          --------
+
+          ## After you complete quality assurance
+
+          For any rejected claims, we will contact schools to confirm they agree. Make sure you speak to the school about rejected claims before you submit your answers to us. This will avoid any confusion about their eligibility for funding.
+
+          --------
+
+          ## Contact us
+
+          If you need any help with completing the quality assurance, contact the team at [#{support_email}](mailto:#{support_email})
+
+          Learn more about [funding for mentor training on GOV.UK](http://claims.localhost/?utm_campaign=provider&utm_medium=notification&utm_source=email)
+
+
+          Claim funding for mentor training team
+        EMAIL
       end
     end
   end
