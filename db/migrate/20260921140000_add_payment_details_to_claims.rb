98f091d86ee643d2e2b5717d86888e60b39611c8
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-09-25 12:14:20 +0100

    Add safety assured to migration

--- db/migrate/20260921140000_add_payment_details_to_claims.rb
+++ db/migrate/20260921140000_add_payment_details_to_claims.rb
@@ -1,8 +1,11 @@
 class AddPaymentDetailsToClaims < ActiveRecord::Migration[8.0]
   def change
-    change_table :claims, bulk: true do |t|
-      t.boolean :paid_to_la
-      t.datetime :date_paid
+    # Rubocop prefers this way of updating the table but it is not compatible with the strong_migrations gem
+    safety_assured do
+      change_table :claims, bulk: true do |t|
+        t.boolean :paid_to_la
+        t.datetime :date_paid
+      end
     end
   end
 end
