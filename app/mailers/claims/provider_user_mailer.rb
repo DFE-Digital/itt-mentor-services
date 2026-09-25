6edc3422d43a4f7ec4578f1aabf2c3461a076d86
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-09-25 11:20:44 +0100

    Update user invitation email now content is ready

--- app/mailers/claims/provider_user_mailer.rb
+++ app/mailers/claims/provider_user_mailer.rb
@@ -4,7 +4,6 @@ class Claims::ProviderUserMailer < Claims::ApplicationMailer
                  subject: t(".subject", service_name:),
                  body: t(
                    ".body",
-                   user_name: user.first_name,
                    organisation_name: organisation.name,
                    service_name:,
                    support_email:,
