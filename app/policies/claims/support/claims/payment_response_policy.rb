21139531622e146292e72e4727e232a049d320cb
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-09-25 13:10:47 +0100

    Revert "Revert "Remove the unreachable payment response upload path""
    This reverts commit 2165d105fc9041c19da654e1e1ed77567d789f98.

--- app/policies/claims/support/claims/payment_response_policy.rb
+++
@@ -1,9 +0,0 @@
-class Claims::Support::Claims::PaymentResponsePolicy < Claims::ApplicationPolicy
-  def new?
-    true
-  end
-
-  def update?
-    Claims::Claim.payment_in_progress.any?
-  end
-end
