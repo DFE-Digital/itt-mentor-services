6aa6332fcbb47a3e8915b84dfe1fa4fcffcb397b
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-09-25 13:12:22 +0100

    Add safety assured to migration as strong migrations does not support change_table bulk operation

--- db/migrate/20260921140000_add_payment_details_to_claims.rb
+++ db/migrate/20260921140000_add_payment_details_to_claims.rb
@@ -1,8 +1,10 @@
 class AddPaymentDetailsToClaims < ActiveRecord::Migration[8.0]
   def change
-    change_table :claims, bulk: true do |t|
-      t.boolean :paid_to_la
-      t.datetime :date_paid
+    safety_assured do
+      change_table :claims, bulk: true do |t|
+        t.boolean :paid_to_la
+        t.datetime :date_paid
+      end
     end
   end
 end
